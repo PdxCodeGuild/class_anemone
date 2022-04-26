4de6dc1bde6ac181b721869ec44afbae8d52f4fa
avocado-jc <cavada.jonathon@gmail.com>
2022-04-26 15:51:56 -0700

    lab01 v1 submission

--- code/cavada/python/lab01.py
+++ code/cavada/python/lab01.py
@@ -1,1 +1,11 @@
 """lab 1 version 1"""
+
+user = float(input("how tall are you in feet? "))
+if user:
+    user *= .3048
+    conversion = round(user, 2)
+    print(f"you are {conversion} meters tall")
+
+
+
+
