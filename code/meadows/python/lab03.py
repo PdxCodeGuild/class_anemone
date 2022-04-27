9e1e43b1824c5b675b8a0bd76466dad1e6b50e95
Jonathan meadows <jon_meadows@ymail.com>
2022-04-27 18:13:13 -0500

    lab03 v3/optional

--- code/meadows/python/lab03.py
+++ code/meadows/python/lab03.py
@@ -1,12 +1,14 @@
 import math
 import string
 
-ones_dic = {1:'one', 2:'two', 3:'three', 4:'four', 5:'five', 6:'six',
- 7:'seven', 8:'eight', 9:'nine', 10:'ten'}
-tens_dic = {2:'twenty', 3:'thirty', 4:'fourty', 5:'fifty', 6:'sixty',
-7:'seventy', 8:'eighty', 9:'ninety'}
-teen_dic = {11:'eleven', 12:'twelve', 13:'thriteen', 14:'fourteen', 15:'fifteen',
-16:'sixteen', 17:'seventeen', 18:'eighteen', 19:'nineteen'}
+roman_ones = {1:'I', 2:'II', 3:'III', 4:'IV', 5:'V', 6:'VI',
+ 7:'VII', 8:'VIII', 9:'IX', 10:'X'}
+roman_tens = {2:'XX', 3:'XXX', 4:'XL', 5:'L', 6:'LX',
+7:'LXX', 8:'LXXX', 9:'XC'}
+roman_teens = {11:'XI', 12:'XII', 13:'XIII', 14:'XIV', 15:'XV',
+16:'XVI', 17:'XVII', 18:'XVIII', 19:'XIX'}
+roman_hundreds = {1:'C', 2:'CC', 3:'CCC', 4:'CD', 5:'D', 6:'DC',
+ 7:'DCC', 8:'DCCC', 9:'CM', 10:'M'}
 
 
 yes = True
@@ -14,29 +16,29 @@ while yes:
     player = int(input('Enter a Number: '))
     if player in range(1, 11):
         ones = player%10
-        b_ones = ones_dic.get(ones)
+        b_ones = roman_ones.get(ones)
         print(b_ones)
     elif player in range(11, 20):
-        teens = teen_dic.get(player)
+        teens = roman_teens.get(player)
         print(teens)
     elif player in range(20, 100):
         tens = player//10
-        a_tens = tens_dic.get(tens)
+        a_tens = roman_tens.get(tens)
         ones = player%10
-        b_ones = ones_dic.get(ones)
-        print(a_tens,'-',b_ones)
+        b_ones = roman_ones.get(ones)
+        print(f'{a_tens}{b_ones}')
     elif player in range(100, 999):
         hundo = player//100
-        a_hundo = ones_dic.get(hundo)
+        a_hundo = roman_hundreds.get(hundo)
         ones = player%10
-        b_ones = ones_dic.get(ones)
+        b_ones = roman_ones.get(ones)
         tens = player%100//10
-        a_tens = tens_dic.get(tens)
-        print(a_hundo,'hundred','-', a_tens ,'-',b_ones)
+        a_tens = roman_tens.get(tens)
+        print(f'{a_hundo}{b_ones}{a_tens}')
     
     yeah = ['yes', 'y', 'yeh', 'sure', 'si']
     no = ['no','nah','nope', 'nay']
-    again = input('check more? ( Yes or No ) : ')
+    again = str(input('check more? ( Yes or No ) : '))
     if again in yeah:
         yes = True    
     else:
