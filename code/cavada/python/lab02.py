757ab8f1847adcef2de193b5c0805dad3897647e
avocado-jc <cavada.jonathon@gmail.com>
2022-04-27 14:27:35 -0700

    lab02 submit

--- code/cavada/python/lab02.py
+++ code/cavada/python/lab02.py
@@ -1,21 +1,21 @@
 """lab 2 v1 & v2"""
 
-nums = [] # setting this for this variable to work in my loop
+nums = [] # setting this list to gather user input numbers
 
-user = ''
+user = '' # setting this up to serve as an interm
 counter = 0
-input("press any key to find the average of a set of numbers...")
-while True:
-    user = 0.0
-    if user == 'done':
-        nums.pop()
+input("press any key to find the average of a set of numbers...") # gathering user input for summing and eventually finding avg of numbers
+while True: # setting up while loop to continuously collect user input until they prompt termination of collection
+    user = 0.0 # might be overkill, did this to make sure user was a float
+    if user == 'done': # honestly not sure if this is neceessary, but intended to prevent counting str 'done' later 
+        nums.pop() 
         break
-    user = (input("add a number, or type 'done' to get the average: "))
-    if user != 'done':
+    user = (input("add a number, or type 'done' to get the average: ")) # this is actual prompt to collect data
+    if user != 'done': 
         user = float(user)
         nums.append(user)
-        counter += 1
-        # print(nums)
+        counter += 1 # not sure if this is efficient but using this to get count of numbers input by user
+        # print(nums) 
         if user == 'done':
             nums.pop()
             break
@@ -23,14 +23,14 @@ while True:
     if user == 'done':
         break
 print(nums)
-def avg(total, n):
+def avg(total, n): # made this function just to get into the habit of using functions
     return total / n
-sum = 0
+sum = 0 # used this variable and subsequent 'for loop' to sum the values in list num
 for num in nums:
     sum += num
 n = counter
 print(f"sum: {sum}; avg: {(avg(sum,n))}")    
-# print(avg(sum, n))
+
 
 
 
