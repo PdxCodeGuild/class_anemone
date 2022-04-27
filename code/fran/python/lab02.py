f1d7a0bc8f7e8cf8f5ceb7823b523ea2129a3f7b
Frances Kappes <fekmacbookpro2019@Feks-MacBook-Pro-2019.local>
2022-04-27 15:31:24 -0700

    final version for submission

--- code/fran/python/lab02.py
+++ code/fran/python/lab02.py
@@ -3,17 +3,27 @@
 
 # Define variables
 sum = 0
-# Define and populate numbers list
-nums = [5, 0, 8, 3, 4, 1, 6]
 
-# Loop through numbers list and sum all the numbers
-for num in nums:
-	sum += num
+# Define list that will store numbers entered by user
+nums = []
 
- # Calculate the average
-average = round((sum/(len(nums))),3)
+while True:
+    num = input("enter a number or 'done': ")
+    if num == 'done':
+        break
+    nums.append(int(num))
 
-# Print results
-print(f"""
-List: {nums}
-Average: {average}""")
+if len(nums) > 0:
+    
+    # Loop through numbers list and sum all the numbers
+    for num in nums:
+        sum += num
+
+    # Calculate the average
+    average = round((sum/(len(nums))),3)
+
+    # Print results
+    print(f"""
+    List: {nums}
+    Average: {average}
+    """)
