d02e2d72ed5218e5ee95773dad04eb430203dcee
Ollie's Macbook Pro <ojc@Ollies-MacBook-Pro.local>
2022-04-27 16:41:23 -0700

    version 1 done

---
+++ code/ollie/python/lab-02.py
@@ -0,0 +1,14 @@
+' Version 1 '
+
+# nums = [5, 0, 8, 3, 4, 1, 6]
+# sum = 0
+# for num in nums:
+#     sum = sum + num
+# average = sum / len(nums)
+# print(average)
+
+' Version 2 '
+
+nums = []
+nums.append("Enter a number or 'done': ")
+print(nums)
