cb1fff6d2a7a8d3cae869b2e047c7d25afa0712d
ICF19 <icfearn92@outlook.com>
2022-04-27 11:53:46 -0700

    updated lab01

--- code/isaac/lab01.py
+++ code/isaac/lab01.py
@@ -18,9 +18,9 @@ yd = 0.9144 # yards
 inc = 0.0254 # inches
 
 # Ask for user input and convert the selected UOM (Unit of Measurement)
-# ask_distance = input("What is the distance in feet? ") # use ft in conditional statements for conversion
-# meter_distance = int(ask_distance) * ft
-# print(f"{ask_distance} feet is {round(meter_distance, 5)} meters ")
+ask_distance = input("What is the distance in feet? ") # use ft in conditional statements for conversion
+meter_distance = int(ask_distance) * ft
+print(f"{ask_distance} feet is {round(meter_distance, 5)} meters ")
 
 # ---------------------------------------------------------------------- #
 
@@ -28,21 +28,21 @@ inc = 0.0254 # inches
 
 # Ask for the distance and the set the distance to the corresponding UOM.
 
-# user_distance = int(input("What is the distance? "))
-# distance_unit = input("What is the unit? ")
+user_distance = int(input("What is the distance? "))
+distance_unit = input("What is the unit? ")
 
-# if distance_unit == 'feet':
-#     total_ft = user_distance * ft
-#     print(f"{user_distance} feet is {total_ft} meters")
-# elif distance_unit == 'miles':
-#     total_mi = user_distance * mi
-#     print(f"{user_distance} miles is {total_mi} meters")
-# elif distance_unit == 'kilometers':
-#     total_km = user_distance * km
-#     print(f"{user_distance} kilometers is {total_km} meters")
-# elif distance_unit == 'meters':
-#     total_m = user_distance * m
-#     print(f"{user_distance} meters is {total_m} meters")
+if distance_unit == 'feet':
+    total_ft = user_distance * ft
+    print(f"{user_distance} feet is {total_ft} meters")
+elif distance_unit == 'miles':
+    total_mi = user_distance * mi
+    print(f"{user_distance} miles is {total_mi} meters")
+elif distance_unit == 'kilometers':
+    total_km = user_distance * km
+    print(f"{user_distance} kilometers is {total_km} meters")
+elif distance_unit == 'meters':
+    total_m = user_distance * m
+    print(f"{user_distance} meters is {total_m} meters")
 
 # ------------------------------------------------------------------------- #
 
@@ -50,33 +50,33 @@ inc = 0.0254 # inches
 
 # Incorporate yards and inches
 
-# user_distance = int(input("What is the distance? "))
-# distance_unit = input("What is the unit? ")
+user_distance = int(input("What is the distance? "))
+distance_unit = input("What is the unit? ")
 
-# if distance_unit == 'feet':
-#     total_ft = user_distance * ft
-#     print(f"{user_distance} foot/feet is {total_ft} meters")
-# elif distance_unit == 'miles':
-#     total_mi = user_distance * mi
-#     print(f"{user_distance} mile/s is {total_mi} meters")
-# elif distance_unit == 'kilometers':
-#     total_km = user_distance * km
-#     print(f"{user_distance} kilometer/s is {total_km} meters")
-# elif distance_unit == 'meters':
-#     total_m = user_distance * m
-#     print(f"{user_distance} meter/s is {total_m} meters")
-# elif distance_unit == 'yards':
-#     total_yd = user_distance * yd
-#     print(f"{user_distance} yard/s is {total_yd} meters")
-# elif distance_unit == 'inches':
-#     total_inc = user_distance * inc
-#     print(f"{user_distance} inches is {total_inc} meters")
+if distance_unit == 'feet':
+    total_ft = user_distance * ft
+    print(f"{user_distance} foot/feet is {total_ft} meters")
+elif distance_unit == 'miles':
+    total_mi = user_distance * mi
+    print(f"{user_distance} mile/s is {total_mi} meters")
+elif distance_unit == 'kilometers':
+    total_km = user_distance * km
+    print(f"{user_distance} kilometer/s is {total_km} meters")
+elif distance_unit == 'meters':
+    total_m = user_distance * m
+    print(f"{user_distance} meter/s is {total_m} meters")
+elif distance_unit == 'yards':
+    total_yd = user_distance * yd
+    print(f"{user_distance} yard/s is {total_yd} meters")
+elif distance_unit == 'inches':
+    total_inc = user_distance * inc
+    print(f"{user_distance} inches is {total_inc} meters")
 
 # ------------------------------------------------------------------------------------ #
 
 # VERSION 4
 
-# Convert to UOM to meters and then convert to new UOM
+# Convert to UOM to meters 
 
 user_distance = int(input("What is the distance? "))
 first_unit = input("Enter first unit: ")
@@ -89,10 +89,16 @@ elif first_unit == 'kilometers':
 elif first_unit == 'meters':
     outcome = user_distance * m
 
-
+# Convert to new UOM
 
 second_unit = input("Enter second unit: ")
 if second_unit == 'miles':
     new_unit_dis = outcome / mi
-    print(round(new_unit_dis, 5))
+elif second_unit == 'kilometers':
+    new_unit_dis = outcome / km
+elif second_unit == 'meters':
+    new_unit_dis = outcome / m
+elif second_unit == 'feet':
+    new_unit_dis = outcome / ft
 
+print(round(new_unit_dis))
