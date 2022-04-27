dd1c7af7ff8b2523e7aa12b9021aecf9ecdf04b6
Jonathan meadows <jon_meadows@ymail.com>
2022-04-27 17:17:58 -0500

    Lab03 V2

--- code/meadows/python/lab03.py
+++ code/meadows/python/lab03.py
@@ -8,15 +8,15 @@ tens_dic = {2:'twenty', 3:'thirty', 4:'fourty', 5:'fifty', 6:'sixty',
 teen_dic = {11:'eleven', 12:'twelve', 13:'thriteen', 14:'fourteen', 15:'fifteen',
 16:'sixteen', 17:'seventeen', 18:'eighteen', 19:'nineteen'}
 
+
 yes = True
 while yes:
     player = int(input('Enter a Number: '))
-
-    if player in range(1,11):
+    if player in range(1, 11):
         ones = player%10
         b_ones = ones_dic.get(ones)
         print(b_ones)
-    elif player in range(11,20):
+    elif player in range(11, 20):
         teens = teen_dic.get(player)
         print(teens)
     elif player in range(20, 100):
@@ -25,6 +25,15 @@ while yes:
         ones = player%10
         b_ones = ones_dic.get(ones)
         print(a_tens,'-',b_ones)
+    elif player in range(100, 999):
+        hundo = player//100
+        a_hundo = ones_dic.get(hundo)
+        ones = player%10
+        b_ones = ones_dic.get(ones)
+        tens = player%100//10
+        a_tens = tens_dic.get(tens)
+        print(a_hundo,'hundred','-', a_tens ,'-',b_ones)
+    
     yeah = ['yes', 'y', 'yeh', 'sure', 'si']
     no = ['no','nah','nope', 'nay']
     again = input('check more? ( Yes or No ) : ')
@@ -33,6 +42,7 @@ while yes:
     else:
         print('BYE!')
         break
+
 
 
 
