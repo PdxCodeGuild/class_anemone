ae491e797c47463752e06e29a0fc3e414d826a00
Zach-EE <zachholloway.n@gmail.com>
2022-04-28 15:28:29 -0400

    made order change in the creation of keys in deck dic

--- code/zach/python/lab04/deck_builder.py
+++ code/zach/python/lab04/deck_builder.py
@@ -4,7 +4,7 @@ def deck_builder(suits_lis, cards_dic):
     deck = {}
     for suit in suits_lis:
         for card in cards_dic:
-            deck[suit+card] = cards_dic[card] 
+            deck[card+' '+suit] = cards_dic[card] 
 
     print(deck)
 
