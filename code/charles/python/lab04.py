b83017ef1043209b050546bd0b7e3ece4f18cc7e
Adeeon <cgt10dt@gmail.com>
2022-04-28 16:32:25 -0500

    lab04

--- code/charles/python/lab04.py
+++ code/charles/python/lab04.py
@@ -9,7 +9,7 @@ def draw():
     blackj_deck = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'] # dict for letters
     numblackj_deck = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':10, 'Q':10, 'K':10 } #dict for letter numbers
     hand = [] # players current hand 
-    total = []
+    total = [] # num list of cards in hand
     card = random.choice(blackj_deck)
     hand.append(card)
     print(f'Your first card is a {card}')
@@ -18,11 +18,11 @@ def draw():
     card = random.choice(blackj_deck)
     hand.append(card)
     print(f'Your second card is a {card}')
-    total.append(numblackj_deck[card]) 
+    total.append(numblackj_deck[card])     #drawing of players first two cards
 
      
 
-    while sum(total) < 21:
+    while sum(total) < 21: # while loop to add cards to hand til bust or stay
         deal = input(f'{print(" " .join(hand))} You are at {sum(total)}. Would you like to hit or stay: ').lower()
                   
         if deal in no:
@@ -35,14 +35,14 @@ def draw():
         else:
             break
     
-    if  sum(total) > 21:
+    if  sum(total) > 21:               # if/elif for based of sum of total
         status = 'Busted'
         print(sum(total), status)
         return status
     elif sum(total) == 21:
-            status = 'BLACKJACK!'
-            print(sum(total), status)
-            return status
+        status = 'BLACKJACK!'
+        print(sum(total), status)
+        return status
     elif sum(total) < 21:
         print(f'Staying at {sum(total)}')
         status = 'Should have gone for broke'
@@ -50,7 +50,7 @@ def draw():
         
 
 
-def Lets_Play():
+def Lets_Play():          # black jack initilizer
     play = True
     yes = ['yes', 'yea', 'y']
     no = ['no', 'na', 'n']
