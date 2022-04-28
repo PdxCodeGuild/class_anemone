5dcb6c8b67ae63e49575c8d678394bf707714b88
Rachel Lloyd <rlloyd98@gmail.com>
2022-04-27 23:31:07 -0400

    Lab 3

--- code/rachel/Python/Lab3.py
+++ code/rachel/Python/Lab3.py
@@ -1,14 +1,19 @@
 #Version 1
 #Convert a given number into its english representation
 
+from pickle import encode_long
+from socketserver import ThreadingUDPServer
+
+
 num = input('Enter a number: ')
 
 #Example: 67
 
-tens_digit = int((num)) // 10
-ones_digit = int((num)) % 10
+hundred_digit = int(num) // 100
+tens_digit = int(num) // 10
+ones_digit = int(num) % 10
 
-#Result: 6 and 7
+# #Result: 6 and 7
 
 english_nums = {
     
@@ -40,6 +45,16 @@ english_nums = {
     70 : "seventy",
     80 : "eighty",
     90 : "ninety",
+
+    100 : "one hundred",
+    200 : "two hundred",
+    300 : "three hundred",
+    400 : "four hundred",
+    500 : "five hundred",
+    600 : "six hundred",
+    700 : "seven hundred",
+    800 : "eight hundred",
+    900 : "nine hundred",
 }
 
 if len(str(num)) == 1:
@@ -58,5 +73,23 @@ elif len(str(num)) == 2:
     tens_digit = int(str(tens_digit) + "0")
     print (english_nums[tens_digit] + "-" + english_nums[ones_digit])    #prints english words for numbers 21 through 99 (not 20,30,etc.)
 
+elif len(str(num)) == 3 and num[1] == "1":
+    hundred_digit = int(str(hundred_digit) + "00")
+    tens_digit = str(num[1]) + str(num[2])
+    tens_digit = int(tens_digit)
+    print (english_nums[hundred_digit], " and " ,english_nums[tens_digit])
 
+elif len(str(num)) == 3 and num[1] == "0" and num[2] == "0":
+    hundred_digit = int(str(hundred_digit) + "00")
+    print (english_nums[hundred_digit])
 
+elif len(str(num)) == 3 and num[1] == "0":                  
+    hundred_digit = int(str(hundred_digit) + "00")          
+    print (english_nums[hundred_digit] + " and " + english_nums[ones_digit])
+
+elif len(str(num)) == 3:
+    hundred_digit = int(str(hundred_digit) + "00") #400
+    hundred_tens_digit = int(num[1] + "0") #50
+    ones_digit = int(num[2])
+    print (english_nums[hundred_digit], english_nums[hundred_tens_digit] + "-" + english_nums[ones_digit])
+    
