813d741b1f8d83b9198d1f2541152d3fa4b43616
Adeeon <cgt10dt@gmail.com>
2022-04-29 12:26:06 -0500

    lab01v2

--- code/charles/python/lab05.py
+++ code/charles/python/lab05.py
@@ -1,24 +1,16 @@
 import random
 
-opick6 = []
-ppick6 = []
-balance = 0
-t_cost = 2
 
 
-def pick6(list):
+def pick6():    # generates 6 random numbers to put out as a list
     dpick6 = []
     while len(dpick6) < 6:
         add = random.choice(range(1, 100))
         dpick6.append(add)
     return dpick6
 
-pick6(opick6)
-pick6(ppick6)
-k = set(opick6)
-n = set(ppick6)
 
-def winnings(counter):
+def winnings(counter):  # takes the input of the matching number to give winnings
     if counter == 0:
         winnings = 0
     elif counter == 1:
@@ -35,29 +27,35 @@ def winnings(counter):
         winnings == 25000000
     return winnings
 
-def compare(list1, list2):
+def compare(list1, list2):  # compares two inputted list and modifys counter if list are equal.
     counter = 0
-    u = list1 - list2
-    if u == 0:
-        counter += 1
-    elif u != 0:
-        counter += 0
+    i = 0
+    while i < 5: 
+        u = list1[i] - list2[i]
+        if u == 0:
+            counter += 1
+            i +=1
+        elif u != 0:
+            i += 1
+            
     return counter
 
-
-
-# def lotto(number):
-#     while x < number:
-#         return
-
-print(opick6, ppick6)
-u = lambda x,y: x - y
-i = 0
-while i < 5:
-    print(u(n,k))
-    i = i + 1
+def lotto():       # plays the lottery 100,000 times... 100,000,000 basicaly bricks the program cause of run time... down by a 10th per digit
+    invest = 0 
+    winning = 0
+    i = 0
+    
+    while i < 100000:
+        i += 1
+        winning += winnings(compare(pick6(), pick6()))
+        invest += -2
+    
+    ROI = (winning + invest) / invest #ROI after the while to allow it to run the amount of times before calculating (also add a negative to subtract... duh)
+    
+    return winning, invest, ROI    
     
 
+print(lotto()) # average loss is roughly -180000
 
 
-
+# print(compare(f, k))
