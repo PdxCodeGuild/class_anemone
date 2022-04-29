5d8adb581a826cd3a82600fd1515477b16616a2a
Zach-EE <zachholloway.n@gmail.com>
2022-04-28 20:15:08 -0400

    changed name to dealer.py/ began working on game logic for black jack in lab04.py/ pushing to save

--- code/zach/python/lab04/lab04.py
+++ code/zach/python/lab04/lab04.py
@@ -1,6 +1,31 @@
-import deck_builder
+import dealer
+import variables
+
+def bj_game(play_game):
+    deal_count = 0
+    game_deck = dealer.build_deck()
+    
+
+    bj_deal(game_deck, deal_count)
+
+
+
+    return print('done')
+
+def bj_deal(game_deck, deal_count):
+    hit = 'H'
+    deck_list = []
+    while hit == 'H' and deal_count < 52:
+        player_hand
+        if deal_count <= 48:
+            print(deal_count)
+            deal_count += 4
+        hit = input('h or s').upper()
+        
+    print(deal_count)
 
 def main():
-    return deck_builder.deck_builder()
+        play_game = input('Would you like to learn to play ♠️♥️♣️♦️BlackJack♠️♥️♣️♦️?[Y/n] ').upper().strip()
+        bj_trainer(play_game)
 
 main()
