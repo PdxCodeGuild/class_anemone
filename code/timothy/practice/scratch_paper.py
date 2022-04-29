c89775b85725215fca9612fad9078b30d516edc0
Timothy Johnson <johnsonandtimothy@gmail.com>
2022-04-29 16:32:29 -0700

    timothy/practice update

---
+++ code/timothy/practice/scratch_paper.py
@@ -0,0 +1,19 @@
+
+
+
+# def go_hiking(weather):
+#     def weather(a, b):
+#         if a == 'spry' and b == 'sunny':
+#             return True
+#         elif a == 'tired'
+#     if weather() == True:
+#         print(True)
+
+def double_digit(num):
+    num = int()
+    tens_digit = num//10
+    hundreds_digit = num//100
+    print(tens_digit)
+    print(hundreds_digit)
+
+double_digit(111)
