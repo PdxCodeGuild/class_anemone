a7b14d442073c64805a65a3d9d2a3ad78711ad8c
ICF19 <icfearn92@outlook.com>
2022-04-29 16:36:01 -0700

    lab 5 in progress

--- code/isaac/lab05.py
+++ code/isaac/lab05.py
@@ -1,5 +1,13 @@
 '''Pick 6'''
 import random
 
+number_pick = []
+
+for i in range(6):
+    aiChoice = random.randint(1, 99)
+    number_pick.append(aiChoice)
+    print(aiChoice)
 
 
+
+def pick6():
