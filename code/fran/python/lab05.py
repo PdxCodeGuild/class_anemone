36f9c1f8c5ff07fdeb161b441903d4306dc93dfe
Frances Kappes <fekmacbookpro2019@Feks-MacBook-Pro-2019.local>
2022-04-29 11:54:29 -0700

    def_num_matches and beginning of main looplogic

--- code/fran/python/lab05.py
+++ code/fran/python/lab05.py
@@ -11,6 +11,7 @@ def pick6():
     nums = []
 
     i = 0
+
     while i < 6:
         nums.append(random.randint(1,99))
         i += 1
@@ -18,21 +19,57 @@ def pick6():
 
 # Define num_matches function, which determines the number of matches
 #   between the winning numbers and a lottery ticket
-def num_matches():
-    return SOMETHING
+def num_matches(lottery_ticket, winning_ticket):
+    # Define and initialize matches counter
+    matches = 0
 
+    # Compare the lottery ticket numbers to the winning ticket numbers
+    # To be a match, numbers in the same position for both tickets must match 
+    i = 0
+    
+    while i < 6:
+        if int(lottery_ticket[i]) == int(winning_ticket[i]):
+            matches += 1
+
+        i += 1
+
+    return matches
+
+# TEST num_matches()
+#lottery_ticket = pick6()
+#winning_ticket = pick6()
+
+lottery_ticket = [1,2,3,4,5,6]
+winning_ticket = [5,99,2,3,5,76]
+
+print(f"lottery ticket: {lottery_ticket}")
+print(f"winning_ticket: {winning_ticket}")
+
+matches = num_matches(lottery_ticket, winning_ticket)
+print(f"matches: {matches}")
+
+# END TEST num_matches()
 
 # Define and initialize balance variable
-balance = 0
+#balance = 0
 
 # Generate the winning ticket. This will be a list of numbers.
-winning_ticket = pick6()
+#winning_ticket = pick6()
 
 # Main loop. This loop will run 100,000 times. Each loop will do a number of things:
 #   Generate a lottery ticket and compare the winning numbers to the lottery ticket.
 #   Subtract 2 from the balance (you bought a $2 lottery ticket)
 #   Find out how many numbers match
 #   Add to your balance the winnings from your matches
+
+i = 0
+
+while i < 100000:
+
+    lottery_ticket = pick6()
+
+    matches = num_matches(lottery_ticket, winning_ticket)
+
 
 
 
