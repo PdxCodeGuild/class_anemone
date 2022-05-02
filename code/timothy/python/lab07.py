471035c06e5459cc9f2380dfcc8b14aad09357d5
Timothy Johnson <johnsonandtimothy@gmail.com>
2022-05-02 16:32:36 -0700

    timothy/lab07

---
+++ code/timothy/python/lab07.py
@@ -0,0 +1,8 @@
+# ....Lab 07 Version 1
+
+def rot13(message):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    alphabet_rot = 'nopqrstuvwxyzabcdefghijklm'
+    for letter in range(alphabet[i]):
+        i = alphabet.find(letter)
+    
