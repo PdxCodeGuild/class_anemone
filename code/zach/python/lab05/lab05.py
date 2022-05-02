e3f7026fd20c8edff980e1f54a8495e5c4e6a081
Zach-EE <zachholloway.n@gmail.com>
2022-05-01 22:36:01 -0400

    got the pick6() and num_matches() functioning as intended

--- code/zach/python/lab05/lab05.py
+++ code/zach/python/lab05/lab05.py
@@ -1,4 +1,27 @@
 import random
 
 def pick6():
+    numbers = []
+    for i in range(0,6):
+        numbers.append(random.randint(1,99))
     
+    return numbers
+
+def num_matches(winning, ticket):
+    matches = 0
+    for i in range(len(winning)):
+        if winning[i] == ticket[i]:
+            matches += 1
+    return matches
+
+
+
+def main():
+    winning = pick6()
+    ticket = pick6()
+
+    wins = num_matches(winning, ticket)
+
+    print(winning, ticket, wins)
+
+main()
