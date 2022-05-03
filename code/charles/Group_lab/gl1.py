a2b0833df2dd0265149e16b1222aca5832d58efd
Adeeon <cgt10dt@gmail.com>
2022-05-03 12:53:38 -0500

    fjc/gl1

---
+++ code/charles/Group_lab/gl1.py
@@ -0,0 +1,18 @@
+# dictonary to track the jackolopes
+jackodict = { 1: 0, 2: 0}
+# Fucnction for testing age
+def old_age_test(age):
+    old = {}
+    i = 1
+    if i in sum(len(list(age))):
+        
+
+    return old
+
+
+# Function for reproducing
+# Kill Switch function
+# 
+
+# While sum(len(list(jackodict))) < 1000:
+#       pop = 
