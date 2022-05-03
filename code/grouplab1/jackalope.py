d2cb906f3cf14aa14bfb55230279279abfef451c
Timothy Johnson <johnsonandtimothy@gmail.com>
2022-05-03 10:53:57 -0700

    update

--- code/grouplab1/jackalope.py
+++ code/grouplab1/jackalope.py
@@ -7,17 +7,40 @@ population = {
     "2": 0,
 }
 """create a while loop to fill the dictionary with tuples representing individual jackalopes with pop id and age as value pair"""
-x = 0
+
+def birth(x):
+    for key in population:
+        if population[key] >=4:
+            population[-1] += 1
+        print(population)
+
 def life_cycle(population):
+    x = 0
     while x < 11:
         for key in population:
             population[key] += 1
             print(type(population[key]),(population[key]))
-
+        birth(population)
         x += 1
         print(population)
     print(population)
+
+life_cycle(population)
 """"""
+population = 0
+
+def birth():
+    for key in population:
+        if population[key] >=4:
+            population[-1] += 1
+        print(population)
+
+
+life_cycle(population)
+
+
+
+
 
 
 
