069db50e836388c52670eceda2d9eb5bed762478
avocado-jc <cavada.jonathon@gmail.com>
2022-05-03 15:50:25 -0700

    lab04 update

--- code/cavada/python/lab04.py
+++ code/cavada/python/lab04.py
@@ -1,7 +1,9 @@
 """lab 4"""
 print("""welcome to 'blackjack' advisor-bot: 
-random hand generating...
 """)
+user = input("Input any key to see your hand: ")
+
+print("random hand generating...""")
 x = 0 # aesthetic addition to simulate loading process for 
 while x < 10000000:
     x += 1
