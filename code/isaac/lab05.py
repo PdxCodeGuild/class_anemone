6197d46030829617b8f929888c22d9a101b2bfa2
ICF19 <icfearn92@outlook.com>
2022-05-03 14:45:56 -0700

    lab 5 complete

--- code/isaac/lab05.py
+++ code/isaac/lab05.py
@@ -2,58 +2,38 @@
 import random
 
 # Generate a list of 6 random numbers representing the winning tickets
-
 def pick6():
     winning_tickets = []
     for i in range(6):
         win_nums = random.randint(1, 99)
         winning_tickets.append(win_nums)
-    print(winning_tickets)
+    return winning_tickets
+
+def num_matches(winning, ticket):
+    payout = [0, 4, 7, 100, 50000, 1000000, 25000000] # price per winning match
+    matches = 0
+    for i in range(len(ticket)):
+        if winning[i] == ticket[i]: # checking to see if winning and ticket match
+            matches += 1
+    return payout[matches]
+
+# assing pick6() to a variable.  Will be used in a for loop
+winning = pick6()
+balance = 0
+
+for m in range(100_000): # loop 100,000 times
+
+    ticket = pick6()
+    print(ticket)
+    balance -= 2
+    payout = num_matches(winning, ticket)
+    balance += payout
+
+# print out both balance and winning tickets
+print(winning)
+print(balance)
 
 
-# pick6()
-
-# Start your balance at 0
-my_ticket_balance = 0
-
-
-# Loop 100,000 times, for each loop:
-winning_tickets = []
-for i in range(6):
-    win_nums = random.randint(1, 99)
-    winning_tickets.append(win_nums)
-print(winning_tickets)
-for m in range(100,000):
-
-
-
-# Generate a list of 6 random numbers representing the ticket
-my_ticket = []
-for i in range(6):
-    my_nums = random.randint(1, 99)
-    my_ticket.append(my_nums)
-print(my_ticket)
-
-
-
-# Subtract 2 from your balance (you bought a ticket)
-total = my_ticket_balance - 2
-print(my_ticket, winning_tickets)
-
-
-# Find how many numbers match
-for numbers in winning_tickets:
-    if my_ticket[numbers] == winning_tickets[numbers]:
-        matches = my_ticket.count() + winning_tickets.count()
-        print(matches)
-
-
-
-# Add to your balance the winnings from your matches
-
-
-
-# After the loop, print the final balance
 
 
 
