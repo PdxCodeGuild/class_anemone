c00abd7b05a854ea71788378b87ed05a91977a4b
Gi-bi <stanjessgrubbs@gmail.com>
2022-05-03 16:02:25 -0700

    lab03 revision done

--- code/jessica/lab03/lab03v2.py
+++ code/jessica/lab03/lab03v2.py
@@ -2,7 +2,7 @@
 
 
 
-number_words_less = {0:'zero', 1:'one', 2: 'two', 3: 'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine'}
+ones = {0:'zero', 1:'one', 2: 'two', 3: 'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine'}
 
 teens = {10:'ten', 11:'eleven', 12:'twelve', 13:'thirteen', 14:'fourteen', 15:'fifteen', 16:'sixteen', 17:'seventeen', 18:'eighteen', 19:'nineteen'}
 
@@ -22,30 +22,21 @@ hun_digit = user_input%1000//100
 
 #if statements with ranges and dictionary [ is user input ]
 if user_input <= 9:
-    print(f"{number_words_less[ones_digit]}")
+    print(f"{ones[ones_digit]}")
 elif user_input >= 10 and user_input <= 19:
     print(f"{teens[user_input]}")
 #-----------------------------------------------------
-elif user_input >= 20 and user_input < 99:
-    print(f"{tens[tens_digit]}")
-    if user_input[1] == 0:
-        print(tens.keys(user_input))
-elif user_input > 99 and user_input < 1000:
-    print(f"{hundreds[hun_digit]}-{tens[tens_digit]}-{number_words_less[ones_digit]}")
+elif user_input >= 20 and user_input <= 99:
+    if ones_digit != 0:
+        print(f"{tens[tens_digit]}-{ones[ones_digit]}")
+    else:
+        print(f"{tens[tens_digit]}")
 
-"""
-if tens_digit == 0:
-    phrase = ones[ones_digit]
-elif tens_digit == 1:
-    phrase = teens[ones_digit]
-elif ones_digit == 0:
-    phrase = tens[tens_digit]
-elif tens_digit > 1:
-    phrase = tens[tens_digit] + '-' + ones[ones_digit]
+elif user_input >= 100:
+    if tens_digit == 0 and ones_digit == 0:
+        print(f"{hundreds[hun_digit]}")
+    elif ones_digit != 0:
+        print(f"{hundreds[hun_digit]} {tens[tens_digit]} {ones[ones_digit]}")
+    elif ones_digit == 0:
+        print(f"{hundreds[hun_digit]} {tens[tens_digit]}")
 
-if hundreds_digit > 0:
-    phrase = hundreds[hundreds_digit] + ' and ' + phrase
-
-if ones_digit == 0 and tens_digit == 0:
-        phrase = hundreds[hundreds_digit]
-"""
