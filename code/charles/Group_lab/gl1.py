c636ad4183382f2ca1a42b29a87aaa7d2f30db17
Adeeon <cgt10dt@gmail.com>
2022-05-03 17:06:59 -0500

    gl1

--- code/charles/Group_lab/gl1.py
+++ code/charles/Group_lab/gl1.py
@@ -1,56 +1,130 @@
-# dictonary to track the jackolopes
-jackodict = { 1: 0, 2: 0}
+jacklist = [0, 0]
 
 
-def grow(age):                                              # Fucnction for testing age
-    i = 1                                                   # Jackodict key 1
-    for i in age:
-        if i in age and age[i] in range(0, 11):
-            age[i] += 1
+
+
+def grow(age):                                              # Fucnction for testing age# Kill Switch function
+    i = 0                                                  # Jackodict key 1
+    for i in range(0, len(age)):
+        age[i] += 1
     return age
 
 def told_test(age):
-    told= {}
-    i = 1                                                   # Jackodict key 1
-    for i in age:
-        if age[i] >= 10:
-            told.update(age)
-    return told
+    i = 0                                                  # Jackodict key 1
+    while i < len(age):
+        if age[i] == 10:
+            del age[i]
+        i += 1
+    return age
 
 def repop(age):
-    i = 1                                                   # Jackodict key 1
-    mates = 0
-    ospring = 0
-    if i in age and age[i] in range(4, 9):
-        mates += len(age) // 2
-        ospring += mates * 2
-    return ospring
+    for i in range(0, len(age)):
+        if age[i] in range(4, 9):
+            jacklist.append(0)
+    return 
 
 
 
 
-
-
-
-print(repop(jackodict))
-
-
-
-year = 0
-while len(jackodict) < 1000:
+year = 1
+while len(jacklist) < 1000:
+    
+    grow(jacklist)
+    told_test(jacklist)
+    repop(jacklist)
+    print(year, len(jacklist))
     year += 1
-    i = len(jackodict) 
-    i = i * i 
-    print(i)
-    if i not in jackodict and len(jackodict) < 1000:
+print(year, len(jacklist))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# jackodict = { 1: 0, 2: 0}                                   # dictonary to track the jackolopes
+
+
+# def grow(age):                                              # Fucnction for testing age# Kill Switch function
+#     i = 1                                                   # Jackodict key 1
+#     for i in age:
+#         if i in age and age[i] in range(0, 11):
+#             age[i] += 1
+#     return age
+
+# def told_test(age):
+#     i = min(age.keys())                                                   # Jackodict key 1
+#     print(age.keys())
+#     while i <= max(age.keys()):
+#         if age[i] == 10:
+#             del age[i]
+#         i += 1
+#     return age
+
+# def repop(age):
+#     i = min(age.keys())                                                 # Jackodict key 1
+#     mates = 0
+#     ospring = 0
+#     if i in age and age[i] in range(4, 9):
+#         mates += len(age) // 2
+#         ospring += mates * 2
+#     return ospring
+
+
+# def name(age, news):
+#     babies = {}
+#     maxk = max(age.keys())
+#     i = 1
+#     for i in range(0, news + 1):
+#             nextk = maxk + 1 + i
+#             babies[nextk] = 0
+#             i += 1
         
-        ospring = repop(jackodict)
-        while i <= ospring:           
-#                       
+#     return babies
+
+# # print(told_test(jackodict))
+
+# year = 0
+# while len(jackodict) < 1000:
+    
+#     grow(jackodict)
+#     told_test(jackodict)
+#     name(jackodict, repop(jackodict))
+#     print(year, len(jackodict))
+#     year += 1
+# print(year, len(jackodict))
     
 
 # Function for reproducing
-# Kill Switch function
+
 # 
 
 # While sum(len(list(jackodict))) < 1000:
