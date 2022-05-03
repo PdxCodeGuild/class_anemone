7d905b43df4174a12a34b8a626520ba901d5b035
Timothy Johnson <johnsonandtimothy@gmail.com>
2022-05-03 11:32:38 -0700

    update

--- code/grouplab1/jackalope.py
+++ code/grouplab1/jackalope.py
@@ -1,67 +1,45 @@
 
-
 """create a dictionary list for population to exist"""
-
-population = {
-    "1": 0,
-    "2": 0,
-}
+# population = {
+#     "1": 0,
+#     "2": 0,
+# }
 """create a while loop to fill the dictionary with tuples representing individual jackalopes with pop id and age as value pair"""
-
-def birth(x):
-    for key in population:
-        if population[key] >=4:
-            population[-1] += 1
-        print(population)
-
-def life_cycle(population):
-    x = 0
-    while x < 11:
-        for key in population:
-            population[key] += 1
-            print(type(population[key]),(population[key]))
-        birth(population)
-        x += 1
-        print(population)
-    print(population)
-
-life_cycle(population)
+# def birth(population, counter):
+#     for key in population:
+#         if population[key] >=4:
+#             population[counter+1] = 0
+       
+#         print(population)
+jacks = [0,0]
+# def life_cycle(population):
+x = 0
+while len(jacks) <1000:
+    for i in jacks:
+        jacks[i] += 1
+        print(type(jacks[i]),jacks[i])
+    counter = len(jacks)
+    x += 1
+    print(jacks)
+    print(jacks)
+# life_cycle(population)
 """"""
 population = 0
-
 def birth():
     for key in population:
         if population[key] >=4:
             population[-1] += 1
         print(population)
-
-
-life_cycle(population)
-
-
-
-
-
-
-
-
-
+# life_cycle(population)
+# def life_cycle(population):
+#     pop = len(list(population))
+#     age = 0
+#     while True:
+#         age = age + 1
+#         if age > 4:
+#             pop + 1
+#         print(pop)
 """"""
-
-
-
-
-
 """"""
-
-
-
-
-
 """"""
-
-
-
-
-
 """"""
