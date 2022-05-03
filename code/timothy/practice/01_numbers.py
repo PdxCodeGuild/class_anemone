022d060f9d5c50438ab0a58ff65ef7bb4166ecd1
Timothy Johnson <johnsonandtimothy@gmail.com>
2022-05-03 14:39:08 -0700

    some practice

--- code/timothy/practice/01_numbers.py
+++ code/timothy/practice/01_numbers.py
@@ -1,18 +1,59 @@
-# Practice 1: Numbers & Arithmetic
-# Copy and paste this file into your own "01_numbers.py"
-# Fill in the code for each of the functions
-# Run the tests by typing "pytest 01_numbers.py"
+# # Practice 1: Numbers & Arithmetic
+# # Copy and paste this file into your own "01_numbers.py"
+# # Fill in the code for each of the functions
+# # Run the tests by typing "pytest 01_numbers.py"
+
+# # Is Even
+# # Write a function returns whether a number is even or odd (hint, compare a/2 and a//2, or use a%2)
+
+# from ast import Num
+
+
+# def is_even(a):
+#     if a % 2 == 0:
+#         return(True)
+#     else: return(False)
+
+# def test_is_even():
+#     assert is_even(5) == False
+#     assert is_even(6) == True
+
+# # Ones Digit
+# # Write a function that returns the ones digit of a number
+
+# def ones_digit(num):
+#     num = num %10
+#     return num
+
+# def test_ones_digit():
+#     assert ones_digit(3) == 3
+#     assert ones_digit(56) == 6
+#     assert ones_digit(812) == 2
+
+
+# # Percentage
+# # Write a function that takes two integers, a value and a maximum, and returns a string representing the percentage as an integer
+
+# def percentage(v, max):
+#     percentage = int(v/max * 100)
+#     return str(f"{percentage}%")
+
+# def test_precentage():
+#     assert percentage(1, 10) == '10%'
+#     assert percentage(600, 1200) == '50%'
+#     assert percentage(1, 3) == '33%'
+
+
+# Do It Again
 
 # Is Even
 # Write a function returns whether a number is even or odd (hint, compare a/2 and a//2, or use a%2)
 
-from ast import Num
-
-
 def is_even(a):
-    if a % 2 == 0:
-        return(True)
-    else: return(False)
+    if a%2 == 0:
+        return True
+    else:
+        return False
 
 def test_is_even():
     assert is_even(5) == False
@@ -22,7 +63,7 @@ def test_is_even():
 # Write a function that returns the ones digit of a number
 
 def ones_digit(num):
-    num = num %10
+    num = num%10
     return num
 
 def test_ones_digit():
@@ -35,8 +76,8 @@ def test_ones_digit():
 # Write a function that takes two integers, a value and a maximum, and returns a string representing the percentage as an integer
 
 def percentage(v, max):
-    percentage = int(v/max * 100)
-    return str(f"{percentage}%")
+    percent = int((v/max)*100)
+    return str(percent) + "%"
 
 def test_precentage():
     assert percentage(1, 10) == '10%'
--- code/timothy/practice/scratch_paper.py
+++ code/timothy/practice/scratch_paper.py
@@ -9,11 +9,23 @@
 #     if weather() == True:
 #         print(True)
 
-def double_digit(num):
-    num = int()
-    tens_digit = num//10
-    hundreds_digit = num//100
-    print(tens_digit)
-    print(hundreds_digit)
+# def double_digit(num):
+#     num = int()
+#     tens_digit = num//10
+#     hundreds_digit = num//100
+#     print(tens_digit)
+#     print(hundreds_digit)
 
-double_digit(111)
+# double_digit(111)
+
+# def is_even(a):
+#     if a%2 == 0:
+#         print("That number is even.")
+
+# is_even(7)
+
+def ones_digit(num):
+    num = num%10
+    print(num)
+
+ones_digit(136)
