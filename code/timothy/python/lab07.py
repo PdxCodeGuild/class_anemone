4359dc0369a4137a8cd6674c15e9707b713b9674
Timothy Johnson <johnsonandtimothy@gmail.com>
2022-05-02 17:18:25 -0700

    timothylab07 complete

--- code/timothy/python/lab07.py
+++ code/timothy/python/lab07.py
@@ -1,8 +1,18 @@
 # ....Lab 07 Version 1
 
-def rot13(message):
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    alphabet_rot = 'nopqrstuvwxyzabcdefghijklm'
-    for letter in range(alphabet[i]):
-        i = alphabet.find(letter)
-    
+# def rot13(message):
+#     alphabet = 'abcdefghijklmnopqrstuvwxyz'
+#     alphabet_rot = 'nopqrstuvwxyzabcdefghijklm'
+#     for letter in range(alphabet[i]):
+#         i = alphabet.find(letter)
+
+
+def rot13():
+    alph_rot = {'a':'n', 'b':'o', 'c':'p', 'd':'q', 'e':'r', 'f':'s', 'g':'t', 'h':'u', 'i':'v', 'j':'w', 'k':'x', 'l':'y', 'm':'z', 
+    'n':'a', 'o':'b', 'p':'c', 'q':'d', 'r':'e', 's':'f', 't':'g', 'u':'h', 'v':'i', 'w':'j', 'x':'k', 'y':'l', 'z':'m', ' ': ' ' }
+    user_input = input("Type your messsage to be encrypted: ")
+    list(user_input)
+    for letter in user_input:
+        print(alph_rot[letter], end= '')
+
+rot13()
