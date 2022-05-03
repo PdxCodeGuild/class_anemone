b4a51334e7b70d7953a8acb1c87060006a29b76f
avocado-jc <cavada.jonathon@gmail.com>
2022-05-03 10:36:15 -0700

    update

--- code/grouplab1/jackalope.py
+++ code/grouplab1/jackalope.py
@@ -1,17 +1,21 @@
 
 
-
 """create a dictionary list for population to exist"""
 
-
-
-
+population = {
+    "1": 0,
+    "2": 0,
+}
 """create a while loop to fill the dictionary with tuples representing individual jackalopes with pop id and age as value pair"""
-
-
-
-
-
+x = 0
+while x < 11:
+    for key in population:
+        population[key] += 1
+        print(type(population[key]),(population[key]))
+        
+    x += 1
+    print(population)
+print(population)
 """"""
 
 
