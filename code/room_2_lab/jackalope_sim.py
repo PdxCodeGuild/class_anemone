02dcab82ddcf87e2adea6b110adfcf9849609355
ICF19 <icfearn92@outlook.com>
2022-05-03 11:22:36 -0700

    group 2 lab jackalope

--- code/room_2_lab/jackalope_sim.py
+++ code/room_2_lab/jackalope_sim.py
@@ -3,3 +3,23 @@
 
 population = 2
 
+jackalope_1 = [5]
+jackalope_2 = [9]
+new_born = []
+
+def check_age(jackalope_1, jackalope_2):
+    for age in jackalope_1 and jackalope_2:
+        if age > 4 and age < 8:
+            new_born.append(0)
+
+            print("Breedable! YAY!")
+        elif age >= 9:
+            print("Dead")
+        else:
+            print("Wait a while")
+    return jackalope_1, jackalope_2
+
+check_age(jackalope_1, jackalope_2)
+print(new_born)
+
+
