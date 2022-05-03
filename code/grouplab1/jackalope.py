2498c4511c4cbc12d1eb24d905e407761255c94a
Timothy Johnson <johnsonandtimothy@gmail.com>
2022-05-03 11:55:45 -0700

    update

--- code/grouplab1/jackalope.py
+++ code/grouplab1/jackalope.py
@@ -1,45 +1,60 @@
 
-"""create a dictionary list for population to exist"""
-# population = {
-#     "1": 0,
-#     "2": 0,
-# }
-"""create a while loop to fill the dictionary with tuples representing individual jackalopes with pop id and age as value pair"""
-# def birth(population, counter):
+# """create a dictionary list for population to exist"""
+# # population = {
+# #     "1": 0,
+# #     "2": 0,
+# # }
+# """create a while loop to fill the dictionary with tuples representing individual jackalopes with pop id and age as value pair"""
+# # def birth(population, counter):
+# #     for key in population:
+# #         if population[key] >=4:
+# #             population[counter+1] = 0
+       
+# #         print(population)
+# jacks = [0,0]
+# # def life_cycle(population):
+# x = 0
+# while len(jacks) <1000:
+#     for i in jacks:
+#         jacks[i] += 1
+#         print(type(jacks[i]),jacks[i])
+#     counter = len(jacks)
+#     x += 1
+#     print(jacks)
+#     print(jacks)
+# # life_cycle(population)
+# """"""
+# population = 0
+# def birth():
 #     for key in population:
 #         if population[key] >=4:
-#             population[counter+1] = 0
-       
+#             population[-1] += 1
 #         print(population)
+# # life_cycle(population)
+# # def life_cycle(population):
+# #     pop = len(list(population))
+# #     age = 0
+# #     while True:
+# #         age = age + 1
+# #         if age > 4:
+# #             pop + 1
+# #         print(pop)
+# """"""
+# """"""
+# """"""
+# """"""
+
 jacks = [0,0]
-# def life_cycle(population):
-x = 0
+years = 0
 while len(jacks) <1000:
-    for i in jacks:
-        jacks[i] += 1
-        print(type(jacks[i]),jacks[i])
+    for jack in jacks:
+        years += 1
+        print(jacks)
+        if jack >= 4 and jack <= 8:
+            jacks.append(0)
+            
+        print(jacks[jack],jacks[jack])
     counter = len(jacks)
-    x += 1
-    print(jacks)
-    print(jacks)
-# life_cycle(population)
-""""""
-population = 0
-def birth():
-    for key in population:
-        if population[key] >=4:
-            population[-1] += 1
-        print(population)
-# life_cycle(population)
-# def life_cycle(population):
-#     pop = len(list(population))
-#     age = 0
-#     while True:
-#         age = age + 1
-#         if age > 4:
-#             pop + 1
-#         print(pop)
-""""""
-""""""
-""""""
-""""""
+    years += 1
+    # print(jacks)
+    # print(jacks)
