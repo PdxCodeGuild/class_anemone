b2141df4a2f3ab7432474702d916d552ad9a0ebe
Gi-bi <stanjessgrubbs@gmail.com>
2022-05-04 15:22:14 -0700

    lab06

--- code/jessica/lab06/lab06.py
+++ code/jessica/lab06/lab06.py
@@ -1,18 +1,50 @@
-credit_card = "4 5 5 6 7 3 7 5 8 6 8 9 9 8 5 5"
-credit_card_split = credit_card.split()
-credit_card_map = map(int, credit_card_split)
-list_of_integers = list(credit_card_map)
-print(list_of_integers)
+#Step 1 convert string to list in a function
 
-#Removing check digit
-list_of_integers.pop()
-print(list_of_integers)
+user_input = input('Enter your credit card number ')
 
-#Reversing the list
-list_of_integers.reverse()
-print(list_of_integers)
+credit_card_list = [int(num) for num in user_input]
+print(credit_card_list)
 
-#Double every other element in the reversed list
-reversed_list = [ele + ele for ele in list_of_integers]
-print(reversed_list)
+##Slice off check digit
+check_digit = credit_card_list.pop()
+print(check_digit) #5
+
+
+# #Reverse car list
+credit_card_list.reverse()
+print(credit_card_list)
+
+##Double every other number                       
+double_card = [2*n if i%2 == 0 else n for i,n in enumerate(credit_card_list)] 
+# double_card = []
+# for i,num in enumerate(credit_card_list):
+#     if i%2 == 0:
+#         double_card.append(2*num)
+#     else:
+#         double_card.append(num)
+
+print(double_card)
+
+over_nine = []
+for i,num in enumerate(double_card):
+    if num > 9:
+        over_nine.append(num-9)
+    else:
+        over_nine.append(num)
+
+print(over_nine)
+
+sum_of_nums = sum(over_nine)
+
+print(sum_of_nums)
+
+check_value = str(sum_of_nums)[1] #5
+print(check_value)
+
+
+#str cannot be equal to an int. I needed to convert the check value back to int. 
+if check_digit == int(check_value):
+    print('Valid card')
+else:
+    print('Try Again')
 
