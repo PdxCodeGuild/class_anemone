fb4554c9a38555c8e223830e2dfabf6b0ad00f1c
JasmineDee <jasminee.davies1@gmail.com>
2022-05-03 20:14:47 -0700

    Lab 5v2 created

---
+++ code/jasmine/Python-Labs/mob1.py
@@ -0,0 +1,39 @@
+
+
+# population list
+
+jack = [0,0]
+
+# filter the years of jacks with loop 
+
+years = 0
+
+#produce at 4-8 and die at 10 
+#remove from list at age 10 
+
+while len(jack) < 1000:
+    # baby making loop
+    for x in jack:
+        if 4 <= x <= 8:
+            jack.append(0)
+
+    # loop to off the jack
+    for i in range(len(jack)-1,-1):
+        if jack[i] == 10:
+            jack.pop(i)
+    
+
+    # loop to age the jack
+    for i in range(len(jack)):
+        jack[i] += 1
+    print(len(jack), years)  
+
+
+
+    years += 1
+
+    # for x in jack:
+    #     if jack
+
+print(f'Our population is {len(jack)} after {years} years')
+
