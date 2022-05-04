6559e6ab37a947bd2a22840acf2953cf06bcc03d
Ollie's Macbook Pro <ojc@Ollies-MacBook-Pro.local>
2022-05-04 14:48:08 -0700

    Lab 06 - done

--- code/ollie/python/lab-06.py
+++ code/ollie/python/lab-06.py
@@ -1,23 +1,19 @@
 ' Lab 6 - Ollie Callanan '
 
-card_num = input("Enter credit card number: ")
-# nums = [int(num) for num in str(card_num)]
-# check_num = nums.pop()
-
-def validate(number):
-    nums = [int(num) for num in str(number)]
+def validate():
+    card_num = input("Enter credit card number: ")
+    nums = [int(num) for num in str(card_num)]
     check_num = nums.pop()
-    nums = nums.reverse()
-    nums = [2 * num for num in nums[::2]]
-    for num in nums:
-        if num > 9:
-            num = num - 9
-        else:
-            return num
+    nums == nums.reverse()
+    for i in range(len(nums)):
+        if i % 2 == 0:
+            nums[i] = nums[i] * 2
+    nums = [i - 9 if i > 9 else i for i in nums]
     ones_digit = sum(nums) % 10
     if check_num == ones_digit:
         print("Valid")
     else:
         print("Invalid")
+    # return card_num, nums, sum(nums), ones_digit, check_num
 
-validate(card_num)
+validate()
