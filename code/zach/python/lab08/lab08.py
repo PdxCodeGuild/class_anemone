8d370645db1684a5e3898707769b2a67943a7e9d
Zach-EE <zachholloway.n@gmail.com>
2022-05-04 17:35:50 -0400

    lab08 initial commit

---
+++ code/zach/python/lab08/lab08.py
@@ -0,0 +1,16 @@
+data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
+def peaks():
+    pass
+
+def valleys():
+    pass
+
+def peaks_and_valleys():
+    pass
+
+peaks= []
+for num in range(len(data)):
+     if num>= data[num-1] and data[num+1]:
+
+        peaks.append()
+print(peaks)
