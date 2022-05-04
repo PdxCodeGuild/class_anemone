92a2afb824f1333e28648395a6052597d53466ff
JasmineDee <jasminee.davies1@gmail.com>
2022-05-04 14:59:50 -0700

    Lab 6 in progress

--- code/jasmine/Python-Labs/lab6.py
+++ code/jasmine/Python-Labs/lab6.py
@@ -1,4 +1,27 @@
   ##Credit Card Validator
+card_num = []
+numbers = []
+
+def credit_card_validator(card_num):
+   
+    card_num = (input('Enter your card number: '))
+
+    num_list = list(map(int,card_num.split(",")))
+    print(num_list)
+
+    check_digit = num_list.pop()
+    print(check_digit)
+
+    # check_digit.reverse()
+    # print(num_list)
+
+    return(card_num)
+
+card_check = credit_card_validator(card_num)
+print(card_check)
+
+
+
 
 
 
