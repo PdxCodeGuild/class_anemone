a5a93a8e39c592ee09bf37643499ad08bc10b602
ICF19 <icfearn92@outlook.com>
2022-05-04 11:41:35 -0700

    lab 6 in progress

--- code/isaac/lab06.py
+++ code/isaac/lab06.py
@@ -6,7 +6,6 @@ def credit_card_validator():
     other_number = card_number
     other_number.reverse() # reverse list
     doubled_number = [2 * x for x in other_number[::2]] # double every other number
-    print(other_number)
-    print(doubled_number)
+    return doubled_number
 
 credit_card_validator()
