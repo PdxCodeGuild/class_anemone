21d47f44d966c2b6634f11a3fc050e4dc68b506b
Zach-EE <zachholloway.n@gmail.com>
2022-05-04 16:56:33 -0400

    have version one basically functioning adding extra features

--- code/zach/python/lab07/lab07.py
+++ code/zach/python/lab07/lab07.py
@@ -1,3 +1,24 @@
-english = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+def encryption(string, function, rotation_num):
+    english = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-user_input = input('input desired encryption')
+    orig_list = list(string)
+    rot_string = []
+    for char in orig_list:
+        if char in english:
+            if english.index(char) <= 12:
+                rot_string.append(english[english.index(char)+13])
+            else:
+                rot_string.append(english[english.index(char)-13])
+    
+    print(rot_string)
+
+
+
+
+
+def main():
+    user_input = str(input('input desired encryption')).lower().strip()
+    function_input = str(input(''))
+    rotation_input = str(input())
+    encryption(user_input)
+main()
