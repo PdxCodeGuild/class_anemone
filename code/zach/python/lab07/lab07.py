47db076ac2cab311632d2893c2f012e4c3d7d39c
Zach-EE <zachholloway.n@gmail.com>
2022-05-04 17:15:10 -0400

    have version2 up and going I think/ known issue getting function working with rotations that take list index out of range

--- code/zach/python/lab07/lab07.py
+++ code/zach/python/lab07/lab07.py
@@ -3,22 +3,30 @@ def encryption(string, function, rotation_num):
 
     orig_list = list(string)
     rot_string = []
-    for char in orig_list:
-        if char in english:
-            if english.index(char) <= 12:
-                rot_string.append(english[english.index(char)+13])
-            else:
-                rot_string.append(english[english.index(char)-13])
-    
-    print(rot_string)
+    if function[0] == 'e':
+        for char in orig_list:
+            if char in english:
+                if english.index(char) <= 13:
+                    rot_string.append(english[english.index(char)+rotation_num])
+                else:
+                    rot_string.append(english[english.index(char)-rotation_num])
+    else:
+        for char in orig_list:
+            if char in english:
+                if english.index(char) >= 13:
+                    rot_string.append(english[english.index(char)-rotation_num])
+                else:
+                    rot_string.append(english[english.index(char)+rotation_num])
+        
+    print('Output: ',str(rot_string))
 
 
 
 
 
 def main():
-    user_input = str(input('input desired encryption')).lower().strip()
-    function_input = str(input(''))
-    rotation_input = str(input())
-    encryption(user_input)
+    user_input = str(input('input desired encryption: ')).lower().strip()
+    function_input = str(input('encrypt/decrypt: ')).lower().strip()
+    rotation_input = int(input('number of rotations: '))
+    encryption(user_input, function_input, rotation_input)
 main()
