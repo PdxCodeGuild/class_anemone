c3b92eb3bc94e2feb553fdb0d0a1a5f137c6e8e1
Zach-EE <zachholloway.n@gmail.com>
2022-05-04 15:44:17 -0400

    cc validator completed and functioning as intended

--- code/zach/python/lab06/lab06.py
+++ code/zach/python/lab06/lab06.py
@@ -1,32 +1,39 @@
-test_number = '4556737586899855'
+test_cc = '4556737586899855'
 
 def cc_check(cc_number):
     # Convert the input string into list
-    cc_number = list(test_number)
-    print(f'ORGINAL: { cc_number }')
-    #* ORGINAL: ['4', '5', '5', '6', '7', '3', '7', '5', '8', '6', '8', '9', '9', '8', '5', '5']
-
+    cc_number = list(test_cc)
+    cc_original = cc_number
     # Slice off last digit, this is check_digit
-    check_digit = test_number[-1]
+    check_digit = test_cc[-1]
     cc_number.pop(-1)
     # Reverse the digits
     cc_number.reverse()
     # Double every other element in reversed list stating with first number
     # subtract 9 from numbers over nine
+    cc_doubled = cc_number[::2]
+    cc_normal = cc_number[1::2]
+    for number in cc_doubled:
+        number = int(number)*2
+        if number > 9:
+            number -= 9
+        cc_normal.append(number)
     # sum all values
-    cc_doubled = []
-    cc_check = []
     check_sum = 0
-    cc_number = map(lambda x : int(x)*2 , cc_number[::2])
+    for number in cc_normal:
+        check_sum += int(number)
         
     # take second digit off sum
     # if that matches the check digit whole card number is valid
-    print(cc_number) #* ['5', '8', '9', '9', '8', '6', '8', '5', '7', '3', '7', '6', '5', '5', '4']
-    print(cc_doubled, check_sum, check_digit)
- 
-    # if check_sum:
-    #     return print(True)
-    # else:
-    #     return print(False)
+    num_str = repr(check_sum)
+    if check_digit == num_str[-1]:
+        return True
+    else:
+        return False
+def main():
 
-cc_check(test_number)
+    if cc_check(test_cc):
+        print('CC Valid')
+    else:
+        print('CC Not Valid')
+main()
