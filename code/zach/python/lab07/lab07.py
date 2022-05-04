43d3b48c8b871b0983b5452e24fd3208ea97cdcf
Zach-EE <zachholloway.n@gmail.com>
2022-05-04 16:25:17 -0400

    initial commit

---
+++ code/zach/python/lab07/lab07.py
@@ -0,0 +1,3 @@
+english = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+user_input = input('input desired encryption')
