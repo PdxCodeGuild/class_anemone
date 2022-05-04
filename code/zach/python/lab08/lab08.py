414625a9605b51f623d9f6e1cb1b413258b96463
Zach-EE <zachholloway.n@gmail.com>
2022-05-04 18:54:46 -0400

    got version 1 of lab08 functioning as intended

--- code/zach/python/lab08/lab08.py
+++ code/zach/python/lab08/lab08.py
@@ -1,16 +1,35 @@
 data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
-def peaks():
-    pass
 
-def valleys():
-    pass
 
-def peaks_and_valleys():
-    pass
+def peaks(data):
+    max_num = 0
+    peaks = []
+    for num in range(1, len(data)-1):
+        max_num = (data[num] if data[num] > max_num else max_num)
+        if data[num] > data[num+1] and data[num] > data[num-1]:
+            peaks.append(num)
+    return peaks, max_num
 
-peaks= []
-for num in range(len(data)):
-     if num>= data[num-1] and data[num+1]:
 
-        peaks.append()
-print(peaks)
+def valleys(data_list):
+    valleys = []
+    for num in range(1, len(data)-1):
+        if data[num] < data[num+1] and data[num] < data[num-1]:
+            valleys.append(num)
+    return valleys
+
+
+def peaks_and_valleys(data):
+
+    peak_vals, max_int = peaks(data)
+    valley_vals = valleys(data)
+    poi = peak_vals + valley_vals
+    poi.sort()
+
+    return poi
+
+
+
+print('peaks: ', peaks(data)[0])
+print('valleys: ', valleys(data))
+print('peaks & valleys', peaks_and_valleys(data))
