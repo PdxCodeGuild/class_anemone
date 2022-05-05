a43146a6e505a027a777e4a57fc2d97c05a547b2
Zach-EE <zachholloway.n@gmail.com>
2022-05-05 18:09:50 -0400

    write_f() function created writing to .csv as intended

--- code/zach/python/lab10/lab10.py
+++ code/zach/python/lab10/lab10.py
@@ -1,9 +1,24 @@
 
 contacts = [
-    {'name': 'matthew', 'favorite fruit': 'blackberries', 'favorite color': 'orange'},{'name': 'matthew', 'favorite fruit': 'blackberries', 'favorite color': 'orange'},{'name': 'matthew', 'favorite fruit': 'blackberries', 'favorite color': 'orange'}
+    {'name': 'matthew', 'favorite fruit': 'blackberries', 'favorite color': 'orange'}, {'name': 'matthew', 'favorite fruit': 'blackberries',
+                                                                                        'favorite color': 'orange'}, {'name': 'matthew', 'favorite fruit': 'blackberries', 'favorite color': 'orange'}
 
 ]
+contact_org = ['name', 'favorite fruit', 'favorite_color']
+contact_info = [['matthew', 'blackberries', 'purple'], ['jill','bananas', 'purple']]
+#! write_f() will rewrite entire .csv file 
 
+def write_f():
+    with open('contacts.csv', 'w') as f:
+        results = []
+        for contact in contact_info:
+            for cat, info in zip(contact_org, contact):
+                print(cat,':', info)
+                #* 'name':'matthew','favorite fruit':'blackberries','favorite_color':'purple',....
+                f.writelines(f'\'{ cat }\':\'{ info }\',')
+            f.writelines('\n')
+        f.close()
+    return
 
 def main():
     # with open('contacts.csv', 'r') as f:
@@ -12,14 +27,9 @@ def main():
     #         words = line.split(',')
     #         results.append((words[0]), words[1:])
     # return
-    with open('contacts.csv', 'w') as f:
-        results = []
-        for contact in contacts:
-            for element in contact:
-                f.writelines(f'{ contact[element] } ,' )
-            f.writelines('\n')
-        f.close()
+    write_f()
     return
+
 
 if __name__ == '__main__':
     main()
