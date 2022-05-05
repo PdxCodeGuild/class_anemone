83ae42c0145108f754ba1bd6436385d8d29f7dea
ICF19 <icfearn92@outlook.com>
2022-05-05 10:55:39 -0700

    lab 7 in progress

---
+++ code/isaac/lab07.py
@@ -0,0 +1,10 @@
+'''ROT Ciphor'''
+# Create a ciphor that rotates 13 places from original string value
+
+message_letters = 'abcdefghijklmnopqrstuvwxyz'
+
+list_letters = []
+
+for letter in message_letters:
+    list_letters.append(message_letters)
+print(list_letters)
