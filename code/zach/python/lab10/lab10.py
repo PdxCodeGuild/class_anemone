dab7dc73fa4009d603a2822e40c6cdb5e2b08a5e
Zach-EE <zachholloway.n@gmail.com>
2022-05-05 17:33:21 -0400

    writing to csv file

--- code/zach/python/lab10/lab10.py
+++ code/zach/python/lab10/lab10.py
@@ -1,6 +1,24 @@
 
+contacts = [
+    {'name': 'matthew', 'favorite fruit': 'blackberries', 'favorite color': 'orange'},{'name': 'matthew', 'favorite fruit': 'blackberries', 'favorite color': 'orange'},{'name': 'matthew', 'favorite fruit': 'blackberries', 'favorite color': 'orange'}
+
+]
+
 
 def main():
+    # with open('contacts.csv', 'r') as f:
+    #     results = []
+    #     for line in f:
+    #         words = line.split(',')
+    #         results.append((words[0]), words[1:])
+    # return
+    with open('contacts.csv', 'w') as f:
+        results = []
+        for contact in contacts:
+            for element in contact:
+                f.writelines(f'{ contact[element] } ,' )
+            f.writelines('\n')
+        f.close()
     return
 
 if __name__ == '__main__':
