7b97c5e3044fe78a626037be4a574cc277c29ec8
ICF19 <icfearn92@outlook.com>
2022-05-05 12:13:00 -0700

    lab 7 in progress

--- code/isaac/lab07.py
+++ code/isaac/lab07.py
@@ -1,10 +1,3 @@
-'''ROT Ciphor'''
-# Create a ciphor that rotates 13 places from original string value
+'''ROT Cipher'''
+# Create a cipher function
 
-message_letters = 'abcdefghijklmnopqrstuvwxyz'
-
-list_letters = []
-
-for letter in message_letters:
-    list_letters.append(message_letters)
-print(list_letters)
