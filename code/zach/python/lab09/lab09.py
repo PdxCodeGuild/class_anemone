12dd7c4b5aceaade9d011ac421a842d54dda3d10
Zach-EE <zachholloway.n@gmail.com>
2022-05-05 14:34:44 -0400

    have ari_calc() functioning properly

--- code/zach/python/lab09/lab09.py
+++ code/zach/python/lab09/lab09.py
@@ -1,5 +1,22 @@
+from fileinput import filename
+from data import *
+import re
+import math
+
+
+def ari_calc():
+    text_file = open('gettysburg_address.txt')
+    text_data = text_file.read()
+    chars = len(re.findall('( *?[0-9a-zA-Z] *?)', text_data))
+    words = len(text_data.split())
+    sentences = len(re.split('[.?!]',text_data))-1 
+
+    ari_eq = math.floor(4.71*(chars/words) + 0.5*(words/sentences)-21.43)
+    return ari_eq
+
 def main():
-    return
+    ari_calc()
+
 
 if __name__ == '__main__':
     main()
