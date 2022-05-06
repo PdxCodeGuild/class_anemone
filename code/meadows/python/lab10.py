b7a27211f9c6c660a1950ea928fe6d37ab4a0a8d
jon meadows <jon_meadows@ymail.com>
2022-05-05 23:39:57 -0400

    lab10 v1 done

--- code/meadows/python/lab10.py
+++ code/meadows/python/lab10.py
@@ -1,28 +1,22 @@
 import math
-#-------------------------------------------------VERSION 1 ? ---------------------------------------------------------------#
-# with open('contacts.csv', 'r') as file:
-#     lines = file.read().split('\n')
-#     print(lines)
-#prints ['name,state,number', 'jon,ne,6556556555', 'sav,ne,5665665666', 'tim,ct,7887887888', '']
-
-names = []
-states = []
-numbers =[]
+#-------------------------------------------------VERSION 1 ---------------------------------------------------------------#
+contact = []
 play = True
 while play:
-    name = input('Name: ')
-    names.append(name)
-    state = input('state: ')
-    states.append(state)
+    contacts = {}
+    name = input('Name: ').lower()
+    contacts['name'] = name
+    state = input('state: ').lower()
+    contacts['state'] = state
     number = input(r'enter phone number: ')
-    numbers.append(number)
-    more = input('add more contacts? ( yes or no ): ')
+    contacts['number'] = number
+    contact.append(contacts)
+    more = input('add more contacts? ( yes or no ): ').lower()
     yes = ['yes','y','yeah','si']
-    no = ['no', 'nah','nay','nope']
+    no = ['no', 'nah','nay','nope','n']
     if more in yes:
         play = True
     if more in no:
         print('generating contact list...')
         break
-contacts = zip(names,states,numbers)
-print(tuple(contacts))
+print(contact)
