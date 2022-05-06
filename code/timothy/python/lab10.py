96a09387af3231de7b30884ba26965fa358e362b
Timothy Johnson <johnsonandtimothy@gmail.com>
2022-05-06 10:41:22 -0700

    lab10 work

--- code/timothy/python/lab10.py
+++ code/timothy/python/lab10.py
@@ -38,32 +38,72 @@ def csv_crud_repl(file_path, mode):
         else:
             values = ''.join(line).split(',')
             contacts.append({keys[i]:values[i] for i in range(len(keys))})
-    create = input("Do you want to create a new contact? y/n ")
-    if create == 'y':
-        def create():
-            new_values = input('Please state: Name,Favorite Color,Favorite Fruit ')
-            new_values = list(new_values.split(','))
-            values.append(new_values)
-            contacts.append({keys[i]:new_values[i] for i in range(len(keys))})
-    elif create == 'n':
-        pass
-        # print(contacts)
-    create() 
+    
+    user_prompt = ''
 
-    def retrieve():
-        display = input("Do you want to retrieve a contact? y/n ")
-        if display == 'y':
-            contact_name = input('State the name of the contact you want to retrieve: ')
-            for elements in contacts:
-                if contact_name == elements['name']:
-                    print(elements)    
-        if display == 'n':
-            pass        
-    retrieve()
+    while user_prompt != 'x':
+        user_prompt = input("""What would you like to do? 
+        v = View contact list
+        c = Create a new contact
+        r = Retrieve a contact
+        u = Update a contact
+        d = Delete a contact
+        s = Save contact list
+        x = Exit program
+        """)
 
-    # def update():
-    #     update = input("Do you want to update a contact? y/n ")
-    #     if update == 'y':
-            
+        if user_prompt == 'v':
+            print(contacts)
+            continue
+
+        elif user_prompt == 'c':
+            def create():
+                new_values = input('Please state: Name, Favorite Color, Favorite Fruit: ')
+                new_values = list(new_values.split(','))
+                values.append(new_values)
+                contacts.append({keys[i]:new_values[i] for i in range(len(keys))})
+                print(contacts)
+            create()
+            continue
+
+        elif user_prompt == 'r':
+            def retrieve():
+                contact_name = input('State the name of the contact you want to retrieve: ')
+                for elements in contacts:
+                    if contact_name == elements['name']:
+                        print(elements)
+                        return elements        
+            retrieve()
+            continue
+
+        elif user_prompt == 'u':
+            def update():
+                contact_name = input('State the name of the contact you want to update: ')
+                for contact in contacts:
+                    if contact_name == contact['name']:
+                        print(contact)
+                        update = input('Which attribute would you like to update? ')
+                        contact[update] = input('Type updated information: ')
+                        print(contact)
+                        return contact
+            update()
+            continue
+
+        elif user_prompt == 'd':
+            def delete():
+                contact_name = input('State the name of the contact you want to delete: ')
+                for contact in contacts:
+                    if contact_name == contact['name']:
+                        del contact['name']
+                        print(contacts)
+                        return(contacts)
+            delete()
+            continue
+                    
+        
 
 csv_crud_repl('C:/users/johns/desktop/contact_list.csv', 'r')
+
+
+
+
