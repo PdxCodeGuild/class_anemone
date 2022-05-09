b7a0d9b4035137574622309b7d9a0fd3b005dd52
ICF19 <icfearn92@outlook.com>
2022-05-09 16:14:56 -0700

    lab 10 in progress

--- code/isaac/lab10.py
+++ code/isaac/lab10.py
@@ -2,38 +2,37 @@
 
 # Open and read the csv file 'contacts.csv'
 with open('contacts.csv', 'r') as file:
-    lines = file.read().split("\n")
+    contact_list = file.read().split("\n")
     # print(lines)
     
 # Create a list containing contacts
-contact_list = []
-for line in lines:
-    contact_list.append(line.split(","))
-# print(contact_list)
-headers = contact_list.pop(0)
-# print(headers, contact_list)
+contact_list = [line.split(",") for line in contact_list.split("\n")]
 
+headers = contact_list[0]
 
-new_list = []
-for contact in contact_list:
-    contact_dict = {headers[0]: contact[0], headers[1]: contact[1], headers[2]: contact[2]}
-    new_list.append(contact_dict)
-# print(new_list)
+# Make a list of contacts to search for through the csv file
+# use the zip() function for familiarity purposes
+new_list = [dict(zip(headers, values)) for values in contact_list[1::]]
 
-# Create new contact
+# Create new conact function
+# add a new contact by name, favorite fruit and favorite color
 def create(new_list, headers):
-    user_name = input("What is your contacts name? ")
-    user_fruit = input("What is their favorite fruit? ")
-    user_color = input("What is their favorite color? ")
-    new_dict = {headers[0]: user_name, headers[1]: user_fruit, headers[2]: user_color}
-    new_list.append(new_dict)
+    new_list.append({header: input(f"What is your contacts {header}?  ") for header in headers})
     return new_list
 
-# Retrieve contact
-def retrieve(new_list):
-    user_retrieve = input("Who are you looking for? ")
-    if user_retrieve in new_list:
-        con_ret = headers[0]
-        return con_ret
-print(retrieve(new_list))
-        
+# Create a retrieve contact name function.
+# Use headers as search options for contact
+def retrieve(new_list, headers):
+    header_search = "\n" + "\n".join(headers) + "\n"
+    contact_search = input(f"What would you like to search for today?  Options are: \n{header_search.title()}\n")
+    search_input = input(f"Enter {contact_search}: ")
+
+    search_results = list(filter(lambda contact: contact[contact_search] == search_input, new_list))
+    return search_results
+
+# Update the contacts name, fruit or color
+def update(new_list, headers):
+    search_results = retrieve(new_list, headers)
+    # update a specific index of the contact
+    positon_update = int(input(f"Which contact would you like to edit? (1-{len(search_results)})")) -1
+    list_update = input()
