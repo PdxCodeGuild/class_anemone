eb07273c219567e7102a4b09b8f4fd69da8de3ed
Jonathan meadows <jon_meadows@ymail.com>
2022-05-09 18:47:25 -0500

    lab12 V2

--- code/meadows/python/lab12.py
+++ code/meadows/python/lab12.py
@@ -5,12 +5,14 @@ class ATM:  # A newly created account will default to a '''balance of 0 and an i
     def __init__(self,balance=0,interest=0.001):
         self.balance = balance
         self.interest = interest
+        self.transaction = []
     def check_balance(self):
         return self.balance
     def calc_interest(self):
         return self.balance * self.interest
     def deposit(self, amount):
         self.balance += amount
+        self.transaction.append(f'Deposit ${amount}')
     def check_withdrawal(self, amount):
         if (self.balance - amount) >= 0:
             return True
@@ -18,7 +20,10 @@ class ATM:  # A newly created account will default to a '''balance of 0 and an i
             return False
     def withdraw(self, amount):
         self.balance -= amount
+        self.transaction.append(f'Withdrew ${amount}')
         return amount
+    def transactions(self):
+        return self.transaction
 
 atm = ATM() # create an instance of our class
 print('Welcome to the ATM')
@@ -46,12 +51,19 @@ while True:
         atm.deposit(amount)
         print(f'Accumulated ${amount} in interest')
 
+    elif command == 'transactions':
+        transactions = atm.transactions()
+        print(transactions)
+
+
+
     elif command == 'help':
         print('Available commands:')
         print('balance  - get the current balance')
         print('deposit  - deposit money')
         print('withdraw - withdraw money')
         print('interest - accumulate interest')
+        print('transactions - amount depoited/withdrew')
         print('exit     - exit the program')
 
     elif command == 'exit':
