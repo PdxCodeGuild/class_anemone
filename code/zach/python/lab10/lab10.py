173fd9b9c57056e9835d66da08e647898b165b2c
Zach-EE <zachholloway.n@gmail.com>
2022-05-08 22:06:09 -0400

    commiting to switch to another branch

--- code/zach/python/lab10/lab10.py
+++ code/zach/python/lab10/lab10.py
@@ -7,8 +7,7 @@ contacts = [
 contact_org = ['name', 'favorite fruit', 'favorite_color']
 contact_info = [['matthew', 'blackberries', 'purple'], ['jill','bananas', 'purple']]
 #! write_f() will rewrite entire .csv file 
-
-def write_f():
+def write_f():#TODO: Figure out a use for this
     with open('contacts.csv', 'w') as f:
         results = []
         for contact in contact_info:
@@ -20,14 +19,29 @@ def write_f():
         f.close()
     return
 
+def read_f():
+    with open('contacts.csv', 'r') as f:
+        results = []
+        f.readline() # ignores header row
+        for line in f:
+            contact = line.split(',\n')
+            results.append(contact)
+        print(contact[0::-1])
+    # answer = {}
+    # with open('path/to/file') as infile:
+    #     infile.readline()  # we don't care about the header row
+    #     for line in infile:
+    #         year, gender, name, count = (s.strip('"') for s in line.split(','))
+    #         key = (name, gender)
+    #         if key not in answer: answer[key] = {}
+    #         answer[key][int(year)] = (int(count), None)
+    return
 def main():
-    # with open('contacts.csv', 'r') as f:
-    #     results = []
-    #     for line in f:
     #         words = line.split(',')
     #         results.append((words[0]), words[1:])
     # return
     write_f()
+    #read_f()
     return
 
 
