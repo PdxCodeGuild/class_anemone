b5a4a9b901b8770a7a2ff7cac1b09bf765b230fb
Rachel Lloyd <rachel@Rachels-iMac.local>
2022-05-09 19:34:39 -0400

    Lab 12 - in progress

---
+++ code/rachel/Python/Lab12.py
@@ -0,0 +1,70 @@
+#Lab12: ATM
+
+
+transactions = []
+
+class ATM:
+
+    def __init__(self):
+        self.balance = 0
+        self.interest_rate = 0.001
+    
+    def check_balance(self):
+        return (self.balance) 
+    
+    def deposit(self, amount):
+        self.balance += amount
+
+    def check_withdrawal(self, amount):
+        if (self.balance - amount) >= 0:
+            return True
+
+    def withdraw(self, amount):
+        self.balance -= amount
+
+    def calc_interest(self):
+        interest = self.balance * self.interest_rate
+        return interest
+    
+    def print_transactions(self):
+        if self == 'deposit':
+            transactions.append(f"User deposited ${amount}")
+        elif self == 'withdraw':
+            transactions.append(f"User withdrew ${amount}")
+
+
+atm = ATM() # create an instance of our class
+print('Welcome to the ATM')
+while True:
+    command = input('Enter a command: ')
+    if command == 'balance':
+        balance = atm.check_balance() # call the check_balance() method
+        print(f'Your balance is ${balance}')
+    elif command == 'deposit':
+        amount = float(input('How much would you like to deposit? '))
+        atm.deposit(amount) # call the deposit(amount) method
+        print(f'Deposited ${amount}')
+    elif command == 'withdraw':
+        amount = float(input('How much would you like '))
+        if atm.check_withdrawal(amount): # call the check_withdrawal(amount) method
+            atm.withdraw(amount) # call the withdraw(amount) method
+            print(f'Withdrew ${amount}')
+        else:
+            print('Insufficient funds')
+    elif command == 'interest':
+        amount = atm.calc_interest() # call the calc_interest() method
+        atm.deposit(amount)
+        print(f'Accumulated ${amount} in interest')
+    elif command == 'transactions':
+        print (transactions)
+    elif command == 'help':
+        print('Available commands:')
+        print('balance  - get the current balance')
+        print('deposit  - deposit money')
+        print('withdraw - withdraw money')
+        print('interest - accumulate interest')
+        print('exit     - exit the program')
+    elif command == 'exit':
+        break
+    else:
+        print('Command not recognized')
