b2f486ec4f722d9c3e5c446f4b688ae167fb99c2
Gi-bi <stanjessgrubbs@gmail.com>
2022-05-09 16:41:09 -0700

    lab12 progress

---
+++ code/jessica/lab12/lab12.py
@@ -0,0 +1,7 @@
+class Checking_account():
+    def __init__(self, balance, interest_rate):
+        self.balance = balance
+        self.interest_rate = interest_rate
+
+    def check_balance():
+        
