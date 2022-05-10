771f25d3d7a5b856f5a97947ed705b835ebbaddd
avocado-jc <cavada.jonathon@gmail.com>
2022-05-10 11:51:27 -0700

    lab12 submit

--- code/cavada/python/lab12.py
+++ code/cavada/python/lab12.py
@@ -1,58 +1,51 @@
-import random
-trans_id=[]
-def trans_id_assign(trans_id):
+import random # imported to generate random numbers for transaction ids
+# trans_id=[] 
+def trans_id_assign():
+    trans_id = []
     while len(trans_id) < 6:
-        trans_id.append(random.randint(1,9))
-    return trans_id
+        trans_id.append(str(random.randint(1,9)))
+    t_id = ''
+    for t in trans_id:
+        t_id+=t
+    return t_id      
 
-balance = 0
-rate = .1
-trans_t = [{"action":"withdraw", "amount": 0, "begin balance":0, "end balance": 0}]
-transactions = []
+balance = 0 # set account balance to zero
+rate = .1 # 10% interest rate for interest accumulation function
+# trans_template = [{"action":"transaction", "amount": 0, "begin balance":0, "end balance": 0}]
 
-class ATM:
-    def __init__(self, balance, rate):
-        self.transactions = transactions
-        self.balance = balance
-        self.rate = rate
-        # print(balance)
+class ATM: # class set to contain all the functions relevant to the instantion represened by calling the balance and rate through it to essentially simulate an ATM session
+    def __init__(self, balance, rate): #dunder init function set to store running variables for rate, balance, and transactions
+        self.transactions = [] # creates shell for storing transactions
+        self.balance = balance # sets this as class varible
+        self.rate = rate # set this as class variavle
 
-    def check_balance(self):
+    def check_balance(self): # function to access the current state of the class variable balance
         return self.balance 
         
-
-    def deposit(self, amount):
+    def deposit(self, amount): # function to make a 'deposit'; essentially add a value to balance value corresponding to the deposit amount
         self.balance += amount
+        trans_id = trans_id_assign()
+        self.transactions.append(f"trans #{trans_id}: \n\tdeposit, amount: ${amount}, ending balance: ${self.balance}") # saves transaction info
 
-
-    def withdrawal_attempt(self, amount):
+    def withdrawal_attempt(self, amount): # function to check if withdraw can be made if the withdrawal won't exeed available funds, or bring balance below zero
         self.amount = amount
         if amount <= self.balance:
             return True
 
-    def withdraw(self, amount):
+    def withdraw(self, amount): # function to reduce balance by value amount 
         self.balance -= amount
+        trans_id = trans_id_assign() # uses function to get a random transaction id number (hash method)
+        self.transactions.append(f"trans #{trans_id}: \n\twithdraw, amount: ${amount}, ending balance: ${self.balance}")
         
-    def calc_interest(self, rate):
+    def calc_interest(self, rate): # tells user the amount of interest expected after 12 accumulation
         interest = self.balance * rate    
         return interest
 
-    
+    def print_transactions(self): # funtction to bring transaction list to global 
+        return self.transactions
 
+atm = ATM(balance, rate) # create an instance of our class, 
 
-    def print_transactions(self, transactions):
-        trans_id=trans_id_assign(trans_id)
-        print(trans_id, transactions)
-        return transactions
-
-    
-    
-# balance = ATM(1000,.05)
-
-
-
-atm = ATM(balance, rate) # create an instance of our class
-# print(type(atm))
 print('Welcome to the ATM')
 while True:
     # trans_id = random.randint()
@@ -66,38 +59,39 @@ while True:
     if command == 'balance' or command == 'b':
         balance = atm.check_balance() # call the check_balance() method
         print(f'Your balance is ${balance}')
+
     elif command == 'deposit' or command == 'd':
         amount = float(input('How much would you like to deposit? '))
         atm.deposit(amount) # call the deposit(amount) method
         print(f'Deposited ${amount}, New balance: ${atm.check_balance()}')
-        x = f"trans #{trans_id} : {command}, amount: {amount}, ending balance: {atm.check_balance()}"
+        
     elif command == 'withdraw' or command == 'w':
         amount = float(input('How much would you like '))
         if atm.withdrawal_attempt(amount): # call the check_withdrawal(amount) method
             balance = atm.withdraw(amount) # call the withdraw(amount) method
             print(f'Withdrew ${amount}; balance: ${atm.check_balance()}')
-
-            x = f"trans #{trans_id}: {command}, amount: {amount}, ending balance: {atm.check_balance()}"
         else:
             print('Insufficient funds')
+            
     elif command == 'interest' or command == 'i':
         amount = atm.calc_interest(rate) # call the calc_interest() method    
-        print(f'${amount} in interest will accumulate over the next 12 months at current balance of ${atm.check_balance()}')
+
     elif command == 'transactions' or command == 't':
         print("Transaction Report Summary: ")
-        transactions = (atm.print_transactions(transactions))  
-        trans_id = transactions.pop()
-        print(transactions)  
-    # elif command == 'help':
-    #     print('Available commands:')
-    #     print('balance  - get the current balance')
-    #     print('deposit  - deposit money')
-    #     print('withdraw - withdraw money')
-    #     print('interest - accumulate interest')
-    #     print('exit     - exit the program')
-    elif command == 'exit':
+        transactions = atm.print_transactions()
+        for trans in transactions:
+            print(trans)
+
+    elif command == 'exit': # exit instance of atm
         break
+
     else:
         print('Command not recognized')
 
-print(f"final balance: {atm.check_balance()}")    
+print("account activity summary: ")
+
+print(f"final balance: ${atm.check_balance()}") # print of final balance
+for trans in transactions: 
+    print(trans) # print each transaction from list compiled during instantiation of ATM class
+
+
