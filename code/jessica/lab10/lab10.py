eea51c7b242db49a9736c8b589d0acee04734fb9
Gi-bi <stanjessgrubbs@gmail.com>
2022-05-10 16:25:44 -0700

    lab10 WIP

--- code/jessica/lab10/lab10.py
+++ code/jessica/lab10/lab10.py
@@ -1,36 +1,85 @@
+#------------LAB10 v1, v2, v3---------------------#
+
+def list_split():
+    with open('contacts.csv') as f:
+        lines = f.read().split("\n")
+     
+    contacts = []
+    header = lines[0].split(",")
+    
+    for i in range(1, len(lines)):
+        row = lines[i].split(",")
+        contact = dict(zip(header, row))
+        contacts.append(contact)
+    return contacts
+
+#-------------CRUD functions v2--------------------#
+
+def create_list():
+    new_line = input("Name, Favorite fruit, Favorite color: ")
+    new_line = list(new_line.split(","))       
+    contacts.append(new_line)
+    print(contacts)
+    return contacts
+
+def retrieve(contacts, name):
+    for index, contact in enumerate(contacts):
+        if contact['name'] == name:
+            print(contact)
+    return contacts[index]
+    
+def update():
+    contact_name = input('Which contact do you want to update? ')
+    for contact in contacts:
+        if contact_name == contact['name']:
+            print(contact)
+            update = input('Which attribute would you like to update? ')
+            contact[update] = input('Type updated information: ')
+            print(contact)
+            return contact
+    
+def delete():
+    contact_name = input('Which contact do you want to delete? ')
+    for contact in contacts:
+        if contact_name == contact['name']:
+            confirm = input(f"Are you sure: yes or no ")
+            if confirm == 'yes':
+                contacts.remove(contact)
+            elif confirm == 'no':
+                pass
+                print(contacts)
+                return(contacts)
 
 
-def list_split(fileName="contacts.csv"):
-    with open(fileName) as file:
-        column_names = file.readline() #reading the header line
-        keys = column_names.split(",") #Splitting the line into keys
-        number_of_columns = len(keys)
 
-        list_of_dictionaries = []
+#-----------While loop for user input---------------#
 
-        data = file.readlines()
+commands = ['1', 'retrieve',
+           '2', 'update', 
+           '3', 'create new',
+           '4', 'delete',
+           '5', 'quit']
 
-        list_of_rows = []
+contacts = list_split()
 
-        for row in data:
-            list_of_rows.append(row.split(","))
-
-        file.close()
-
-        for item in list_of_rows:
-            row_as_a_dictionary = {}
-            for i in range(number_of_columns):
-                row_as_a_dictionary[keys[i]] = item[i]
-            list_of_dictionaries.append(row_as_a_dictionary)
-            
+while True:
+    print(commands)
+    command = input("Name a command: ")
+    if command == '1':
+        name = input("Name? ")
+        retrieve(contacts, name)
+    elif command == '2':
+        update()
+    elif command == '3':
+        create_list()
+    elif command == '4':
+        delete()
+        print(contacts)
+    elif command == '5':
+        break
+    
+    
         
-        for i in range(len(list_of_dictionaries)):
-            print(list_of_dictionaries)
-            return list_of_dictionaries[i]
-
-contact_list = [] #comprehension
-list_split()
-
 
 
 
