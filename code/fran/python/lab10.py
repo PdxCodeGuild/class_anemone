5e6c649683394264e18f1c622c47eb2c6e39d025
Frances Kappes <fekmacbookpro2019@Feks-MacBook-Pro-2019.local>
2022-05-09 18:40:22 -0700

    version 2

--- code/fran/python/lab10.py
+++ code/fran/python/lab10.py
@@ -15,94 +15,120 @@ with open(file_name, 'r') as file:
 # Create a string of individual contact dictionaries from the contacts file
 contacts = []
 lines = contents.split('\n')            # split record on new line character
-print(f"lines: {lines}")
+#print(f"lines: {lines}")               ### TEST
 header = lines[0].split(',')            # split header into individual items based on comma
 
 for i in range(1,len(lines)):           # loop through all lines except the header
     row = lines[i].split(',')           # split record on comma
-    print(f"row: {row}")
+    #print(f"row: {row}")               ### TEST
     contact = dict(zip(header,row))     # creates tuple
-    print(f"contact: {contact}")       # TEST
+    # print(f"contact: {contact}")      ### TEST
     contacts.append(contact)            # append individual's dictionary contents to contacts list
 
 print(f"contacts: {contacts}")       # TEST
 
-### Notes from discussion with Liz ###
 
-# Need to have REPL (while True loop)
-# Pass in contacts dictionary to all functions
+# Define create_contact function, which creates a new contact record
+def create_contact(contacts):
 
-##### END NOTES #####
-
-# Prompt user to create, view, modify, or delete a contact record, then execute the action
-user_action = input("What would you like to do? Enter 'c' to create a new contact, 'r' to retrieve a contact record, 'u' to update an existing contact record, or 'd' to delete an existing contact record: ")
-
-new_record = []
-
-if user_action == 'c':
     user_name = input("What is your first name? ")
     user_fruit = input("What is your favorite fruit? ")
     user_color = input("What is your favorite color? ")
 
-    # Assemble new record from user input
-    new_record = user_name + ',' + user_fruit + ',' + user_color
-    print(f"new record: {new_record}")      ### TEST
+    # Create new dictionary entry
+    contacts.append({"name": user_name,"favorite fruit": user_fruit,"favorite color": user_color})
     
-    # Add new record to file
-    with open(file_name, 'a') as file:
-        file.write(new_record)
+    #print(f"contacts, with new record: {contacts}")      ### TEST
 
-elif user_action == 'r':
-    user_name = input("What is the first name for the contact record you wish to view? ")
 
-    # Find the record
-    #existing_record = contact.split('\n')
-    #existing_record = re.findall(r'{user_name}', contact)
-    #existing_record = contacts.find(user_name)
+# Define retrive_contact function, which retrieves an existing contact record
+def retrieve_contact(contact_name, contacts):
+
+    # Search through dictionaries for contact record, based on name key
+    for contact in contacts:
+        
+        #print(f"contact name to search for: {contact_name}")   ### TEST
+        #print(contact['name'])                 ### TEST
+
+        if contact['name'] == contact_name:
+            found_contact = contact
+            break
+            #print(f"name found: {contact}")    ### TEST
+        else:
+            found_contact = []
+            #print("not this record")           ### TEST
+
+    return found_contact
+
+
+# Define update_contact function, which updates an existing contact record
+def update_contact(user_name, contacts, update_key, update_value):
+
+    # Find existing contact record
+    existing_contact = retrieve_contact(user_name, contacts)
+
+    # Update attribute value for the given attribute key for this contact record
+    existing_contact[update_key] = update_value
+
+    #print(f"contacts after update: {contacts}")    ### TEST
+
+# Define delete_contact function, which deletes an existing contact record
+def delete_contact(user_name, contacts):
+
+    # Find existing contact record
+    existing_contact = retrieve_contact(user_name, contacts)
+
+    # Delete contact record from dictionary
+    del existing_contact['name']
+    del existing_contact['favorite fruit']
+    del existing_contact['favorite color']
+    #del existing_contact
     
-    # for i in range(1,len(lines)):           # loop through all lines except the header
-    #     row = lines[i].split(',')           # split record on comma
-    #     print(f"row: {row}")
-    #     contact = dict(zip(header,row))     # creates tuple
-    #     print(f"contact: {contact}")       # TEST
-    #     contacts.append(contact)
+    #print(f"contacts after delete: {contacts}")  ### TEST
 
 
-    matches = [match for match in lines if user_name in match]
-    values = [value for value in contact.values() if user_name in value]  ### RETURNS empty list []
-    print(f"matches: {matches}")  ### RETURNS ['bert,banana,blue']
+# REPL loop. Prompt user to create, view, modify, or delete a contact record, then execute the action
+while True:
+    user_action = input("What would you like to do? Enter 'c' to create a new contact, 'r' to retrieve a contact record, 'u' to update an existing contact record, 'd' to delete an existing contact record, or 'exit' to exit: ")
+
+    if user_action == 'c':      
     
-    match_name = matches[0]   ### RETURNS bert,banana,blue
-    print(f"match name: {match_name}")
-    #record = matches.split(',')  ### GET AttributeError: 'list' object has no attribute 'split'
-    #print(f"record: {record}")
-    # print(f"name: {matches[0]}")
-    # print(f"favorite fruit: {matches[1]}")
-    # print(f"favorite color: {matches[2]}")
-    print(f"values: {values}")
+        create_contact(contacts)
     
-    
-    # print(f"lines: {lines}")
-    # if user_name in lines: #contents:
-    #     print("user name exists")
-    # else:
-    #     print("not found")
-    # #print(f"existing record: {existing_record}")
+    elif user_action == 'r':    
+        # Prompt user for contact name to search on
+        user_name = input("What is the first name for the contact record you wish to view? ")
+        
+        found_contact = retrieve_contact(user_name, contacts)
+
+        #print(f"found contact: {found_contact}")   ### TEST
+
+    elif user_action == 'u':
+        # Prompt user for contact info to search on and update
+        user_name = input("What is your first name? ")
+        user_attribute = input("What attribute would you like to update? fruit or color? ")
+        user_attribute_value = input("What is your new favorite fruit or color? ")
+
+        if user_attribute == 'fruit':
+            user_attribute = 'favorite fruit'
+        elif user_attribute == 'color':
+            user_attribute = 'favorite color'
+
+        update_contact(user_name, contacts, user_attribute, user_attribute_value)
+
+    elif user_action == 'd':
+        # Prompt user for contact name for record they want to delete
+        user_name = input("What is the first name for the contact record you wish to delete? ")
+
+        delete_contact(user_name, contacts)
+
+    elif user_action == 'exit':
+        break
+
+# Write contents of dictionary to csv file
+
+        # Add new record to file
+        # with open(file_name, 'a') as file:
+        #     file.write(new_record)
 
 
-
-elif user_action == 'u':
-    user_name = input("What is your first name? ")
-    user_attribute = input("What attribute would you like to update? fruit or color? ")
-    user_attribute_value = input("What is your new favorite fruit or color? ")
-
-
-
-elif user_action == 'd':
-    user_name = input("What is the first name for the contact record you wish to delete? ")
-
-
-
-
-
-#print(f"contacts list: {contacts}")
