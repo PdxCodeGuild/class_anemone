1f059279858e1700c2a00b397deea6f831b57c4b
Frances Kappes <fekmacbookpro2019@Feks-MacBook-Pro-2019.local>
2022-05-09 20:52:19 -0700

    Final version for submission

--- code/fran/python/lab07.py
+++ code/fran/python/lab07.py
@@ -10,20 +10,16 @@ def rot_cipher(letter, rotation):
 
     # Determine ROTn character
     rot_index = 0
-    rot_index += 13 if alphabet_list.index(letter) < 14 else alphabet_list.index(letter)-13
-    
-    # if alphabet_list.index(letter) < 14:
-    #     rot_index += 13
-    # else:
-    #     rot_index -=13
+    rot_index = alphabet_list.index(letter)+13 if alphabet_list.index(letter) < 13 else alphabet_list.index(letter)-13
     
     rot_char = alphabet_list[rot_index]
 
-    print(f"letter: {letter}")      ### TEST
-    char_index = alphabet_list.index(letter)    ### TEST
-    print(f"char_index: {char_index}")  ### TEST
-    print(f"rot_index: {rot_index}")    ### TEST
-    print(f"rot_char: {rot_char}")      ### TEST
+    # print(f"letter: {letter}")      ### TEST
+    # char_index = alphabet_list.index(letter)    ### TEST
+    # print(f"char_index: {char_index}")  ### TEST
+    # print(f"rot_index: {rot_index}")    ### TEST
+    # print(f"rot_char: {rot_char}")      ### TEST
+   
     return rot_char
 
 # Prompt user for a string
@@ -32,14 +28,13 @@ user_string = input("Enter a string: ")
 # Define rotation factor
 rotation = 13
 
-
 # Break string up into a list of individual characters
 user_char_list = []
 i = 0
 
 user_char_list = [user_string[i] for user_string in user_string]
 
-print(f"user_char_list: {user_char_list}")   ### TEST
+# print(f"user_char_list: {user_char_list}")   ### TEST
 
 
 # Loop through the user character list and call ROT Cipher function
