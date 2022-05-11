8778e83a3ef5d72df04689f365905b337e8023d4
Timothy Johnson <johnsonandtimothy@gmail.com>
2022-05-11 15:07:13 -0700

    Timothy lab 13 complete

--- code/timothy/python/lab13.py
+++ code/timothy/python/lab13.py
@@ -1,9 +1,11 @@
-# ....Lab 13 ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
+# ....Lab 13 COMPLETE~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
 
 class Player:
     def __init__(self, name, token):
         self.name = name
         self.token = token
+    def __eq__(self, other):
+        pass
 
 # player_one = Player('Merry', 'X')
 # player_two = Player('Pippin', 'O')
@@ -34,9 +36,7 @@ class Game(Player):
     def move(self, x, y, player):
         # x = int(input('Which row: '))
         # y = int(input('Which column: '))
-        if self.board[x][y] != '-':
-            print('Space already claimed...')
-        else: self.board[x][y] = player.token
+        self.board[x][y] = player.token
 
     def calc_winner(self, player):
         if self.board[0][0] == self.board[0][1] == self.board[0][2] == player.token:
@@ -71,36 +71,53 @@ class Game(Player):
 # ....TIC TAC TOE ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
 
 def Main():
+
     while True:
+
         game = Game()
+
         print('Welcome to Tic Tac Toe')
-        player_one = Player(input('Player One Name: '), input('Player One Token: '))
-        player_two = Player(input('Player Two Name: '), input('Player Two Token: '))
+
+        player_one = Player(input('Player One Name: ').title(), input('Player One Token: ').upper())
+        player_two = Player(input('Player Two Name: ').title(), input('Player Two Token: ').upper())
+
         print(f'{player_one.name} is {player_one.token}')
         print(f'{player_two.name} is {player_two.token}')
+
         turn = 0
+
         while True:
+
             player = player_one if turn % 2 == 0 else player_two
+
             print(game.__repr__())
-            x = int(input(f'{player.name}, which row: '))
-            y = int(input(f'{player.name}, which column: '))
-            game.move(x, y, player)
+
+            while True:
+                x = int(input(f'{player.name}, place {player.token} in which row: '))
+                y = int(input(f'{player.name}, place {player.token} in which column: '))
+                if game.board[x][y] != '-':
+                    print('Space already claimed...')
+                else:
+                    game.move(x, y, player)
+                    break
+
             if game.calc_winner(player) == player.token:
                 print(game.__repr__())
                 print(f'{player.name} has won the game!')
                 break
+
             elif game.is_full() == True:
                 print(game.__repr__())
                 print('The game was a draw!')
                 break
+
             turn += 1
+
         if game.is_game_over(player) == True or player.token:
-            another_game = input('Would you like to play again? y/n ')
-            if another_game == 'n':
+            another_game = input('Want to play again? Y/N ').upper()
+            if another_game == 'N':
                 break
-            elif another_game == 'y':
-                False
+
+
 Main()
 
-# Issues:
-# - 'Space already claimed' skips player turn
