2a6d2271ea3707f08ba91a582cc205a6ea16d866
Zach-EE <zachholloway.n@gmail.com>
2022-05-12 13:45:29 -0400

    have add and del contacts functions up and running

--- code/zach/python/lab10/lab10.py
+++ code/zach/python/lab10/lab10.py
@@ -1,8 +1,8 @@
 #! write_f() will rewrite entire .csv file
-def write_f():  # TODO: Figure out a use for this
+def write_f(data):  # TODO: Figure out a use for this
     with open('contacts.csv', 'w') as f:
-        results = []
-        for contact in contact_info:
+
+        for contact in range (len(data)):
             for cat, info in zip(contact_org, contact):
                 print(cat, ':', info)
                 # * 'name':'matthew','favorite fruit':'blackberries','favorite_color':'purple',....
@@ -10,31 +10,37 @@ def write_f():  # TODO: Figure out a use for this
             f.writelines('\n')
         f.close()
     return
-
+re
 
 def init_contacts():
     with open('contacts.csv', 'r') as f:
         data_csv = f.read()
-        data_csv = [line.split(',') for line in data_csv.split('\n')]
-        keys = data_csv[0]
-        contacts = [dict(zip(keys, values)) for values in data_csv[1::]]
+        csv_list = [line.split(',') for line in data_csv.split('\n')]
+        keys = csv_list[0]
+        contacts = [dict(zip(keys,values)) for values in csv_list[1::]]
         f.close()
     return contacts
 
 
-def add_contacts():
-    return
+def add_contacts(data, name, fav_fruit, fav_color):
+    new_contact = {'name':name,'favorite fruit':fav_fruit,'favorite color': fav_color}
+    data.append(new_contact)
+    return data
 
 
-def del_contacts():
-    return
+def del_contacts(data, name):
+    for i in range(len(data)):
+        if name == data[i].get('name'):
+            data.remove(data[i])
+            return data
+    return print(f'{ name.capitalize() } not found in contacts')
+
 
 
 def main():
     # REPL loop for user_input and application navigation and control
 
     contact_list = init_contacts()
-    print(type(contact_list[0]))
     while True:
         user_input = input('User Enter: read, add, remove, or quit: ')
         if user_input == 'quit':
@@ -42,7 +48,6 @@ def main():
         elif user_input == 'read':
             name = str(input('enter contact name: '))
             for i in range(len(contact_list)):
-
                 if name == contact_list[i].get('name'):
                     print('\nName: ', contact_list[i].get('name').capitalize())
                     print('Favorite Fruit: ',
@@ -51,11 +56,20 @@ def main():
                           contact_list[i].get('favorite color'))
                     print('______________________________________________')
         elif user_input == 'add':
-            add_contacts()
+            print('Enter info of contact to add.')
+            name = input('Name: ')
+            fruit = input('Favorite Fruit: ')
+            color = input('Favorite Color: ')
+            add_contacts(contact_list, name, fruit, color)
         elif user_input == 'remove':
-            del_contacts()
+            name = input('Enter name of contact you wish to remove\nName: ')
+            del_contacts(contact_list, name)
         else:
+            print(contact_list[0],contact_list)
             print('input not recognized... \n')
+    
+    # def write_f(contact_list)
+        
     return
 
 
