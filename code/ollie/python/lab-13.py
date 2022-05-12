494ad246c977b8d10046f5935c78bd10fa58b567
Ollie's Macbook Pro <ojc@Ollies-MacBook-Pro.local>
2022-05-11 19:42:32 -0700

    fixed a faulty board

--- code/ollie/python/lab-13.py
+++ code/ollie/python/lab-13.py
@@ -1,16 +1,25 @@
 ' Lab 13 - Tic Tac Toe '
 
-#class Game:
+class Player:
+    pass
 
-def __repr__(board):
 
-    print('-------')
-    print('|' + board[1] + '|' + board[2] +'|' + board[3] + '|')
-    print('-------')
-    print('|' + board[4] + '|' + board[5] +'|' + board[6] + '|')
-    print('-------')
-    print('|' + board[7] + '|' + board[8] +'|' + board[9] + '|')
-    print('-------')
+class Game:
 
-test = ['', 'x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x']
-__repr__(test)
+    def __init__(self):
+        self.board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+    def __repr__(self):
+        board = (f"""
+        -------
+        |{self.board[0]}|{self.board[1]}|{self.board[2]}|
+        -------
+        |{self.board[3]}|{self.board[4]}|{self.board[5]}|
+        -------
+        |{self.board[6]}|{self.board[7]}|{self.board[8]}|
+        -------
+        """)
+        print(board)
+
+tictactoe = Game()
+print(tictactoe.__repr__)
