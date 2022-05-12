7d04a35768a0c254cf2bcf07fe16a9ceae4ea7f6
Jonathan meadows <jon_meadows@ymail.com>
2022-05-11 19:03:39 -0500

    fixed lab13

--- code/meadows/python/lab13.py
+++ code/meadows/python/lab13.py
@@ -8,7 +8,7 @@ class Player:  # input for top/middle/bottom then for left/middle/right
         self.token = token
 
     def __str__(self):
-        if self.token == 'x':
+        if self.token == 'x':                # Making the 2 token so player1 can = x and player 2 = o
             return str(self.token)
         elif self.token == 'o':
             return str(self.token)
@@ -20,18 +20,18 @@ class Game:
             [' ',' ',' '],
             [' ',' ',' ']
         ]
-    def __repr__(self):
+    def __repr__(self):       # taking the board layout as above and joining them with | to create the splits and then \n for new lines
         a = ''
         for x in self.board:
             a += ("|".join(x))
             a += ('\n')
         return a
         
-    def move(self, player, spot1):
+    def move(self, player, spot1):   # Referr to bottom notes Also.. but its checking EACH SPOT
 
             if spot1 == 'q':
                 if self.board[0][0] != ' ':
-                    return move()
+                    return print('\nSpot Already in use\n')
                 self.board[0][0] = player.token
 
             if spot1 == 'w': 
@@ -74,7 +74,7 @@ class Game:
                     return print('\nSpot Already in use\n')
                 self.board[2][2] = player.token
 
-    def calc_winner(self):
+    def calc_winner(self):         # determing the winner by checking EACH postion in their respected order.. bottom notes explain daignal
 
         # side to side winner
         if self.board[0][0] == self.board[0][1] == self.board[0][2] != ' ':
@@ -98,8 +98,7 @@ class Game:
         elif self.board[2][0] == self.board[1][1] == self.board[0][2] != ' ':
             return self.board[2][0]
        
-    def is_full(self):
-        # space = ' '
+    def is_full(self):              # checking the lists in self.board for and ' ' left and if there is none it returns false full a TIE!
         for list in self.board:
             for item in list:
                 if item == ' ':
@@ -108,13 +107,13 @@ class Game:
         return True
     
     def is_game_over(self):
-        return self.calc_winner or self.is_full
+        return self.calc_winner or self.is_full  # used as a ref to make checking game over is true or false
 
 
         
 yes = True   
-while yes:
-    game = Game()
+while yes:          # loop in a loopm so the game wont ALWAYS ask for your name and reset
+    game = Game()        # pulling the board class
     game.is_game_over()
     name1 = input('\nenter "player 1" name: ')
     token1 = 'x'
@@ -135,7 +134,7 @@ while yes:
             print(rematch_1)
             break
         if game.is_full():
-            rematch_1 = input('TIE GAME.. Re Match?: ( Y or N ):').lower()
+            rematch_1 = input('TIE GAME.. Re Match?: ( Y or N ):').lower()            # just a loop if they wanna keep playing or not
             break
         
         print(f"\n'Q'|'W'|'E'\n'R'|'T'|'Y'\n'U'|'I'|'O'\n") 
@@ -144,7 +143,8 @@ while yes:
         game.move(player2, choose)
         print(game.__repr__())
         if game.calc_winner():
-            print('nice')
+            rematch_1 = input(f'\nWINNER is! {name1}!!!\n \n New game?: ( Y or N ): ') # if this works move to player 2 also
+            print(rematch_1)
             break
 
     yeah = ['yes', 'y', 'yeh', 'sure', 'si']
@@ -152,7 +152,7 @@ while yes:
     if rematch_1 in yeah:
         yes = True    
     elif rematch_1 in no:
-        print('BYE!')
+        print('\nBYE!')
         break
     else:
         print(f'Choose either {yeah} or {no} words')
