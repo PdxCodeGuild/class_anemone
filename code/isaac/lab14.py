823ba2e2f650a7fcebbbc92562cc3061d4930018
ICF19 <icfearn92@outlook.com>
2022-05-13 11:40:00 -0700

    lab 14 complete

--- code/isaac/lab14.py
+++ code/isaac/lab14.py
@@ -1,7 +1,14 @@
 from urllib import response
 '''Dad Joke API'''
-
+# import modules for use
+# random module to use for random seconds per joke
+# requests to request information from URL
+# time for time between jokes
+import random
 import requests
 import time
 
-response = requests.get('')
+# response variable for the url with headers as 'Accept': 'application/json'
+response = requests.get('https://icanhazdadjoke.com/', headers={'Accept': 'application/json'})
+time.sleep(1)  # one second sleep between jokes
+print(response.json()['joke'])  # print off the joke to the user
