12c5752da1b7ab8dc1ed7d06f70f8eea04464b5f
Timothy Johnson <johnsonandtimothy@gmail.com>
2022-05-13 15:33:39 -0700

    timothy lab14

--- code/timothy/python/lab14.py
+++ code/timothy/python/lab14.py
@@ -4,7 +4,7 @@ import requests
 import pprint
 import time
 
-def random_dad_joke():
+def random_dad_joke(): # Displays one random dad joke.
     response = requests.get("https://icanhazdadjoke.com", headers={'Accept':'application/json'})
     response_dict = response.json()
     joke = response_dict['joke']
@@ -14,9 +14,7 @@ def random_dad_joke():
 
 # random_dad_joke()
 
-# ....Lab 14 Version 2 ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
-
-def search_dad_jokes():
+def rolling_term_jokes(): # Asks user for a keyword to filter jokes, and rolls through each with 5sec gaps.
     search_term = input('Enter a keyword and I will tell you some Dad Jokes: ')
     response = requests.get('https://icanhazdadjoke.com/search', headers ={'Accept':'application/json'}, params={'term':search_term})
     response_dict = response.json()
@@ -24,7 +22,32 @@ def search_dad_jokes():
     for joke in jokes:
         print(joke['joke'])
         time.sleep(5)
-search_dad_jokes()
+    print(f'End of {search_term} jokes.')
 
+# rolling_term_jokes()
+
+# ....Lab 14 Version 2 ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
+
+def search_dad_jokes():
+    search_term = input('Enter a keyword and I will tell you some Dad Jokes: ')
+    response = requests.get('https://icanhazdadjoke.com/search', headers ={'Accept':'application/json'}, params={'term':search_term})
+    response_dict = response.json()
+    jokes = response_dict['results']
+    pages = response_dict['total_pages']
+
+    if jokes == []: # Case for search term returning no jokes.
+        print(f"I don't have any {search_term} jokes...")
+    else: print(f'I found {pages} page(s) of {search_term} jokes.')
+
+    for joke in jokes:
+        print(joke['joke'])
+        time.sleep(3)
+        another = input('Want to hear another? ').lower()
+        if another in ['no', 'n', 'please god no']:
+            break
+        time.sleep(3)
+    print(f'End of {search_term} jokes.')
+
+search_dad_jokes()
 
 
