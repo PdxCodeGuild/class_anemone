1a113727ef22bf66d84ae10f23256c8bffe83c46
Frances Kappes <fekmacbookpro2019@Feks-MacBook-Pro-2019.local>
2022-05-16 15:17:10 -0700

    Versions 1 and 2 for submission

--- code/fran/python/lab15.py
+++ code/fran/python/lab15.py
@@ -5,10 +5,37 @@ import requests
 
 import pprint
 
-# Make API request
-response = requests.get('https://favqs.com/api/qotd', headers={'Accept': 'application/json'})
+# Initialize page variable, to be used as a request message element
+page_num = 1
 
-# pprint.pprint(response.json())        ### Determine response message data elements
-print('')
-print(response.json()['quote']['body'],'  -',response.json()['quote']['author']) 
-print('')
+while True:
+    quote_filter = input("Enter a keyword to search for quotes, or 'exit': ")
+
+    if quote_filter.lower() in ('exit','quit','done','q',''):
+        break
+    else:
+        while True:
+            # Make API request
+            # response = requests.get('https://favqs.com/api/qotd', headers={'Accept': 'application/json'})   # Version 1
+            response = requests.get('https://favqs.com/api/quotes', headers={'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"', 'Accept': 'application/json'}, params={'filter': quote_filter, 'page': page_num})
+
+            # pprint.pprint(response.json())   ### Determine response message data elements
+    
+            results = response.json()['quotes']     # list of dictionaries
+
+            # Display quotes to user
+            for result in results:
+                print('')
+                print(result['body'],'  -',result['author']) 
+                print('')
+
+            next_page = input(f"Enter 'next page' or 'done': ")
+
+            if next_page.lower() in ('next page','next','n'):
+                page_num += 1
+                continue
+            elif next_page.lower() in ('done','exit','quit','q'):
+                break
+            else:
+                break
+        
