487aa03794599101c598c254866b6fb7beab94c9
Zach-EE <zachholloway.n@gmail.com>
2022-05-16 14:54:53 -0400

    version 1 operating as intended

--- code/zach/python/lab15/lab15.py
+++ code/zach/python/lab15/lab15.py
@@ -1,2 +1,15 @@
 import requests
 
+def api_call():
+    r = requests.get('https://favqs.com/api/qotd')
+    data = dict(r.json())
+    return data
+
+def main():
+    qotd = api_call()
+    
+    print (f"\t\t{ qotd['quote']['body'] }\n\t\t~{ qotd['quote']['author'] }")
+   
+
+if __name__ == '__main__':
+    main()
