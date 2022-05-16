c8248aace9fc9d5b42bc999c433a77bf3998967f
Zach-EE <zachholloway.n@gmail.com>
2022-05-16 18:16:57 -0400

    quote generator fully functioning

--- code/zach/python/lab15/lab15.py
+++ code/zach/python/lab15/lab15.py
@@ -1,16 +1,52 @@
 import random
 import requests
 
+
+url = 'https://favqs.com/api/'
+
+
 def random_quote():
     r = requests.get('https://favqs.com/api/qotd')
     data = dict(r.json())
     return data
 
+
+def filter_quote():
+    page = 1
+    prompt = input('enter keyword:').lower().strip()
+    while True:
+        url = f'https://favqs.com/api/quotes?page={ page }&filter={ prompt }'
+        r = requests.get(url, headers={
+                         'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'})
+        data = dict(r.json())
+        print(
+            f'Showing { len(data["quotes"]) } results with keyword [{ prompt }] on page - { page }')
+
+        for item in data['quotes']:
+            print(f"-> '{ item['body'] }' \n- { item['author'] }\n")
+
+        print('------------------------------------------------------------------')
+
+        user_input = input('enter [n]ext,[s]earch, or [q]uit: ')
+
+        if user_input == 'q':
+            break
+        elif user_input == 'n':
+            page += 1
+        elif user_input == 's':
+            prompt = input('enter keyword:').lower().strip()
+        else:
+            print('bad input!!!')
+
+    return
+
+
 def main():
-    random_quote = random_quote()
-    
-    print (f"\t\t{ random_quote['quote']['body'] }\n\t\t~{ random_quote['quote']['author'] }")
-   
+    # # version 1:
+    # test_quote = random_quote()
+    # print (f"\t{ test_quote['quote']['body'] }\n\t~{ test_quote['quote']['author'] }")
+    filter_quote()
+
 
 if __name__ == '__main__':
     main()
