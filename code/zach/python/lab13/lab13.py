f042dc76a8a82d2d464cddf4491a0daeedda667a
Zach-EE <zachholloway.n@gmail.com>
2022-05-16 09:07:48 -0400

    starting fresh recreated Player and made a GameBoard class

--- code/zach/python/lab13/lab13.py
+++ code/zach/python/lab13/lab13.py
@@ -2,23 +2,44 @@ class Player:
     def __init__(self, name, token):
         self.name = name
         self.token = token
+        self.prompt = f'Player - { name } enter row and column [r,c]: '
 
-class Game(Player):
-    def __init__(self, board):
-        self.board = board
+    def get_move(self):
+        message = input(self.prompt)
+        row, col = [int(x) for x in message.split(',')]
+        return row, col
 
     def __repr__(self):
-        return
+        return self.name
 
-    def move(self, dx, dy, board):
-        return
 
-    def calc_winner(self):
-        return
+class GameBoard:
+    def __init__(self, size = 3, blank_space = '_' ):
+        self.size = size
+        self.data = [[blank_space for i in range (size)] for j in range(size)]
 
-    def is_full(self):
-        return
 
-    def is_game_over(self):
-        return
 
+
+# class Game:
+#     def __init__(self, board):
+#         self.board = []
+
+#     def __repr__(self):
+#         for i in range(3):
+
+#         return
+
+#     def move(self, dx, dy, player):
+#         return
+
+#     def calc_winner(self):
+#         return
+
+#     def is_full(self):
+#         return
+
+#     def is_game_over(self):
+#         return
+
+        
