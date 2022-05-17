bf7ee32f68770daa5446afa1a945a6967f670971
ICF19 <icfearn92@outlook.com>
2022-05-17 14:04:48 -0700

    lab 13 in progress

--- code/isaac/lab13.py
+++ code/isaac/lab13.py
@@ -1,16 +1,20 @@
 '''Tic-Tac-Toe'''
 # Create the game class
+class Player:
+    def __init__(self, player, token):
+        self.player = player
+        self.token = token
+
+
 class Game:
     def __init__(self):
         self.board = {'7': '', '8': '', '9': '',
                     '4': '', '5': '', '6': '',                                         
                     '1': '', '2': '', '3': ''}
     
-    def __repr__(self, board):  # this function will display the game board
-        self.board['7'] + '|' + self.board['8'] + '|' + self.board['9']
-        self.board['4'] + '|' + self.board['5'] + '|' + self.board['6']
-        self.board['1'] + '|' + self.board['2'] + '|' + self.board['3']
-        return repr(board)
+    def __repr__(self):  # this function will display the game board
+        tac_board = ' | | \n | | \n | |'
+        return tac_board
      
     # def move(self, x, y, player):
     #     x = 0
@@ -23,6 +27,6 @@ game_board = (repr(['7']) + '|' + repr(['8']) + '|' + repr(['9']),
 
 
 full_game = Game()
-print(full_game.__repr__(game_board))
+print(full_game.__repr__())
 
 
