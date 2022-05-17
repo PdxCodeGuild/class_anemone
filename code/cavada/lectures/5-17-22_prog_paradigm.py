d61ddfb208b457c6e571fd95551fff87c833bb08
avocado-jc <cavada.jonathon@gmail.com>
2022-05-17 11:21:03 -0700

    prog paradigm lecture

---
+++ code/cavada/lectures/5-17-22_prog_paradigm.py
@@ -0,0 +1,17 @@
+"""programmimg paradigms"""
+
+"""procedural imperative programming"""
+# mostly in the global?
+
+
+"""declarative/logical programming"""
+# declare what we want, lets computer create it
+
+"""functional programming"""
+
+"""object-oriented progamming"""
+# inheritance
+# encapsulation
+""""""
+
+""""""
