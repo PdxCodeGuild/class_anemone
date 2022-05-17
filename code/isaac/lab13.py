6ec0e3aa8d384397f3ac191e39368cf0b1eab22f
ICF19 <icfearn92@outlook.com>
2022-05-17 16:33:20 -0700

    lab 13 in progress

--- code/isaac/lab13.py
+++ code/isaac/lab13.py
@@ -8,25 +8,32 @@ class Player:
 
 class Game:
     def __init__(self):
-        self.board = {'7': '', '8': '', '9': '',
-                    '4': '', '5': '', '6': '',                                         
-                    '1': '', '2': '', '3': ''}
+        self.board = {'7': ' ', '8': ' ', '9': ' ',
+                      '4': ' ', '5': ' ', '6': ' ',                                         
+                      '1': ' ', '2': ' ', '3': ' '}
     
     def __repr__(self):  # this function will display the game board
-        tac_board = ' | | \n | | \n | |'
+        tac_board = f"{self.board['7']}|{self.board['8']}|{self.board['9']}\n{self.board['4']}|{self.board['5']}|{self.board['6']}\n{self.board['1']}|{self.board['2']}|{self.board['3']}"
         return tac_board
-     
-    # def move(self, x, y, player):
-    #     x = 0
-    #     y = 0 
-    #     return x, y, player
+    
+    def move(self, key, player):
+        self.board[key] = player.token
+    
+    # def calc_winner(self)
 
-game_board = (repr(['7']) + '|' + repr(['8']) + '|' + repr(['9']),
-              repr(['4']) + '|' + repr(['5']) + '|' + repr(['6']),
-              repr(['1']) + '|' + repr(['2']) + '|' + repr(['3']))
+    def is_full(self):
+        for key in self.board:
+            if self.board[key] == ' ':
+                return False
+        return True
+
+
+
 
 
 full_game = Game()
+player_1 = Player("Isaac", "X")
+full_game.move("7", player_1)
 print(full_game.__repr__())
 
 
