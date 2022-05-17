35878a37e62b1945e5f900e43d626016a7ac126f
Timothy Johnson <johnsonandtimothy@gmail.com>
2022-05-17 14:41:16 -0700

    timothy lab16

--- code/timothy/python/mini_capstone/mini_cap.py
+++ code/timothy/python/mini_capstone/mini_cap.py
@@ -35,9 +35,27 @@ class reSimple:
         for sample in samples:
             print(sample)
             playsound(self.filepath + '/' + sample)
+            next_sound = input("1. Edit \n2. Next Sample\n3. Exit\n").lower()
+            if next_sound == '2':
+                continue
+            elif next_sound == '3':
+                break
+            elif next_sound == '1':
+                return sample
+            else: print("Invalid Response")
+
+    def batch_rename(self):
+        i = 0
+        batch_name = input('New Batch Sample Name: ').lower()
+        for filename in os.listdir(self.filepath):
+            new_name = batch_name + str(i) + '.wav'
+            os.rename(os.path.join(self.filepath, filename), os.path.join(self.filepath, new_name))
+            i+=1
+        print(f"Samples batch renamed {batch_name}().wav")
 
 tim = reSimple()
 reSimple.folder_path(tim)
-# reSimple.folder_contents(tim)
-reSimple.sample_run(tim)
+reSimple.folder_contents(tim)
+# reSimple.sample_run(tim)
+# reSimple.batch_rename(tim)
 
