5c93858f747c831a6aedf2c797dcc5bec09b33a3
JasmineDee <jasminee.davies1@gmail.com>
2022-05-18 11:21:52 -0700

    lab 15 complete

--- code/jasmine/Python-Labs/lab15.py
+++ code/jasmine/Python-Labs/lab15.py
@@ -1,9 +1,13 @@
 
 
 import requests
+import pprint
 
 
-# ## get a Random Quote
+##get a Random Quote
+##Version 1
+
+
 # response = requests.get("https://favqs.com/api/qotd", headers = {"accept": "application/json"})
 # ##raw data
 # # print(response.text)
@@ -22,23 +26,50 @@ import requests
 
 
 ##List quotes by keyword
+##Version 2
+
+
+
+
+# #next-page
+# next_page = ''
 
 ## While loop for user to enter keyword until done
 while True:
 
+    ##pagination
+    page = 1
+
+
     user_keyword = input("Please enter a keyword that you would like to search for or 'quit': ")
+    while True:
+            
+        response = requests.get("https://favqs.com/api/quotes", headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'},params= {'filter': user_keyword,'page':page})
+        # print(response.text)
+        # url = "https://favqs.com/api/quote"
+        # response = requests.get(url,headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'})
+        result = response.json()['quotes']
+        # pprint.pprint(result)
 
-    url = "https://favqs.com/api/quotes?page=<page>&filter={user_keyword}"
-    new_response = requests.get(url,headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'})
-    result = new_response.json()
-    print(result)
+        ## response codes
+        
+        for quote in result:
+            author = quote['author']
+            tagline = quote['tags']
+            body = quote['body']
+            print(author,tagline,body)
 
-    if user_keyword == 'quit':
-        break
+        quit = input("Would you like to 'quit' or 'next page'? ")
 
-    test = []
+        if quit == 'quit':
+            break
+        elif quit == 'next page':
+            page += 1
+            continue
 
-    # for i in result:
-    #     new_quotes = i['body']
-    #     test.append(new_quotes)
-    # print(test)
+        # if user_keyword == 'quit':
+        #     break
+
+        # if user_keyword == 'next page':
+        #     page += 1
+
