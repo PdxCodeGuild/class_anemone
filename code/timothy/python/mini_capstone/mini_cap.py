ac5fcce88ac3bbab8b69319625bc4e85a7353a8d
Timothy Johnson <johnsonandtimothy@gmail.com>
2022-05-18 13:38:43 -0700

    timothy lab 16

--- code/timothy/python/mini_capstone/mini_cap.py
+++ code/timothy/python/mini_capstone/mini_cap.py
@@ -1,6 +1,6 @@
 # ....Function Playground for Mini Capstone Project ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
 
-# imports necessary ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
+# Necessary Imports
 import os
 from playsound import playsound
 import librosa
@@ -11,47 +11,51 @@ from librosa import beat
 # C:/Users/johns/pdx_code_guild/class_anemone/code/timothy/python/mini_capstone/samples/snares
 # C:/Users/johns/pdx_code_guild/class_anemone/code/timothy/python/mini_capstone/samples/breaks
 
-# class/funcs ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
+# reSimple - Renaming samples, simply.
 
 class reSimple:
     def __init__(self):
-        pass
-
-    def folder_path(self):
         self.filepath = input("Please enter the path to your sample folder: ")
-        return self.filepath
+
+    # def folder_path(self):
+    #     self.filepath = input("Please enter the path to your sample folder: ")
+    #     return self.filepath
 
     def folder_contents(self): # NEEDS NATURAL SORTING
         basepath = self.filepath
         for entry in os.listdir(basepath):
             if os.path.isfile(os.path.join(basepath, entry)):
                 print(entry)
+
+    def sample_rename(self, sample):
+        new_sample_name = input('Rename Sample: ').lower()
+        new_name = new_sample_name + '.wav'
+        os.rename(os.path.join(self.filepath, sample), os.path.join(self.filepath, new_name))
+        print(f'Sample renamed {new_name}')
+        return sample
     
-    def sample_rename(self):
+    def sample_player(self): #WinError 32 (file is being used), 'with open' somewhere? or split off new function for 'rename' to call within.
         repr(self.filepath)
         samples = os.listdir(self.filepath)
         for sample in samples:
             print(sample)
             playsound(self.filepath + '/' + sample)
-            name_check = input("Do you need to rename this sample? Y/N ")
-            if name_check == 'Y':
-                new_name = input("What would you like to rename this sample? ")
-                os.rename(os.path.join(self.filepath, sample), os.path.join(self.filepath, new_name))
-                continue
-            elif name_check == 'N':
-                continue
+            name_check = input("Do you need to rename this sample? Y/N ").lower()
+            if name_check == 'y':
+                self.sample_rename(sample)
         print(f'End of samples...')
 
-    def batch_rename(self):
-        i = 0
-        batch_name = input('New Batch Sample Name: ').lower()
-        for filename in os.listdir(self.filepath):
-            new_name = batch_name + '(' + str(i) + ')' + '.wav'
-            os.rename(os.path.join(self.filepath, filename), os.path.join(self.filepath, new_name))
-            i+=1
-        print(f"Samples batch renamed {batch_name}().wav")
+    def batch_rename(self): # WORKING
+            i = 0
+            batch_name = input('New Batch Sample Name: ').title()
+            for filename in os.listdir(self.filepath):
+                new_name = batch_name + '(' + str(i) + ')' + '.wav'
+                os.rename(os.path.join(self.filepath, filename), os.path.join(self.filepath, new_name))
+                i+=1     
+            print(f"Samples batch renamed {batch_name}().wav")
 
-    def tempo_finder(self):
+
+    def tempo_finder(self): # Runtime Error Opening & FileNotFound Errno 2
         repr(self.filepath)
         samples = os.listdir(self.filepath)
         for sample in samples:
@@ -62,8 +66,9 @@ class reSimple:
             print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
 
 tim = reSimple()
-reSimple.folder_path(tim)
+
+# reSimple.folder_path(tim) OBSOLETE
 # reSimple.folder_contents(tim)
-# reSimple.sample_rename(tim)
-# reSimple.batch_rename(tim)
-reSimple.tempo_finder(tim)
+# reSimple.sample_player(tim)
+reSimple.batch_rename(tim)
+# reSimple.tempo_finder(tim)
