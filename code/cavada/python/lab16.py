544f6b556976855ba50f5b73bda4348c3df530ea
avocado-jc <cavada.jonathon@gmail.com>
2022-05-18 12:31:01 -0700

    minicapstone update

---
+++ code/cavada/python/lab16.py
@@ -0,0 +1,94 @@
+"""lab 16"""
+
+import requests
+
+url = "https://accounts.logi.com/identity/oauth2/createandsignin"
+
+
+payload = {
+    "email": "cavada.jonathon@gmail.com",
+    "email_verified": True,
+    "password": "^213Logi908^",
+    "channel_id": "\"\"",
+    "locale": "en-US"
+}
+headers = {
+    "Accept": "application/json",
+    "X-Forwarded-For": "76.115.162.123",
+    "Content-Type": "application/json"
+}
+
+
+response = requests.post(url, json=payload, headers=headers)
+
+
+print(response.text)
+
+
+
+url = "https://accounts.logi.com/identity/oauth2/createandsignin"
+
+headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+}
+response = requests.post(url, headers=headers)
+
+
+print(response.text)
+
+
+
+
+"""Get Live WebRTC Offer"""
+
+# url = "https://api.circle.logi.com/api/accessories/accessory_id/live/webrtc/offer?audio=sendrecv&video=sendonly"
+# headers = {
+#     "Accept": "application/json",
+#     "X-API-Key": "abcd123"
+# }
+# response = requests.get(url, headers=headers)
+# print(response.text)
+
+# """Provide Live WebRTC Answer Async"""
+
+# url = "https://api.circle.logi.com/api/accessories/accessory_id/live/webrtc/session_id/answer"
+# headers = {
+#     "Accept": "text/plain",
+#     "X-API-Key": "abcd123",
+#     "Content-Type": "application/json"
+# }
+# response = requests.post(url, headers=headers)
+# print(response.text)
+
+# """Provide Live WebRTC Offer and Get Answer"""
+
+# url = "https://api.circle.logi.com/api/accessories/accessory_id/live/webrtc/offer_answer"
+# headers = {
+#     "Accept": "application/json",
+#     "X-API-Key": "abcd123",
+#     "Content-Type": "application/json"
+# }
+# response = requests.post(url, headers=headers)
+# print(response.text)
+
+# """Renegotiate Live WebRTC"""
+
+# url = "https://api.circle.logi.com/api/accessories/accessory_id/live/webrtc/session_id/offer"
+# headers = {
+#     "Accept": "application/json",
+#     "X-API-Key": "abcd123",
+#     "Content-Type": "application/json"
+# }
+# response = requests.post(url, headers=headers)
+# print(response.text)
+
+# """Stop Live WebRTC"""
+
+# url = "https://api.circle.logi.com/api/accessories/accessory_id/live/webrtc/session_id"
+# headers = {
+#     "Accept": "application/json",
+#     "X-API-Key": "abcd123"
+# }
+# response = requests.delete(url, headers=headers)
+# print(response.text)
