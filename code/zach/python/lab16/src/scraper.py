7235ed76c3ccd7cd70c7e5588730bbd6ce467a8f
Zach-EE <zachholloway.n@gmail.com>
2022-05-18 19:23:50 -0400

    added pandas df output/ going in different direction

--- code/zach/python/lab16/src/scraper.py
+++ code/zach/python/lab16/src/scraper.py
@@ -1,18 +1,16 @@
 import requests
+import pandas as pd
 
 def regular_season(season, seasonType):
-    reqUrl = f"https://site.web.api.espn.com/apis/common/v3/sports/football/nfl/statistics/byteam?region=us&lang=en&contentorigin=espn&sort=team.returning.yardsPerKickReturn%3Adesc&limit=32&season={ season }&seasontype={ seasonType }"
+    url = f"https://site.web.api.espn.com/apis/common/v3/sports/football/nfl/statistics/byteam?region=us&lang=en&contentorigin=espn&sort=team.returning.yardsPerKickReturn%3Adesc&limit=32&season={ season }&seasontype={ seasonType }"
 
     headersList = {
     "Accept": "*/*",
     }
 
-    payload = ""
+    r = requests.get(url,  headers=headersList)
+    season_data = r.json()
+    df = pd.json_normalize(season_data['teams'])
+    df.to_csv('season_data.csv', index=False)
 
-    response = requests.request("GET", reqUrl, data=payload,  headers=headersList)
-
-    print(response.text)
-
-
-
-regular_season(2011, 2)
+x = regular_season(2011, 2)
