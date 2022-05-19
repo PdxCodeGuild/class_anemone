77b4a79973b0aa668232aeb8e7809818cf0fe544
unknown <wfm366@gmail.com>
2022-05-19 09:34:35 -0600

    Lab10

--- code/will/Python/Lab/Lab10.py
+++ code/will/Python/Lab/Lab10.py
@@ -1,4 +1,5 @@
 import re
+from unicodedata import name
 list_of_list = []
 contacts = []
 
@@ -11,61 +12,98 @@ with open('contacts.csv', 'r') as file:
 
 #print(list_of_list)        
 
-key = list_of_list.pop(0)
+keys = list_of_list.pop(0)
 #Now that I have my list of lists I need to combine them to form their respective dictionary entries using the header with the other positions
 
 
 for item in list_of_list:
-    dict_1 = dict(zip(key, item))
+    dict_1 = dict(zip(keys, item))
     contacts.append(dict_1)
+
+print('1', contacts)
+
+
+
+def contacts_create(contacts, keys):
+    new_contact = {}
+    for key in keys:
+        new_contact[key] = input(f'What is your new contact {key}? ')
+    contacts.append(new_contact)
+    
+ 
+def contacts_delete(contacts):
+ 
+    #confirmation = contacts_read(contacts)
+    confirmation = input(f'Are you sure you want to delete {name}? ')
+    if confirmation == 'yes':
+        contacts.remove(name)
+        
+
+def contacts_update(contacts, name, keys):
+    result = contacts_read(contacts, name)
+
+    key_to_update = input (f"Which key would you like to update? {keys} ")
+    value_to_update = input(f'what do you want to change {key_to_update} to? ')
+
+    result[key_to_update] = value_to_update  
+    print(contacts)  
+    return contacts
+
+
+
+def contacts_read(contacts, name):
+    for contact in contacts:
+        if contact['name'] == name:
+            # for key in contact:
+            #    print(f'{key}:{contact[keys]}')
+            return contact
    
-print(contacts)
-
-
-
 
 #Here I am working on my REPL input command structure
 
 def start_menu():
+    #print(contacts)
     print("Type c to Create a contact")
     print("Type r to Retrieve a contact")
     print("Type u to Update a contact")
     print("Type d to delete a contact")
-    print("Type s to Save your contacts for export")
-    print("Type o to open a contact")
-    print("Type 'Help' to see this lsit again")
-    print("Type 'Exit' to exit program")
+   
 
 
     while True:
-        user_input = input("What function would you like to perform?")
+        user_input = input("What function would you like to perform? ")
         if user_input.upper == "exit":
             break 
 
         elif user_input == 'help':
             start_menu()
 
-        elif user_input == 'o':
-            file_select = input('Enter the filepath you want to access')
-            contacts.load(file_select)
-        
-        elif user_input == "s" :
-            contacts.save(file_select)
-
+    
         elif user_input == "d" :
-            name_delete = input('Enter the name of the contact you want to delete')
-            contacts.delete(name_delete)
+            name = input('Enter the name of the contact you want to delete ')
+            contacts_delete(name)
+            
 
         elif user_input == "u" :
-            contact_update = input('Enter the name of the contact you want to update')
-
+            name = input('Enter the name of the contact you want to update ')
+            contacts_update(contacts, name, keys)
+            
 
 
         elif user_input == "r" : 
-            retrieve_name = input('Enter the name of the contact you want to retreive: ')
-            contacts.read(retrieve_name)
+            name = input("What is your contact's name? ")
+            print(contacts_read(contacts, name))
+            
+            
 
         elif user_input == "c" :
-            create_contact = input("Enter the name of the contact you would like to create")
+            contacts_create(contacts, keys)
+            print(contacts)
+            
+
+data_csv_out = []
+data_csv_out.append(keys)
+for contact in contacts:
+    data_csv_out.append(list(contact.values()))
 
 start_menu()
