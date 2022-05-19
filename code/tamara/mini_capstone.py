f785d554b9ec4880ed7c2fa0236a952233e0bbb7
tamarajwood <tjwood270@gmail.com>
2022-05-19 16:31:34 -0700

    functional retrieves notable bird observations from past 30 days based on county and then can retrieve bird song recording based on user input. I would like to also find a way to retrieve bird image and information

--- code/tamara/mini_capstone.py
+++ code/tamara/mini_capstone.py
@@ -1,83 +1,139 @@
-import requests, pprint
+import requests
 from collections import Counter
 
-region_code = "US-WA-061" 
+### can also easily add on option to retrieve ALLL species EVER sighted in a region as fun additional option
+# create dict of 'user state input':'state code for API' to refer to
+state_code_dict = {
+    'alabama': 'US-AL', 
+    'alaska': 'US-AK', 
+    'arizona': 'US-AZ', 
+    'arkansas': 'US-AR', 
+    'california': 'US-CA', 
+    'colorado': 'US-CO', 
+    'connecticut': 'US-CT', 
+    'delaware':'US-DE', 
+    'florida':'US-FL', 
+    'georgia':'US-GA', 
+    'hawaii': 'US-HI', 
+    'idaho': 'US-ID', 
+    'illinois': 'US-IL', 
+    'indiana': 'US-IN', 
+    'iowa': 'US-IA', 
+    'kansas':'US-KS', 
+    'kentucky':'US-KY', 
+    'louisiana': 'US-LA', 
+    'maine': 'US-ME', 
+    'maryland': 'US-MD', 
+    'massachusetts': 'US-MA', 
+    'michigan':'US-MI', 
+    'minnesota':'US-MN', 
+    'mississippi':'US-MS', 
+    'missouri':'US-MO', 
+    'montana':'US-MT', 
+    'nebraska':'US-NE', 
+    'nevada':'US-NV', 
+    'new hampshire':'US-NH', 
+    'new jersey':'US-NJ', 
+    'new mexico':'US-NM', 
+    'new york':'US-NY', 
+    'north carolina':'US-NC', 
+    'north dakota':'US-ND', 
+    'ohio':'US-OH', 
+    'oklahoma':'US-OK', 
+    'oregon':'US-OR', 
+    'pennsylvania': 'US-PA', 
+    'rhode island': 'US-RI', 
+    'south carolina': 'US-SC', 
+    'south dakota':'US-SD', 
+    'tennessee': 'US-TN', 
+    'texas':'US-TX', 
+    'utah':'US-UT', 
+    'vermont': 'US-VT', 
+    'virginia':'US-VA', 
+    'washington':'US-WA', 
+    'west virginia':'US-WV', 
+    'wisconsin':'US-WI', 
+    'wyoming':'US-WY'
+}
 
-def regional_birds(region_code): # create method to tell you species of bird in a region
-    response=requests.get(f"https://api.ebird.org/v2/product/spplist/{region_code}", headers = { 'X-eBirdApiToken': 'f20hu1432los'})
-    # bird_species_list = response.json()
-    return response
+class Bird:
 
-regional_bird_species = regional_birds(region_code) # L7884500 works
+    def __init__(self, species):
+        self.species = species
 
-# pprint.pprint(regional_bird_species.text)
+    def species_song(self): # returns a single link for a recording of the species. Returns the download link. If you can find a way to use the download link to play the file through python then the 'file-name' key will give you the mp3 file name.
+        response=requests.get(f"https://www.xeno-canto.org/api/2/recordings", params = {'query': f'{self.species}', 'page':'1'})
+        species_songs_list = response.json()
+        species_song = species_songs_list['recordings'][0]['file']
+        return species_song ## can add a parameter for number of songs you want retrieved and then can retrieve multiple songs
+    
+    def species_image(self):
+        # response=requests.get()
+        pass ### this is for later when I can find a bird image api, can also make a bird info one as well when I find that kind of API
 
-# now create a method to show the information for a species of bird then you can create a for loop that takes
-# all the species of birds in a region and gives you there information
-# additional addons are converting a place name into > GPS > regional code somehow ish like that
-# also consider adding on bird song data and taxanomic info like pictures etc. for a species that is selected from the list.
-# you can also addon endangered species information from other API for bird species in the list
-# still need to import another thingy
-
-state_code = 'US-WA'
-
-def regional_codes(state_code): # create method to tell you species of bird in a region
+def regional_code(state_code, county):
     response=requests.get(f"https://api.ebird.org/v2/ref/region/list/subnational2/{state_code}", headers = { 'X-eBirdApiToken': 'f20hu1432los'})
-    # bird_species_list = response.json()
-    return response
+    regional_state_codes = response.json()
 
-regional_state_codes = regional_codes(state_code) # L7884500 works
+    subregion_code = ''
+    for region in regional_state_codes:
+        if county in region.values():
+            subregion_code += region['code']
 
-# pprint.pprint(f"\n{regional_state_codes.text}")
+    return subregion_code ### returns the code for the county enterted by the user and passed in as a paramter
 
-# OKAY so what you need to do is have user input a state and county which is converted into a state code via a dict
-# the state code is then entered into the regional_codes function and then the code for the given county is retrieved
-# this code can then be used to retrieve all of the species sighted in that region
-# then these species can be used to retrieve additional info about them
-# it may be easier if you turn this into some kind of Class
-
-# returns a LARGE number of bird species 100 + so just return the common names of the birds and asking the user which one they want more info about may be easiest
-# perhaps also organize them by COMMON sightings
-# can build on this later on - webpage with pictures, bird songs and information about birds sighted in specific regions
-        # can add conservation information, movement information etc.
-        # could add the abiity for someone to look up a species and then find what regions of the US they have been seen
-        # and list recent sightings
-# can even have someone enter a town and then convert that to the county that it is in
-
+# use to retrieve recent notable sightings in a region
 def region_sightings(subregion_code):
     response = requests.get(f"https://api.ebird.org/v2/data/obs/{subregion_code}/recent/notable?detail=full", headers = {'X-eBirdApiToken':'f20hu1432los'})
     sighting_list = response.json()
-    return sighting_list
 
-species_sightings = region_sightings("US-WA-061")
+    list_species = []
+    for sighting in sighting_list:
+        list_species.append(sighting['sciName'])
+    list_names = []
+    for sighting in sighting_list:
+        list_names.append(sighting['comName'])
+    species_names = dict(zip(list_names, list_species))
+    
+    number_sightings = Counter(list_names) 
+    number_sightings_sorted = sorted(number_sightings.items(), key=lambda x:x[1], reverse = True) 
+    number_sightings_dict = dict(number_sightings_sorted)
 
-# pprint.pprint(species_sightings.json())
+    return [number_sightings_dict, species_names]
 
-# print(species_sightings[0]['comName']) # returns mountain bluebird!!! 
+while True:
+    state = input(f"\nType the name of the state you would like to search for notable bird sightings in or type 'done' to exit: ").lower()
+    if state in ['n', 'done', 'exit', 'd']:
+        break
+    else:
+        try:
+            state_code = state_code_dict[state]
+        except KeyError:
+            print(f"\n'{state}' was an invalid state entry please check your spelling and try again.")
 
-# list_species_sighted = [item for item in species_sightings]
+    county = input(f"\nWhat county in {state.title()} would you like to search for notable bird sightings in? ").title()
 
-# print(list_species_sighted)
+    sightings = (region_sightings(regional_code(state_code, county)))[0] # gives dict of sightings in common name ranked by number of sightings
+    species_name = (region_sightings(regional_code(state_code, county)))[1] # gives dict of common name: species name // can be used to retrieve species info from other apis
 
-# figure out a way to get the local ID from an adress would make search more refined
+    print(f"\nIn {county} county over the past 30 days the following notable birds have been reported:") 
 
-list_species = []
+    for bird in sightings:
+        print(f"\n{bird}: seen {sightings.get(bird)} times")
 
-for sighting in species_sightings:
-    list_species.append(sighting['comName'])
-
-number_sightings_species = dict(Counter(list_species))
-
-print(sorted(number_sightings_species.items(), key=lambda x:x[1]), False) ## this works kind of to sort based on sightings
-
-# print(list_species) 
-# 
-# ## perhaps do a count of the number of sightings per species? can .pop the species
-# and add a counter and then perhaps add it to a set that will delete the extras
-# OR create a list of each species and add the sighting to the list
-# then count the list with len and .pop from each list or something
-
-# may need to pull the species name too to use for other data from other APIs
-# can create a dict with key: value that is common_name:species_name to pull from later
-
-# print(set(list_species))
+    while True:
+        common_name = input(f"\nWhich type of bird would you like more information on or type 'done' to try another regional search? ") # will need to .lower when looking through dict as well
+        if common_name in ['done', 'd', 'exit', 'cancel']:
+            break
+        else:
+            try:
+                species = species_name[common_name]
+            except KeyError:
+                print("That was an invalid entry. Please enter the bird's common name exactly as it is displayed.")
+        species_info = Bird(species)
+        print(f'\n{common_name} ({species}) song: {species_info.species_song()}')
+        another_bird = input(f"\nWould you like to look up the info for another bird sighted in {county} county (y/n)? ")
+        if another_bird in ['yes', 'y', 'yeah', 'yep']:
+            pass
+        else:
+            break
