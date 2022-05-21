e9e6e10093e19c11b55289371dbcbb6136c28182
Rachel Lloyd <rachel@Rachels-iMac.local>
2022-05-20 21:11:00 -0400

    Lab 16 - in progress

--- code/rachel/Python/Lab16.py
+++ code/rachel/Python/Lab16.py
@@ -51,12 +51,11 @@ while True:
         'player5' : 0,
 
         }
-    print('Welcome to Cards Against Humanity - Family Edition!\nTo exit the game at any time enter "quit".')
+    print('Welcome to Cards Against Humanity - Family Edition!\nTo exit the pick_card_page at any time enter "quit".')
 
     while True:
         #Empty Lists
         random_responses_list = []
-        funniest_responses = []
 
         #Pick random card from blackcards pile and show to player
         random_prompt = random.choice(blackcards)
@@ -76,6 +75,7 @@ while True:
         selected_card = input("\nSelect your funniest answer (1, 2, 3, 4, or 5): ")
         if selected_card == "quit":
             break
+        funniest_responses = []
         selected_card_index = funny_card[selected_card]
         funniest_card = random_responses_list.pop(selected_card_index)
         funniest_responses.append(funniest_card)
@@ -88,7 +88,7 @@ while True:
             funniest_responses.append(random_computer_responses)
         for i, card in enumerate(funniest_responses):
             i += 1
-            print (i, card)
+            print ('fr', i, card)
 
         #Have the card Czar pick the funniest card
         winning_card = random.choice(funniest_responses)
@@ -110,7 +110,7 @@ while True:
             players['player5'] += 1
         print(f'Player Points:\n-Player 1: {players["player1"]}\n-Player 2: {players["player2"]}\n-Player 3: {players["player3"]}\n-Player 4: {players["player4"]}\n-Player 5: {players["player5"]}')
 
-        #End game once one player reaches 5 points
+        #End pick_card_page once one player reaches 5 points
         game_done = False
         for player in players:
             if players[player] == 5:
@@ -138,24 +138,30 @@ clock = pygame.time.Clock()
 #Text Setup
 header_font = pygame.font.Font('basic_light.ttf', 50)
 card_font = pygame.font.Font('basic_light.ttf', 25)
-blackcard = pygame.Surface((1100, 325))
-blackcard.fill('black')
+top = pygame.Surface((1100, 325))
+top.fill('black')
 header = header_font.render('Cards Against Humanity', True, 'white')
 prompt = card_font.render(random_prompt, True, 'white')
-response = card_font.render(random_response, True, 'black')
+#response = card_font.render(random_responses_list, True, 'black')
 
 #Button Setup
 new_game_img = pygame.image.load('new_game_img.png').convert_alpha()
+one_img = pygame.image.load('one.png').convert_alpha()
+two_img = pygame.image.load('two.png').convert_alpha()
+three_img = pygame.image.load('three.png').convert_alpha()
+four_img = pygame.image.load('four.png').convert_alpha()
+five_img = pygame.image.load('five.png').convert_alpha()
 
 class Button():
     def __init__(self, x, y, image, scale):
         width = image.get_width()
         height = image.get_height()
         self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
-        self.image = image
+        #self.image = image
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
         self.clicked = False
+        print (width, height)
     
     def draw(self):
         #Allow for mouse clicks
@@ -166,60 +172,109 @@ class Button():
                 self.clicked = True
                 click = True
         if pygame.mouse.get_pressed()[0] == 0:
-            self.clicked == False
+            self.clicked = False
 
         #Add image on screen
         screen.blit(self.image, (self.rect.x, self.rect.y))
         
         return click
 
-new_game_button = Button (325, 300, new_game_img, 0.5)
+new_game_button = Button (325, 300, new_game_img, 1)
+one_button = Button(20, 350,  one_img, 0.35)
+two_button = Button(20, 390, two_img, 0.35)
+three_button = Button(23, 430, three_img, 0.35)
+four_button = Button(19, 468, four_img, 0.35)
+five_button = Button(24, 510, five_img, 0.35)
 
 #Next Screen Setup
 class GamePlay ():
     def __init__(self):
-        self.play = 'game'
+        self.play = 'intro'
     
     def intro (self):
+        #Allow game exit
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-        #Add header and start button
-        screen.fill('black')
+            screen.fill('black')
+            if new_game_button.draw():
+                self.play = 'pick_card_page'
+                #print(self.play)
         screen.blit(header, (290,150))
-        if new_game_button.draw():
-            #go to next screen
-            pass
 
         pygame.display.update()
 
-    def game(self):
+    def pick_card_page(self):
+        #Add game exit
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-        #Add header, prompts, and responses to game
-        screen.fill('white')
-        screen.blit(blackcard, (0,0))
+            screen.fill('white')
+            if one_button.draw():
+                funniest_responses.append(random_responses_list[0])
+                self.play = 'funniest_card_page'
+            if two_button.draw():
+                funniest_responses.append(random_responses_list[1])
+                self.play = 'funniest_card_page'
+            if three_button.draw():
+                funniest_responses.append(random_responses_list[2])
+                self.play = 'funniest_card_page'
+            if four_button.draw():
+                funniest_responses.append(random_responses_list[3])
+                self.play = 'funniest_card_page'
+            if five_button.draw():
+                funniest_responses.append(random_responses_list[4])
+                self.play = 'funniest_card_page'
+        #Add header, prompt, and whitecards to page
+        
+        screen.blit(top, (0,0))
         screen.blit(header, (290,20))
         screen.blit(prompt, (10, 150))
-        screen.blit(response, (10, 350))
+        
+        x = 70
+        y = 350
+        for response in random_responses_list:
+            response_position = (x, y)
+            response_display = card_font.render(response, True, 'black')
+            screen.blit(response_display, (response_position))
+            y += 40
 
         pygame.display.update()
     
-    def game_manager(self):
-        if self.play == 'intro':
-            self.intro
-        if self.play == 'game':
-            self.game
+    def funniest_card_page(self):
+        #Add game exit
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+        #Add header, prompt, and funniest cards to page
+        screen.fill('white')
+        screen.blit(top, (0,0))
+        screen.blit(header, (290,20))
+        screen.blit(prompt, (10, 150))
 
+        x = 70
+        y = 350
+        #print(funniest_responses)
+        for funny_response in funniest_responses:
+            funny_response_position = (x, y)
+            funny_response_display = card_font.render(funny_response, True, 'black')
+            screen.blit(funny_response_display, (funny_response_position))
+            y += 40
+            
+        pygame.display.update()
 
 game_play = GamePlay()
 while True:
-    game_play.intro()
+    if game_play.play == 'intro':
+        game_play.intro()
+    if game_play.play == 'pick_card_page':
+        game_play.pick_card_page()
+    if game_play.play == 'funniest_card_page':
+        game_play.funniest_card_page()
 
 
 
-    
     clock.tick(60)
