f175cfa281f513bb1be7b84b8b19678d1c296337
unknown <wfm366@gmail.com>
2022-05-23 08:25:15 -0600

    Uploading labs 12-15 to main

---
+++ code/will/Python/Lab/Lab15-main.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+search_term = input("Please enter a keyword for your quote search")
+
+response = requests.get('https://favqs.com/api/qotd',  headers= {'Accept' : 'application/json'})
+joke_response = response.text
+
+y = json.loads(joke_response)
+
+for result in y['results']:
+    print(result[search_term])
