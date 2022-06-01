9fbf117a2652c3ef1561e0b50056e2999c206f48
avocado-jc <cavada.jonathon@gmail.com>
2022-06-01 10:58:02 -0700

    update

--- code/Breakout Room 2/app.py
+++ code/Breakout Room 2/app.py
@@ -1,8 +1,22 @@
 from flask import Flask, render_template, redirect, request
 import json
+from jsondb import JsonDB
+
+
+"""
 import from jsondb import JsonDB
+db = JsonDB('db.json')
+db.load()
+x = db.get('x', 0)
+x += 1
+db.set('x', x)
+db.save()
+"""
+
 
 @app.route('/', methods=['GET', 'POST'])
+
 def index():
+
     return render_template("index.html")
 
