24257fa8a6156c003759d6607c596e43b99ee346
JasmineDee <jasminee.davies1@gmail.com>
2022-06-01 11:04:18 -0700

    template created

--- code/todo_group1/app.py
+++ code/todo_group1/app.py
@@ -3,6 +3,20 @@ from jsondb import JsonDB
 from flask import Flask, render_template, request, redirect
 
 
-## create the database json file
+app = Flask(__name__)
 
-## 
+db = JsonDB('db.json')
+db.load()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    tasks = db.get('todos')
+    if request.method == 'POST':
+        contact_name = request.form['input_text']
+        print(contact_name)
+        # handle data here
+        return redirect('/')
+    return render_template('index.html',tasks = tasks)
+
+
