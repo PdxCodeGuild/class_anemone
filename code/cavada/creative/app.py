f5b57e89d92870a6a82afe68792b10111e3cd501
avocado-jc <cavada.jonathon@gmail.com>
2022-06-01 09:49:15 -0700

    lab03 update

---
+++ code/cavada/creative/app.py
@@ -0,0 +1,21 @@
+from pyble import bible
+
+
+
+text = bible.get_chapter()
+
+print(text)
+
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index(text):
+    book= text
+    # name = 'Jack'
+    # temperature = 67
+     
+    return render_template('index.html', book=book)
+
+app.run(debug=True)
