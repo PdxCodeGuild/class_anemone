f7e79ac2ee52af0545cc11149f59849df2e63c48
unknown <wfm366@gmail.com>
2022-06-01 12:40:37 -0600

    Group Project update

--- code/Breakout Room 2/app.py
+++ code/Breakout Room 2/app.py
@@ -20,7 +20,17 @@ db.load()
 
 def index():
     to_do = db.get('todos')
+    if request.method == 'POST':
+        new_task = request.form['input_text']
+        db.set("todos", new_task)
+        db.save()
+        
+        return redirect('/')
+    
+        
+    
     return render_template("index.html", to_do = to_do)
+
 
 app.run(debug=True)
 
