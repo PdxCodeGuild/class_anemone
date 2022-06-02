efac2db05d31c760f24f9b24060a0599b455e3b4
JasmineDee <jasminee.davies1@gmail.com>
2022-06-02 16:33:03 -0700

    complete with errors

--- code/jasmine/HTML_CSS/Lab05/app.py
+++ code/jasmine/HTML_CSS/Lab05/app.py
@@ -1,31 +1,51 @@
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
-unit_convert = {
-    'meters': 1,
-    'feet': 0.3048,
-    'miles': 1609.34,
-    'kilometers': 1000,
-    'inches': 0.0254,
-    'yard': 0.9144
+# unit_convert = {
+#     'meters': 1,
+#     'feet': 0.3048,
+#     'miles': 1609.34,
+#     'kilometers': 1000,
+#     'inches': 0.0254,
+#     'yard': 0.9144
 
 
-}
+# }
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # if request.method == 'GET':
-    #     return render_template('index.html')
-    if request.method == 'POST':
+            
+    unit_convert = {
+        'meters': 1,
+        'feet': 0.3048,
+        'miles': 1609.34,
+        'kilometers': 1000,
+        'inches': 0.0254,
+        'yard': 0.9144
+
+
+    }
+
+    if request.method == 'GET':
+        print("GET working")
+        return render_template('index.html')
+
+
+    elif request.method == 'POST':
+        print("Hello")
         distance_amount = request.form['distance_amount']
         distance_amount = float(distance_amount)
-        meters = unit_convert(distance_amount)
-        return render_template('index.html', meters=meters)
-    else:
-        return render_template('index.html', meters='')
+        print(distance_amount)
+        old_units = request.form['input_units']
+        new_units = request.form['output_units']
+        meters_convert = distance_amount * unit_convert[old_units]
+        # meters_convert = float(meters_convert)
+        new_convert = round(meters_convert / distance_amount[new_units], 3)
+        return render_template('index.html', distance_amount=distance_amount, old_units=old_units,new_units=new_units, result=new_convert)
+   
 
 
 app.run(debug=True)
