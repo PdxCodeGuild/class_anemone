bdaab6f346673ec34b1916278d4fafcf17686886
Timothy Johnson <johnsonandtimothy@gmail.com>
2022-06-02 12:45:26 -0700

    html lab05

--- code/timothy/html/lab05/app.py
+++ code/timothy/html/lab05/app.py
@@ -1,43 +1,22 @@
-from flask import Flask, render_template
+import randoms
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
-import random
-import string
+object = randoms.Randoms()
 
-# def random_characters(length, alphabet):
-#     output = ''
-#     for i in range(length):
-#         char = random.choice(alphabet)
-#         output += char
-#     return output
-
-# def random_password(n_lowercase, n_uppercase, n_digits, n_punct):
-#     output = ''
-#     output += random_characters(n_lowercase, string.ascii_lowercase)
-#     output += random_characters(n_uppercase, string.ascii_uppercase)
-#     output += random_characters(n_digits, string.digits)
-#     output += random_characters(n_punct, string.punctuation)
-#     output = list(output)
-#     random.shuffle(output)
-#     output = ''.join(output)
-#     return output
-
-# print(random_password(20))
-
-def random_password(length):
-    characters = string
-    n = int(input('Enter password length: '))
-    password = ''
-    for i in range(n):
-        password += random.choice(characters)
-    return password
-
-
-
-@app.route('/')
+@app.route('/', methods=('GET', 'POST'))
 def index():
-    
+    # if request.method == 'POST':
+
     return render_template('index.html')
 
-
-
 app.run(debug=True)
+
+
+# RNDM GEN
+# uppers = int(input('How many Uppercase Chars in new password: '))
+# lowers = int(input('How many Lowercase Chars in new password: '))
+# nums = int(input('How many Number Chars in new password: '))
+# special = int(input('How many Special Chars in new password: '))
+
+# random_password(lowers, uppers, nums, special)
+
