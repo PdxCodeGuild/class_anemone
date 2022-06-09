36a63f2d69fbd18a2eec4020e156af1588d7cf22
Keegan Good <keegood8@gmail.com>
2022-06-08 17:22:42 -0700

    created Question and Polls models and registered them in admin

--- code/merritt/22-apr/polls-22apr/polls/admin.py
+++ code/merritt/22-apr/polls-22apr/polls/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+# import models that we want to register
+from .models import Question, Choice
+
+# register the models in the admin panel
+admin.site.register([Question, Choice])
