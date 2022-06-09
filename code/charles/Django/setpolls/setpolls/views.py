400b94faa2f68d9a3ab8058890c95377b388cfbb
Adeeon <cgt10dt@gmail.com>
2022-06-09 18:29:27 -0500

    django/setup

---
+++ code/charles/Django/setpolls/setpolls/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from .models import Question
+from django.shortcuts import render, get_object_or_404
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'setpolls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'setpolls/detail.html', {'question':question})
+
+def results(request, question_id):
+    return HttpResponse(f"This is the results of question number {question_id}.")
+
+def vote(request, question_id):
+    return HttpResponse(f"You are voting on question number {question_id}.")
