5a04791e23596a51fc9b7a6255f8c78e5919e825
Merritt Lawrenson <merrittlawrenson@gmail.com>
2022-06-09 16:31:28 -0700

    more django example

--- code/merritt/22-apr/polls-22apr/polls/views.py
+++ code/merritt/22-apr/polls-22apr/polls/views.py
@@ -1,7 +1,21 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
 
 def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
 
-    # questions = Question.objects.all()
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
-    return HttpResponse("Hello world!")
+def results(request, question_id):
+    return HttpResponse(f"This is the results of question number {question_id}.")
+
+def vote(request, question_id):
+    return HttpResponse(f"You are voting on question number {question_id}.")
