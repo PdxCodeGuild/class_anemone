0a5fe7a4a09552a5e1559dda2f8a4a3269cdad89
tamarajwood <tjwood270@gmail.com>
2022-06-14 16:36:36 -0700

    lab 1 in progress

--- code/tamara/lab_1_django/grocery_list/models.py
+++ code/tamara/lab_1_django/grocery_list/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 class GroceryItem(models.Model):
-    grocery_text = models.CharField(max_length=50)
-    date_created = models.DateTimeField(verbose_name='date created')
-    date_completed = models.DateTimeField(verbose_name='date completed')
+    grocery_text = models.CharField(max_length=50, verbose_name = 'Grocery item')
+    date_created = models.DateTimeField(verbose_name='Date created')
+    date_completed = models.DateTimeField(null=True, blank=True, verbose_name='Date completed')
+    is_complete = models.BooleanField(verbose_name = 'Competed')
+
+    def __str__(self):
+        return self.grocery_text
+
+# superuser username tjwoo
--- code/tamara/lab_1_django/grocery_list/views.py
+++ code/tamara/lab_1_django/grocery_list/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from .models import GroceryItem
 # from .models import classes here
 # check to make sure you are actully using these imports for this app
 
 def index(request):
-    context = {'':''}
+    grocery = GroceryItem.objects.all()
+    context = {'grocery':grocery}
     # replace with whatever variable you need?
     return render(request, 'grocery_list/index.html', context)
 
