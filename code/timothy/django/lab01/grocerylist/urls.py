a197f61114d6b25560bbec380c267ac56654a25d
Timothy Johnson <johnsonandtimothy@gmail.com>
2022-06-14 16:37:01 -0700

    django progress

--- code/timothy/django/lab01/grocerylist/urls.py
+++ code/timothy/django/lab01/grocerylist/urls.py
@@ -5,4 +5,8 @@ app_name = 'grocerylist'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('complete/<int:grocery_item_id>/', views.complete, name='complete'),
+    path('delete/<int:grocery_item_id>/', views.delete, name='delete'),
+    path('create/', views.create, name='create'),
+    # path('<int:grocery_item_id>/update', views.update, name='update'),
 ]
--- code/timothy/django/lab01/grocerylist/views.py
+++ code/timothy/django/lab01/grocerylist/views.py
@@ -1,11 +1,43 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import GroceryItem
 
 def index(request):
     latest_grocerylist = GroceryItem.objects.order_by('-date_created')
+    not_complete = latest_grocerylist.filter(is_done = False)
+    complete = latest_grocerylist.filter(is_done = True)
+    # grocery_item = get_object_or_404(GroceryItem, pk=grocery_item.id)
     context = {
-        'latest_grocerylist': latest_grocerylist
+        'latest_grocerylist': latest_grocerylist,
+        'not_complete': not_complete,
+        'complete': complete,
+        # 'grocery_item':grocery_item,
     }
-    print(latest_grocerylist)
     return render(request, 'grocerylist/index.html', context)
+
+def create(request):
+    context = {}
+    form = GroceryItem(request.POST or None)
+    if form.is_valid():
+        form.save
+    context['form'] = form
+    return HttpResponseRedirect(reverse('grocerylist:index'))
+
+def complete(request, grocery_item_id):
+    # if request.method == 'post':
+    print(grocery_item_id)
+    grocery_item = get_object_or_404(GroceryItem, pk=grocery_item_id)
+    if grocery_item.is_done == False:
+        grocery_item.is_done = True
+    else: 
+        grocery_item.is_done = False
+    grocery_item.save()
+    return HttpResponseRedirect(reverse('grocerylist:index'))
+
+def delete(request, grocery_item_id):
+    grocery_item = get_object_or_404(GroceryItem, pk=grocery_item_id)
+    if request.method == "POST":
+        grocery_item.delete()
+    return HttpResponseRedirect(reverse('grocerylist:index'))
+
