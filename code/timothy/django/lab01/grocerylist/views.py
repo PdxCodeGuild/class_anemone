1040fe0104d850dac6f57fb644d46de2b934ceb9
Timothy Johnson <johnsonandtimothy@gmail.com>
2022-06-14 10:54:12 -0700

    list works!

--- code/timothy/django/lab01/grocerylist/views.py
+++ code/timothy/django/lab01/grocerylist/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import GroceryItem
 
 def index(request):
-    latest_grocerylist = GroceryItem.objects.order_by('-pub_date')
+    latest_grocerylist = GroceryItem.objects.order_by('-date_created')
     context = {
         'latest_grocerylist': latest_grocerylist
     }
+    print(latest_grocerylist)
     return render(request, 'grocerylist/index.html', context)
