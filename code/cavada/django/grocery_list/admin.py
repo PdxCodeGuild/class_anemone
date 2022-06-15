d4b0b0213c6f5bccf36ca9b34d254022de08a6e9
avocado-jc <cavada.jonathon@gmail.com>
2022-06-14 22:08:24 -0700

    update

--- code/cavada/django/grocery_list/admin.py
+++ code/cavada/django/grocery_list/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from grocery_list.models import GroceryItem
+
+admin.site.register(GroceryItem)
--- code/cavada/django/grocery_list/views.py
+++ code/cavada/django/grocery_list/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.utils import timezone
 
 # Create your views here.
+from .models import GroceryItem
+
+def index(request):
+    grocery_list= GroceryItem.objects.all()
+    context = {
+        'grocery_list': grocery_list
+    }
+    if 'new' in request.POST:
+        new_item = request.POST['new']
+        g = GroceryItem(desc=new_item,date_created = timezone.now(),status=False, date_fulfilled = None)
+        g.save()
+        grocery_list= GroceryItem.objects.all()
+        context = {
+            'grocery_list': grocery_list
+        }
+    elif 'item' in request.POST:
+        id = request.POST['item']
+        print(id)
+        g = GroceryItem.objects.get(pk=id)
+        g.status=True
+        g.date_fulfilled=timezone.now()
+        g.save()
+        grocery_list= GroceryItem.objects.all()
+        context = {
+            'grocery_list': grocery_list
+        }
+    else:
+        id = request.POST['delete']
+        print(id)
+        g = GroceryItem.objects.get(pk=id)
+        g.delete()
+        grocery_list= GroceryItem.objects.all()
+        print(grocery_list)
+        context = {
+            'grocery_list': grocery_list
+        }
+    return render (request, 'grocery_list/index.html', context)
+
+
+
+# def new_item(request):
+    
+
+# def vote(request, question_id):
+    # question = get_object_or_404(Question, pk=question_id)
+    # try:
+    #     selected_choice = question.choices.get(pk=request.POST['choice'])
+    # except (KeyError, Choice.DoesNotExist):
+    #     return render(request, 'polls/detail.html', {
+    #         'question': question,
+    #         'error_message': "Please select a choice."
+    #     })
+    # selected_choice.votes += 1
+    # selected_choice.save()
+    # return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
