486d3adfc0b52ca6a4314bc9e8c55629d89d914f
Zach-EE <zachholloway.n@gmail.com>
2022-06-16 19:49:01 -0400

    added template.html for the application, added bootstrap for styling

--- code/zach/django/lab01/grocery_list/views.py
+++ code/zach/django/lab01/grocery_list/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import GroceryItem
 # Create your views here.
+def index(request):
+    item_list = GroceryItem.objects.all()
+    context = {
+        'item_list' : item_list
+    }
+    return render(request, 'grocery_list/index.html', context)
