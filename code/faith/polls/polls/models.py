9fe2b12211c81291c7528963b02a9bb140f348ca
Faith Cicero <faithmates98@gmail.com>
2022-06-17 14:42:09 -0600

    polls

--- code/faith/polls/polls/models.py
+++ code/faith/polls/polls/models.py
@@ -1,11 +1,20 @@
 from django.db import models
-
+import datetime
+from django.utils import timezone
 # Create your models here.
+pub_date = models.DateTimeField(verbose_name = 'date published')
+
+question_text = models.CharField(max_length=255)
+
 class Question(models.Model):
     question_text = models.CharField(max_length=255)
     pub_date = models.DateTimeField(verbose_name='date published')
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        return timezone.now()>=self.pub_date >= timezone.now()-datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choices')
@@ -14,4 +23,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.question.question_text+'-' + self.choice_text
-    
+   
--- code/faith/polls/polls/tests.py
+++ code/faith/polls/polls/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+
+from .models import Question
+from django.utils import timezone
+import datetime
+from django.urls import reverse
+
+def create_question(question_text, days):
+    time = timezone.now( + datetime.timedelta(days=days))
+    return Question.objects.create(question_text,pub_date=time )
+
+
+class QuestuinModelTest(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_publsihed_recently()
+        """
+        time = timezone.now()+ datetime.timedelta(days=30)
+        future_question = Question(pub_date = time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_publsihed_recently_with_old_question(self):
+        time = timezone.now()- datetime.timedelta(days=1,seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_publsihed_recently_with_recent_question(self):
+        time = timezone.now()- datetime.timedelta(hours=23,minutes=59,seconds=1)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
+
+class IndexViewTests(TestCase):
+    def test_no_questions(self):
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+        
