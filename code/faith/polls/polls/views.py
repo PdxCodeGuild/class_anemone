ccd030a7adb46ef4a8ad5ad744ccc5ea73f21d79
Faith Cicero <faithmates98@gmail.com>
2022-06-17 15:49:33 -0600

    polls

--- code/faith/polls/polls/views.py
+++ code/faith/polls/polls/views.py
@@ -13,12 +13,12 @@ def index(request):
 
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    question = get_object_or_404(Question.objects.filter(pub_date__lte=timezone.now()), pk=question_id)
     return render(request, 'polls/detail.html', {'question':question})
 
 
 def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    question = get_object_or_404(Question.filter(pub_date__lte=timezone.now()), pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
 
 
