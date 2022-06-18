c3cdf20263a0fe046d646d6706d5c036be08a359
Timothy Johnson <johnsonandtimothy@gmail.com>
2022-06-17 18:46:04 -0700

    ugh

--- code/timothy/django/short_url/shurl/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'shurl'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('shorten/<int:short_url_id>/', views.shorten, name='shorten'),
-]
