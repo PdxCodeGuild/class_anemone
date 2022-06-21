bb74b5ff4983c495b80f9ce28ee479f33924fb91
Zach-EE <zachholloway.n@gmail.com>
2022-06-21 10:36:40 -0400

    bad commit

--- code/zach/django/lab02/lab02/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-import url_short.views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('url_short.urls'))
-]
