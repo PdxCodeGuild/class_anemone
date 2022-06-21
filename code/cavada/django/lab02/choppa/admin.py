63e16181ffae7b3d08aad5869e1a408273891b65
avocado-jc <cavada.jonathon@gmail.com>
2022-06-21 13:35:23 -0700

    update

--- code/cavada/django/lab02/choppa/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from choppa.models import UrlChoppa
-
-admin.site.register(UrlChoppa)
--- code/cavada/django/lab02/choppa/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChoppaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'choppa'
