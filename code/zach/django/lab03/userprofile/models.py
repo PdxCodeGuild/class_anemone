23cb5e208afb19b8ca9157b965a8f71c3323b42e
Zach-EE <zachholloway.n@gmail.com>
2022-06-23 14:45:25 -0400

    added user model

--- code/zach/django/lab03/userprofile/models.py
+++ code/zach/django/lab03/userprofile/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    fname = models.CharField(max_length=200)
+    lname = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=200)
+    age = models.IntegerField()
