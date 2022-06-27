6dff66079a4f9ac52dfc812897f1071ed37afa87
Adeeon <cgt10dt@gmail.com>
2022-06-27 17:23:58 -0500

    lab3wip

--- code/charles/Django/lab3/Twitter_Clone_called_Chipper/posts/views.py
+++ code/charles/Django/lab3/Twitter_Clone_called_Chipper/posts/views.py
@@ -1,18 +1,31 @@
-from django.views.generic import ListView
+from django.shortcuts import render, get_list_or_404
+from django.urls import reverse
 from django.http import HttpResponseRedirect
 from .models import Posts
+from django.contrib.auth.models import User
 
 
-class HomePageListView(ListView):
-    model = Posts
-    template_name = "home_page.html"
-        
-    def get_queryset(self):
-        return Posts.objects.order_by('-created')
+def home_page(request):
+    posts= Posts.objects.order_by('-created')
+    context = {
+        'posts':posts
+    }
+    return render(request, 'home_page.html', context)
 
-
-def newpost(request, user.id):
-  
+def newpost(request):
+    title=request.POST['title']
+    author= User.username.id
+    body=request.POST['body']
+    created=request.POST['created']
+    updated_last = request.POST['updated_last']
+    new = Posts.objects.create(title=title, author=author, body=body, created=created, updated_last=updated_last)
+    
+def edited(request, postid):
+        title = request.POST['title']
+        author=User.username.id
+        body=request.POST['body']
+        updated_last= request.POST['updated_last']
+        edit =    
 
 
 # Create your views here.
