46b60b7d7f64cd18b1f8526aa40667fa9dd498d8
Frances Kappes <fekmacbookpro2019@Feks-MacBook-Pro-2019.local>
2022-06-27 00:46:39 -0700

    progress

--- code/fran/django/lab03/posts/views.py
+++ code/fran/django/lab03/posts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Post
+      
+class ChirpListView(ListView):  
+    model = Post
+    template_name = 'home.html'
+
+class ChirpDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
