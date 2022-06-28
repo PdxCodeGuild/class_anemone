5a73c95fdaef1e3c79c4ec33a346175b9ec422b4
Adeeon <cgt10dt@gmail.com>
2022-06-28 16:23:11 -0500

    wip

--- code/charles/Django/lab3/Twitter_Clone_called_Chipper/posts/views.py
+++ code/charles/Django/lab3/Twitter_Clone_called_Chipper/posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import get_object_or_404, render, get_list_or_404
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from .models import Posts
@@ -20,12 +20,31 @@ def newpost(request):
     updated_last = request.POST['updated_last']
     new = Posts.objects.create(title=title, author=author, body=body, created=created, updated_last=updated_last)
     
-def edited(request, postid):
+def edited(request, postid, userid):
+    user = get_object_or_404(User, pk=userid)
+    post = get_object_or_404(Posts, pk=postid)
+    if user != post.author:
+        error_message = "You do did not create this so cannot edit it."
+        na = {
+            'error_message':error_message
+        }
+        return render(request, 'posts:home_page.html', na)
+    elif user == post.author:
         title = request.POST['title']
-        author=User.username.id
-        body=request.POST['body']
-        updated_last= request.POST['updated_last']
-        edit =    
+        body = request.POST['body']
+        updated_last = request.POST['updated_last']
+
+        post.title = title
+        post.body = body
+        post.updated_last = updated_last
+        post.save()
+
+def userprofile(request, reuserid):
+    posts=Posts.objects.order_by('author')
+    user = get_object_or_404(User, pk=reuserid)
+    pots = 
+    for post in posts:
+        if posts.author == user.username:
 
 
 # Create your views here.
