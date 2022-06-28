56f0ff7b7fdf373ee708dc4c828dcca38729812d
bakerbailey6 <bakerbailey9@gmail.com>
2022-06-28 16:09:32 -0700

    added complete lab 1

---
+++ code/bailey/JavaScript/lab_1/lab_1.js
@@ -0,0 +1,44 @@
+var card = prompt("Please input credit card number: ");
+
+var cardArr = String(card).split("").map((card)=>{
+    return Number(card)
+  })
+
+console.log(cardArr)
+
+check_digit = cardArr.pop();
+
+console.log(check_digit)
+
+cardArr.reverse();
+
+console.log(cardArr);
+
+for (let i = 0; i < cardArr.length; i+=2){
+    cardArr[i] = cardArr[i] * 2
+}
+
+console.log(cardArr)
+
+for (let i=0; i<cardArr.length; i++){
+    if (cardArr[i] > 9){
+        cardArr[i] = cardArr[i] - 9
+    }
+}
+
+console.log(cardArr)
+
+let total = 0
+
+for (let i = 0; i<cardArr.length; i++){
+    total += cardArr[i]
+}
+console.log(total)
+total = total % 10
+console.log(total)
+
+if (total == check_digit){
+    alert("True, Valid!")
+}else{
+    alert("Card number not valid")
+}
