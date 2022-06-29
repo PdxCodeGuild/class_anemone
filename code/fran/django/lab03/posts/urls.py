f3bd4f8dcfa7a3b11f29ac0860974da4927329f1
Frances Kappes <fekmacbookpro2019@Feks-MacBook-Pro-2019.local>
2022-06-29 16:09:49 -0700

    more progress

--- code/fran/django/lab03/posts/urls.py
+++ code/fran/django/lab03/posts/urls.py
@@ -7,7 +7,7 @@ app_name = 'posts'
 urlpatterns = [
     path('', views.ChirpListView.as_view(), name='home'),
     path('post/<int:pk>/', views.ChirpDetailView.as_view(), name='detail'),
-    # path('post/new/', views.ChirpCreateView.as_view(), name='new'),
+    path('post/new/', views.ChirpCreateView.as_view(), name='new'),
     # path('post/<int:pk>/edit/', views.ChirpEditView.as_view(), name='edit'),
     # path('post/<int:pk>/delete/', views.ChirpDeleteView.as_view(), name='delete'),
 ]
--- code/fran/django/lab03/posts/views.py
+++ code/fran/django/lab03/posts/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Post
       
@@ -10,3 +12,11 @@ class ChirpDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
+class ChirpCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['body']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
