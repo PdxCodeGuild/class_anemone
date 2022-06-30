f824fc2b8f768998a44413767fc4204a65806055
Rachel Lloyd <rlloyd98@gmail.com>
2022-06-30 18:55:02 -0400

    Lab1-3 part B - complete

---
+++ code/rachel/JavaScript/lab1-3/lab1-b/apps.js
@@ -0,0 +1,36 @@
+let nums = document.getElementsByClassName('num')
+let addNumBtn = document.getElementById('add-num')
+let numDiv = document.getElementById('num-div')
+let calcAvgBtn = document.getElementById('calc-avg')
+let results = document.getElementById('results')
+
+addNumBtn.addEventListener('click', function() {
+    let newNum = document.createElement('input')
+    newNum.type = 'number'
+    newNum.classList.add('num')
+
+    let newNumRemoved = document.createElement('button')
+    newNumRemoved.innerText = 'x'
+    newNumRemoved.addEventListener('click', function(){
+        newNum.remove()
+        newNumRemoved.remove()
+    })
+
+    numDiv.appendChild(newNum)
+    numDiv.appendChild(newNumRemoved)
+})
+
+calcAvgBtn.addEventListener('click', function() {
+    let answer = 0
+    for (let i=0; i<nums.length; i++) {
+        if (nums[i].value === "") {
+            nums[i].valie = 0
+        }
+        answer += parseInt(nums[i].value)
+        avg_answer = answer / nums.length
+    }
+
+    let result = document.createElement('r')
+    result.innerText = avg_answer
+    results.prepend(result)
+})
---
+++ code/rachel/JavaScript/lab1-3/lab2-b/apps.js
@@ -0,0 +1,34 @@
+let cardsDiv = document.getElementById('cards-div')
+let cards = document.getElementsByClassName('card')
+let adviceBtn = document.getElementById('advice')
+let results = document.getElementById('results')
+let card1 = document.getElementById('card1')
+let card2 = document.getElementById('card2')
+let card3 = document.getElementById('card3')
+
+adviceBtn.addEventListener('click', function(){
+    let total = parseInt(card1.value) + parseInt(card2.value) + parseInt(card3.value)
+    console.log(total)
+
+    if (total < 17) {
+        let hitResult = document.createElement('hit')
+        hitResult.innerText = `${total} - Hit`
+        results.prepend(hitResult)
+    } else if (total >= 17) {
+        if (total >21) {
+            let bustedResult = document.createElement('busted')
+            bustedResult.innerText = `${total} - "Already Busted"`
+            results.prepend(bustedResult)
+        }
+        if (total < 21) {
+            let stayResult = document.createElement('stay')
+            stayResult.innerText = `${total} - "Stay"`
+            results.prepend(stayResult)
+        }
+        if (total === 21) {
+            let blackjackResult = document.createElement('blackjack')
+            blackjackResult.innerText = `${total} - "Blackjack!"`
+            results.prepend(blackjackResult)
+        }
+    }
+})
