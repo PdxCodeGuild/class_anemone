6ddfbf0cb5239dd300ce0aca0f25a3eac3643329
Jonathan meadows <jon_meadows@ymail.com>
2022-06-30 14:23:10 -0500

    lab01 3/3 -CC validator- Done

--- code/meadows/java03/myscript3.js
+++ code/meadows/java03/myscript3.js
@@ -3,32 +3,45 @@ var card = prompt("Enter Credit Card Number: ")
 let myFunc = num => Number(num)
 
 var intArr = Array.from(String(card), myFunc)
+var intArr2 = [...intArr]
 
-hold = []
-hold = intArr.pop()
+let hold = intArr.pop()
 
-Reverse = intArr.reverse()
+let Reverse = intArr2.reverse()
 
-// for (let i = 0; i < Reverse.length; i++) {
-//     console.log(Reverse[i] *= 2)
-// }
-
-function everyOther(ccArray) {
-    for (var i = 1; i < ccArray.length; i+=2) {
-        ccArray[i] *= 2;
+function everyOther(multArray) {
+    for (var i = 1; i < multArray.length; i+=2) {
+        multArray[i] *= 2
     }
-    return ccArray;
+    return multArray
 }
 
-doubleReverse = everyOther(Reverse)
+let doubleReverse = everyOther(Reverse)
+let dubReverse = []
 
-function DivNum(divArray) {
-    for (var i = 0; i < divArray.lenght; i++) {  // not subtracting
-        if (i >= 10){
-            divArray - 9;
+let Reversedub = dubReverse.concat(doubleReverse)
+
+function SubNum(subArray) {
+    for (var i = 0; i < subArray.length; i++) {
+        if (subArray[i] >= 10){
+            subArray[i] = subArray[i] - 9
         }
     }
-    return divArray;
+    return subArray
 }
 
-console.log(DivNum(doubleReverse))
+let subNum = SubNum(Reversedub)
+
+let sum=0
+for(let i = 0; i < subNum.length; i++){
+    sum += subNum[i]
+}
+
+let numSum = sum / intArr2.length
+
+if ( Math.floor(numSum)===hold) {
+    alert("Your Credit Card is Valid")
+}
+else {
+    alert("Your Credit Card is NOT valid")
+}
