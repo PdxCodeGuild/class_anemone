c99e8e9c093c35f86ec6f283aeb6574128d6df7b
Timothy Johnson <johnsonandtimothy@gmail.com>
2022-07-01 15:20:37 -0700

    'practice'

--- code/timothy/javascript/todo_list/assets/todo.js
+++ code/timothy/javascript/todo_list/assets/todo.js
@@ -6,13 +6,12 @@ let completeList = document.getElementById("completeList")
 
 let todos = []
 
-function createTodos(text) {
+createButton.addEventListener('click', function() {
     todo = {
         text,
         checked: false,
         id: Date.now(),
     }
     todos.push(todo)
-    console.log(todos)
-}
-
+    console.log(todos
+})
