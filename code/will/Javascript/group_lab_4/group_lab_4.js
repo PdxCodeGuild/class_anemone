8c7a54824c5fb4da7f4d753de25741c738c81e5e
Rachel Lloyd <rlloyd98@gmail.com>
2022-07-01 16:42:03 -0400

    group lab 4

--- code/will/Javascript/group_lab_4/group_lab_4.js
+++ code/will/Javascript/group_lab_4/group_lab_4.js
@@ -15,30 +15,30 @@ add_button.addEventListener('click', function(){
     printItem.innerText = item
     itemsDiv.prepend(printItem)
 
+    let completeBtn = document.createElement('button')
+    completeBtn.innerText = "Complete"
+    completeBtn.addEventListener('click', function(){
+        let completeItem = to_do_list.splice(to_do_list.indexOf(item), 1)
+        completeList.push(completeItem)
+        printItem.remove()
+        removeItem.remove()
+        completeBtn.remove()
+
+        let displayItem = document.createElement('p')
+        displayItem.innerText = item.strike()
+        completeDiv.prepend(displayItem) 
+    })   
+
+    itemsDiv.appendChild(completeBtn)
+
     let removeItem = document.createElement('button')
     removeItem.innerText = "Delete"
     removeItem.addEventListener('click', function(){
         printItem.remove()
         removeItem.remove()
+        completeBtn.remove()
     })
 
     itemsDiv.appendChild(removeItem)
-
-    let completeBtn = document.createElement('button')
-    completeBtn.innerText = "Complete"
-    completeBtn.addEventListener('click', function(){
-        let completeItem = to_do_list.splice(to_do_list.indexOf(item), 1)
-        console.log(completeItem)
-        completeList.push(completeItem)
-        console.log(completeList)
-    })
-    itemsDiv.appendChild(completeBtn)
-    
-    
-    
 })
-// need to figure this out
-let displayItem = document.createElement('p')
-displayItem.innerText = item
-completeDiv.prepend(displayItem)
 
