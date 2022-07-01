041d9093c3be17a8068c53662415062570dac976
Ollie's Macbook Pro <ojc@Ollies-MacBook-Pro.local>
2022-07-01 11:15:46 -0700

    jcot

--- code/cjot Java Group Lab/lab_4.js
+++ code/cjot Java Group Lab/lab_4.js
@@ -1,24 +1,36 @@
-let delta = document.getElementById('delete')
-let omega = document.getElementById('create')
-let mark = document.getElementById('completed')
+// let delta = document.getElementById('delete')
+// let omega = document.getElementById('create')
+// let mark = document.getElementById('completed')
+let nitems = document.getElementsByClassName('nitem')
+let itemDiv = document.getElementById('todoitem')
 let Confirm = document.getElementById('submit')
 
-
 Confirm.addEventListener('click', function() {
-    let todoitem=document.getElementById('item')
-    let completed=document.getElementById('citem')
-    let newitem=document.getElementById('nitem')
-    console.log(todoitem, completed, newitem)
-    
-    delta.addEventListener('click', function() {
-        let something = 'import data'
-    })
-
-    omega.addEventListener('click', function() {
-        let something = 'import data'
-    })
-
-    // mark.addEventListener('click', function() {
-    //     let something = 'import data'
-    // })
+    let addeditem = document.getElementById('nitem')
+    addeditem.classList.add('nitem')
+    itemDiv.appendChild(addeditem)
+    console.log(addeditem)
+    // let newtext = document.createTextNode()
+    // let item1 = document.getElementById('')
 })
+
+// Confirm.addEventListener('click', function() {
+//     let todoitem=document.getElementById('item')
+//     let completed=document.getElementById('citem')
+//     let newitem=document.getElementById('nitem')
+//     console.log(todoitem, completed, newitem)
+    
+//     delta.addEventListener('click', function() {
+//         let something = 'import data'
+//     })
+
+//     omega.addEventListener('click', function() {
+//         let something = 'import data'
+//     })
+
+//     // mark.addEventListener('click', function() {
+//     //     let something = 'import data'
+//     // })
+// })
+
+
