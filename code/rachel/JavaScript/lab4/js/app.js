c82e98aa26264988bcfdd3d497328e9a97fb8e92
Rachel Lloyd <rachel@Rachels-iMac.local>
2022-07-06 17:15:14 -0400

    Lab4 - completed

--- code/rachel/JavaScript/lab4/js/app.js
+++ code/rachel/JavaScript/lab4/js/app.js
@@ -1,28 +1,35 @@
 new Vue({
     el: '#app',
     data: {
-        tasks : [
-            {text : 'Finish Lab 4'}
-        ],
+        tasks : [],
         inputField: "",
-        completedTasks : []
     },
     methods: {
         addNewTask: function() {
-            this.tasks.push({text: this.inputField})
+            this.tasks.push({text: this.inputField, completed : false})
+            this.inputField = ''
         },
         removeTask: function(task) {
             this.tasks.splice(this.tasks.indexOf(task), 1)
         },
-        // completedTask: function(task) {
-        //     let compTask = this.tasks.splice(this.tasks.indexOf(task), 1)
-        //     this.completedTasks.push({text: compTask})
-            
-        // }
+        markTask: function(task) {
+            if (task.completed === true) {
+                task.completed = false
+            } else {
+                task.completed = true
+            }
+        },
     },
     computed: {
-        completedTask() {
-            
+        completedTask: function() {
+            return this.tasks.filter(function(item){
+                return item.completed === true
+            })
+        },
+        incompleteTask: function() {
+            return this.tasks.filter(function(item){
+                return item.completed === false
+            })
         }
     }
 })
