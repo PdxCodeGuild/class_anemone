3c55a6a6c528cc752891486e562477d0863864a2
Faith Cicero <faithmates98@gmail.com>
2022-07-06 13:19:07 -0600

    lab05html

--- code/faith/lab05html.html/app.py
+++ code/faith/lab05html.html/app.py
@@ -1,11 +1,19 @@
-
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html', )
+
+@app.route('/', methods=['GET','POST'])
+def number_input():
+    distance = request.form['distance']
+    processed_text = distance.upper()
+    return processed_text
+          
+
 
 app.run()
