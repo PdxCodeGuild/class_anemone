6bc23059a6f4b4dc441a83badffafff8f638e74d
Ollie's Macbook Pro <ojc@Ollies-MacBook-Pro.local>
2022-07-06 16:10:29 -0700

    part b - done

--- code/ollie/js/Lab-01/part_b.js
+++ code/ollie/js/Lab-01/part_b.js
@@ -1,39 +1,43 @@
 let distance = document.getElementById('distance')
 let input = document.getElementById('in_unit')
 let output = document.getElementById('out_unit')
-let result 
+let convert = document.getElementById('convert')
+let display = document.getElementById('display')
+let result
+let newDist
 
-// let distance = parseInt(prompt("What is the distance (no units): "))
-// let input = prompt("What are the input units? (ft, m , mi, km, in, yd): ")
-// let output = prompt("What are the output units? (ft, m , mi, km, in, yd): ")
-// let result
+convert.addEventListener('click', function() {
+    if (input.value === 'ft') {
+        result = distance.value * 0.3048
+    } else if (input.value === 'm') {
+        result = distance.value * 1
+    } else if (input.value === 'mi') {
+        result = distance.value * 1609.34
+    } else if (input.value === 'km') {
+        result = distance.value * 1000
+    } else if (input.value === 'in') {
+        result = distance.value * 0.0254
+    } else if (input.value === 'yd') {
+        result = distance.value * 0.9144
+    } else {
+        alert('This unit converter does not support that unit of measurement.')
+    }
+    
+    if (output.value === 'ft') {
+        newDist = result / 0.3048
+    } else if (output.value === 'm') {
+        newDist = result / 1
+    } else if (output.value === 'mi') {
+        newDist = result / 1609.34
+    } else if (output.value === 'km') {
+        newDist = result / 1000
+    } else if (output.value === 'in') {
+        newDist = result / 0.0254
+    } else if (output.value === 'yd') {
+        newDist = result / 0.9144
+    }
 
-// if (input === 'ft') {
-//     result = distance * 0.3048
-// } else if (input === 'm') {
-//     result = distance * 1
-// } else if (input === 'mi') {
-//     result = distance * 1609.34
-// } else if (input === 'km') {
-//     result = distance * 1000
-// } else if (input === 'in') {
-//     result = distance * 0.0254
-// } else if (input === 'yd') {
-//     result = distance * 0.9144
-// } else {
-//     alert('This unit converter does not support that unit of measurement.')
-// }
-
-// if (output === 'ft') {
-//     alert(`${distance} ${input} is ${(result / 0.3048).toFixed(2)} ${output}.`)
-// } else if (output === 'm') {
-//     alert(`${distance} ${input} is ${(result / 1).toFixed(2)} ${output}.`)
-// } else if (output === 'mi') {
-//     alert(`${distance} ${input} is ${(result / 1609.34).toFixed(2)} ${output}.`)
-// } else if (output === 'km') {
-//     alert(`${distance} ${input} is ${(result / 1000).toFixed(2)} ${output}.`)
-// } else if (output === 'in') {
-//     alert(`${distance} ${input} is ${(result / 0.0254).toFixed(2)} ${output}.`)
-// } else if (output === 'yd') {
-//     alert(`${distance} ${input} is ${(result / 0.9144).toFixed(2)} ${output}.`)
-// }
+    let print = document.createElement('p')
+    print.innerText = `${distance.value} ${input.value} is ${newDist.toFixed(1)} ${output.value}.`
+    display.prepend(print)
+})
