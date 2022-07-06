8d11b6ad2da9f9e38cf43d546219e5d58d222d94
Jonathan meadows <jon_meadows@ymail.com>
2022-07-06 18:32:05 -0500

    lab04 wip

---
+++ code/meadows/js_lab04/todo.js
@@ -0,0 +1,15 @@
+new Vue({
+    el:'#app',
+    data: {
+        newToDo: '',
+        todos: [
+            {text: 'Get BEER'},
+            {text: 'Go Swimming with Sharks'},
+            {text: 'Befriend a Grizzly Bear'}
+        ]
+    },
+    
+    methods: {
+
+    }
+})
