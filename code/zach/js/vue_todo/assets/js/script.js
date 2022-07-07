acd051553df3be990a110f8b56514638fdebc0b3
Zach-EE <zachholloway.n@gmail.com>
2022-07-07 12:49:55 -0400

    have basic todo up and running

--- code/zach/js/vue_todo/assets/js/script.js
+++ code/zach/js/vue_todo/assets/js/script.js
@@ -1,6 +1,37 @@
-new Vue({
+let vm = new Vue({
   el: '#app',
-  data: {
-    message: 'Hello world!',
+  data: {      
+    todos: [
+      { text: 'Push Me' },
+      { text: 'And Just Touch Me' },
+      { text: 'Till I get my' },
+      { text: 'Satisfaction' },
+    ],
+    inputField: '',
+    return: {
+      isIncomplete: false,
+      isFinished: true,    
+    } 
+
+  },
+  methods: {
+    addTodo: function () {
+      this.todos.push({ text: this.inputField });
+    },
+    delTodo: function (todo) {
+      // console.log(todo)
+      this.todos.splice(this.todos.indexOf(todo), 1);
+    },
+    findClass: function () {
+      return this.class;
+    },
+    markDone: function (event) {
+      console.log(this.class);
+      if (this.class == 'col btn btn-outline-success btn-sm') {
+        this.class = 'col btn btn-success btn-sm';
+      } else {
+        this.class = 'col btn btn-outline-success btn-sm';
+      }
+    },
   },
 });
