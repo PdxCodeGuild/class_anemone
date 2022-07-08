c841f87c29d5111fec13cac170d05e6b187363a9
Ollie's Macbook Pro <ojc@Ollies-MacBook-Pro.local>
2022-07-08 16:31:29 -0700

    save

---
+++ code/ollie/js/Lab-02/part_b.js
@@ -0,0 +1,10 @@
+let nums = document.getElementById('nums')
+let num = document.getElementsByClassName('num')
+let add = document.getElementById('addNum')
+let del = document.getElementById('deleteNum')
+let avg = document.getElementById('averageBtn')
+let display = document.getElementById('averageDisplay')
+
+add.addEventListener('click', function() {
+    
+})
