cda8252e6dae806cf1d8077146c27e6465ac63ad
tamarajwood <tjwood270@gmail.com>
2022-07-08 16:47:58 -0700

    lab 5 JS in progress

---
+++ code/tamara/lab_5_JS/quote lab/app.js
@@ -0,0 +1,77 @@
+Vue.component('user-search', {
+    data: function() {
+        return {
+            userInput: '',
+            userChoice: 'keyword',
+        }
+    },
+    template:
+        `<div>
+            <input type="text" v-model="userInput" @keyup.enter="loadUserQuotes">
+            <input type="radio" v-model="userChoice" value="keyword" id="keyword"><label for="keyword">keyword</label>
+            <input type="radio" v-model="userChoice" value="tag" id="tag"><label for="tag">tag</label>
+            <input type="radio" v-model="userChoice" value="author" id="author"><label for="author">author</label>
+            <button @click="loadUserQuotes">Search</button>
+        </div>`
+    ,
+    methods: {
+        userSearch: function () {
+            this.$emit('search', {userChoice: this.userChoice, userInput: this.userInput})
+            this.userInput = ''
+        }
+    }
+})
+
+const quoteApp = new Vue ({
+    el: '#quote-app',
+    data: {
+        randomQuotes: {},
+        userQuotes: {},
+        pageNumber: 1,
+    },
+    methods: {
+        loadRandomQuotes: function () {
+            axios({
+                method: 'get',
+                url: 'https://favqs.com/api/quotes/',
+                headers: {
+                    "Authorization": `Token token="855df50978dc9afd6bf86579913c9f8b"`
+                }
+            }).then(response => this.randomQuotes = response.data)
+        },
+
+        loadUserQuotes: function () {
+            axios({
+                method: 'get',
+                url: 'https://favqs.com/api/quotes/',
+                headers: {
+                    "Authorization": `Token token="855df50978dc9afd6bf86579913c9f8b"`
+                },
+                params: {
+                    filter: this.userInput,
+                    type: this.userChoice,
+                    page: this.pageNumber,
+                }
+            }).then(response => this.userQuotes = response.data)
+
+            
+        },
+
+        nextPage: function() {
+            this.pageNumber ++
+            this.loadUserQuotes()
+        },
+        
+        backPage: function() {
+            this.pageNumber --
+            this.loadUserQuotes()
+        }
+        
+    },
+
+    created: function () {
+        this.loadRandomQuotes()
+    }
+})
+
+// if (this.userQuotes.last_page === false) 
