29ee19367608a506d950ba03cdec5109e174050d
James Johnson <jamesjhnsn1995@gmail.com>
2022-07-11 10:38:52 -0700

    Accepted current changes

--- code/isaac/javascript_two/lab_two/blackjack_partB.js
+++ code/isaac/javascript_two/lab_two/blackjack_partB.js
@@ -1,10 +1,6 @@
-<<<<<<< Updated upstream
 // Blackjack Advise
 
 // Create an object of cards and their values
-=======
-// Blackjack Part B
->>>>>>> Stashed changes
 
 let playerDeck = {
     'A': 1,
@@ -27,7 +23,6 @@ let playerDeck = {
 let winningNumber = 21
 let lowestDraw = 17
 
-<<<<<<< Updated upstream
 // create input for player
 let firstCard = document.querySelector('#card-one');
 let secondCard = document.querySelector('#card-two');
@@ -35,20 +30,11 @@ let thirdCard = document.querySelector('#card-three');
 
 let drawBtn = document.querySelector('#draw');
 let div1 = document.querySelector('#div1');
-=======
-let drawBtn 
-
-// create input for player
-let firstCard = prompt("Draw your first card: ")
-let secondCard = prompt("Draw your second card: ")
-let thirdCard = prompt("Draw your third card: ")
->>>>>>> Stashed changes
 
 // Create a function containing the players deck of cards
 // return drawn cards and add all cards drawn
 
 function cardDeck(cardOne, cardTwo, cardThree) {
-<<<<<<< Updated upstream
     return playerDeck[cardOne.value] + playerDeck[cardTwo.value] + playerDeck[cardThree.value]
 }
 // console.log(cardDeck())
@@ -73,28 +59,3 @@ drawBtn.addEventListener('click', function(){
         div1.innerText = `Your cards are ${finalDraw} Blackjack!`
     }
 })
-=======
-    return playerDeck[cardOne] + playerDeck[cardTwo] + playerDeck[cardThree]
-}
-// console.log(cardDeck(firstCard, secondCard, thirdCard))
-
-// sum up all cards drawn
-
-let cardsTotal = cardDeck(firstCard, secondCard, thirdCard) 
-
-if (cardsTotal in playerDeck) {
-    alert(cardsTotal)
-}
-
-// compare cards drawn and display hit, stay, blackjack or bust.
-
-if (cardsTotal < lowestDraw) {
-    alert(`${cardsTotal} Hit`)
-} else if (cardsTotal >= lowestDraw && cardsTotal < winningNumber) {
-    alert(`${cardsTotal} Stay`)
-} else if (cardsTotal > winningNumber) {
-    alert(`${cardsTotal} Busted!!`)
-} else if (cardsTotal === winningNumber) {
-    alert(`${cardsTotal} Blackjack!`)
-}
->>>>>>> Stashed changes
