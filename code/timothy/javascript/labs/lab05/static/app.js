f17fb44d594796a66b971b8f56380d64e95a5f06
Timothy Johnson <johnsonandtimothy@gmail.com>
2022-07-11 09:24:17 -0700

    ++

--- code/timothy/javascript/labs/lab05/static/app.js
+++ code/timothy/javascript/labs/lab05/static/app.js
@@ -1,35 +1,4 @@
-Vue.component('pagination', {
-    props: ['page'],
-    template: `
-        <div>
-            <button @click="nextPage">Next Page</button>
-        </div>
-    `,
-    data: function() {
-        return {
-            nextpage: this.page
-        }
-    },
-    methods: {
-        nextPage: function() {
-            axios({
-                method: 'get',
-                url: 'https://favqs.com/api/quotes',
-                params: {
-                    filter: this.searchText,
-                    type: this.searchTag,
-                    page: this.page,
-                    last_page: this.last_page
-                },
-                headers: {
-                    "Authorization": `Token token="3599aff7e790f1c38ab2e9ee892e36bf"`,}
-            })
-            this.nextpage++
-            this.$emit('next-page', this.nextpage)
-            console.log(this.nextpage)
-        }
-    }
-})
+
 
 
 const vm = new Vue({
