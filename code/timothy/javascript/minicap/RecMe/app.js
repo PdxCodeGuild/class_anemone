bdf12aeac89f471efd1c6cd442eed1e982c25359
Timothy Johnson <johnsonandtimothy@gmail.com>
2022-07-11 13:21:44 -0700

    first ex

--- code/timothy/javascript/minicap/RecMe/app.js
+++ code/timothy/javascript/minicap/RecMe/app.js
@@ -1,6 +1,28 @@
 const vm = new Vue ({
     el: '#app',
     data: {
-        
+        searchResults: {},
+        searchResultName: "",
+        searchResultTitle: "",
+        searchTerm: "",
+        searchType: ""
+    },
+    methods: {
+        loadResults: function() {
+            axios({
+                method: 'get',
+                url: 'https://api.lyrics.ovh/v1/',
+                params: {
+                    artist: this.searchResultName,
+                    title: this.searchResultTitle
+                }
+                
+            }).then(response => this.searchResultName = response.data)
+            .then(console.log(this.searchResultName)
+            .catch(error => {
+                console.log(error)
+                console.log(error.response.data)
+            })
+        )}
     }
 })
