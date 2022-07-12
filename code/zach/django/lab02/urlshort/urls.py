513d5bca6b311f7df998591198e56e6f96fd7850
Zach-EE <zachholloway.n@gmail.com>
2022-07-12 17:47:26 -0400

    got redirect working

--- code/zach/django/lab02/urlshort/urls.py
+++ code/zach/django/lab02/urlshort/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home_page_view, url_short
-
+from .views import home_page_view, url_short, url_Redirect
+app_name = 'url_shortener'
 urlpatterns = [
     path('', home_page_view, name='home'),
     path('urlshort', url_short, name='url'),
+    path('<str:slug>/', url_Redirect, name='redirect')
 ]
