92fe03f4233a7277e2a3f101e882a69e943fd53a
Adeeon <cgt10dt@gmail.com>
2022-07-12 18:35:07 -0500

    capstone

--- code/charles/JavaScript/capstone/main.js
+++ code/charles/JavaScript/capstone/main.js
@@ -1,29 +1,36 @@
 const vm = new Vue({
     el:"#main",
     data:{
-        Chardata:{
-            ability_scores:{},
-            alignments:{},
-            backgrounds:{},
-            languages:{},
-            proficiencies:{},
-            skills:{}
-        },
-        Class:{
-
-        },
-        search:''
+        response:{},
+        out:{},
+        sout:{},
+        error:{},
+        element:'',
+        element2: '',
+        element3:''
     },
 
     methods:{
         loadhome: function() {
             axios({
                 method: 'get',
-                url: 'https://www.dnd5eapi.co/api/ability-scores'
-            }).then(response => this.quotd = response.data, console.log(response.data))
+                url: 'https://api.fiscaldata.treasury.gov/services/api/fiscal_service'
+            }).then(response => {this.out = response.json}, console.log(response.data, this.out))
             .catch(error => {
                 console.log(error, error.response.data)
                 this.error=error.response.data})
+        },
+        search: function() {
+            if (element2 != '') {
+                element +='/'+element2
+            } if (element3 != '') {
+                element +='/'+element3
+            }
+            axios({
+                method: 'get',
+                url: 'https//www.dnd5eapi.com/api/'+this.element
+            }).then(response => {this.sout = response.data},
+            console.log(response.data)).catch(error => {this.error=error.response.data})
         }
     }
 })
