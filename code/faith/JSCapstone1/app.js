765d4dbc93af95ac9169ec28ae59ef75c0def8f8
Faith Cicero <faithmates98@gmail.com>
2022-07-13 15:11:28 -0600

    JSCapstone

--- code/faith/JSCapstone1/app.js
+++ code/faith/JSCapstone1/app.js
@@ -1,13 +1,42 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+        message: '',
+        url: 'https://api.funtranslations.com/translate/pig-latin.json',
+        results: {},
+        translated:''
+    },
+    methods: {
+        getPigLatin: function() {
+            axios({
+                method: 'get',
+                url: 'https://api.funtranslations.com/translate/pig-latin.json',
+                params: {
+                   
+                    text:this.message
+                }
+                }
+              ).then((response) => {
+                console.log(response.data)
+                this.results=response.data
+                this.translated = this.results.contents.translated
+              })
+        }
+        
     }
   })
 
-  axios({
-    method: 'get',
-    url: 'https://api.funtranslations.com/translate/'
-  }).then((response) => {
-    console.log(response.data)
-  })
+
+
+  
+  
+//   axios({
+//     method: 'post',
+//     url: 'https://api.funtranslations.com/translate/pig-latin.json',
+//     data: {
+//         translated:this.message,
+        
+//     }
+// }).then((response) => {
+//     console.log(response.data)
+// })
