37e48defa0a9422c06142a0440f89f2d6f1aec54
tamarajwood <tjwood270@gmail.com>
2022-07-12 23:28:56 -0700

    minicap JS in progress

--- code/tamara/JS_minicap/app.js
+++ code/tamara/JS_minicap/app.js
@@ -1,33 +1,3 @@
-// var pf = new petfinder.Client({apiKey: "QUQDsRiiouriGF02BN27GLhwZWufM4PSoAxwGypLyH9WjTtpS7", secret: "Yoe36Ouy6oVLiXlX7hJCQJMkDW5VJHhxDLQ73LEw"});
-// console.log(pf)
-// dogData = {}
-// pf.animalData.type('Dog')
-//     .then(function (response) {
-//     dogData = response.data
-//     })
-// console.log(dogData)
-
-// import { Client } from "@petfinder/petfinder-js";
-
-// dogData = {}
-
-// const client = new Client({
-//   apiKey: "QUQDsRiiouriGF02BN27GLhwZWufM4PSoAxwGypLyH9WjTtpS7",
-//   secret: "Yoe36Ouy6oVLiXlX7hJCQJMkDW5VJHhxDLQ73LEw",
-//   token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJRVVFEc1JpaW91cmlHRjAyQk4yN0dMaHdaV3VmTTRQU29BeHdHeXBMeUg5V2pUdHBTNyIsImp0aSI6ImYwNTM0YzA3NzQxMGNkYWEzNTc4OTBhNmFhYWNjN2E1NmEyZmNiMzM1MTY3OWRjM2U0ZjQ1NWZkOGFlNmVhZGFhNGNhNDYyOTFiMGIyOTk0IiwiaWF0IjoxNjU3NTgxMTk0LCJuYmYiOjE2NTc1ODExOTQsImV4cCI6MTY1NzU4NDc5NCwic3ViIjoiIiwic2NvcGVzIjpbXX0.xjgzg6GVNSyF-xFmzgADul54dZoz54-_ceHl8KuE9GWpcmO6x7svILSFrS6rNu-dLCcrPeJ99qdRukWPJ209QSbd9iP59YPjTfRbxjBKwxa4vxFPiW3Wv0UPzW9KEoeuAqdtEseE23-qz0_K17Ez7Bro2_JlC4rScQ6j-PEdEnsrvH0wTbgmlciHAvtJmPg-pNQ1SEvQJXmwgnvuUfQdZ_QXO7srS3PaAKz7JiJPBzz-Y8hoGvHTcJ5kamuK6sx0MRmmmLA6fj11AODQ2tyjGFYklPh3xUhbsayp6COiKADu8LpmC7wTlFptEPgTyokB_mqsMLE4UgI0J0SKw42AOQ",
-// });
-
-// client.animalData.breeds('dog')
-//   .then(resp => {
-//     dogData = resp.data.breeds
-//     console.log(dogData)
-//   });
-
-// import { Client } from "@petfinder/petfinder-js";
-// Client = require('@petfinder/petfinder-js');
-
-// pf: new petfinder.Client({apiKey: "QUQDsRiiouriGF02BN27GLhwZWufM4PSoAxwGypLyH9WjTtpS7", secret: "Yoe36Ouy6oVLiXlX7hJCQJMkDW5VJHhxDLQ73LEw"})
-
 const dogFinder = new Vue ({
     el: '#dog-finder',
     data: {
@@ -40,46 +10,64 @@ const dogFinder = new Vue ({
         // addition 'endurance' option for hardcore athletes
         // can add aditional options later and climate options like hot or cold climates
         // based on what the user selects it will filter by breed and it will only show adult and younger dogs
-        location: '',
+        location: '98294',
         // user enters gps cordinates, zip or city and state
         // need results sorted by closest first
         // can later allow user to choose sort function
         pageNumber: 1,
         // the starting page number
         // equivalent to current_page in the json data
-        hikingBreeds: {},
-        runningBreeds: {},
-        cyclingBreeds: {},
-        huntingBreeds: {}
-        // list out all the breeds that fall into each category
+        userBreed: '',
+        breedList: [
+            {breed: 'labrador retriever', activities: ['hiking', 'hunting'], endurance: false}, 
+            {breed: 'husky', activites: ['hiking', 'running'], endurance: true},
+            {breed: 'golden retriever', activites: ['running', 'hunting'], endurance: false},
+    ]
         // can add 'endurance' key with true or false to each breed as well
     },
     methods: {
 
-        getDogData: function() {
-            pf.animal.search().then(function (response) {
+        getNewDogData: function() {
+            this.pf.animal.search({
+                type: "dog",
+                breed: this.userBreed,
+                page: this.pageNumber,
+                location: this.location,
+                // breed: "husky"
+            }).then(response => {
                 this.dogData = response.data
-                console.log(response)
-            }).catch(function(error) {
-                console.log(error)
+            })
+            this.pageNumber = 1
+        },
+        
+        getDogData: function() {
+            this.pf.animal.search({
+                type: "dog",
+                breed: this.userBreed,
+                page: this.pageNumber,
+                location: this.location,
+                // breed: "husky"
+            }).then(response => {
+                this.dogData = response.data
             })
        },
-        // getDogData: function() {
-        //     axios({
-        //         method: 'get',
-        //         url: 'https://api.petfinder.com/v2/',
-        //         headers: {
-        //             "Authorization": `Token token="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJRVVFEc1JpaW91cmlHRjAyQk4yN0dMaHdaV3VmTTRQU29BeHdHeXBMeUg5V2pUdHBTNyIsImp0aSI6ImYwNTM0YzA3NzQxMGNkYWEzNTc4OTBhNmFhYWNjN2E1NmEyZmNiMzM1MTY3OWRjM2U0ZjQ1NWZkOGFlNmVhZGFhNGNhNDYyOTFiMGIyOTk0IiwiaWF0IjoxNjU3NTgxMTk0LCJuYmYiOjE2NTc1ODExOTQsImV4cCI6MTY1NzU4NDc5NCwic3ViIjoiIiwic2NvcGVzIjpbXX0.xjgzg6GVNSyF-xFmzgADul54dZoz54-_ceHl8KuE9GWpcmO6x7svILSFrS6rNu-dLCcrPeJ99qdRukWPJ209QSbd9iP59YPjTfRbxjBKwxa4vxFPiW3Wv0UPzW9KEoeuAqdtEseE23-qz0_K17Ez7Bro2_JlC4rScQ6j-PEdEnsrvH0wTbgmlciHAvtJmPg-pNQ1SEvQJXmwgnvuUfQdZ_QXO7srS3PaAKz7JiJPBzz-Y8hoGvHTcJ5kamuK6sx0MRmmmLA6fj11AODQ2tyjGFYklPh3xUhbsayp6COiKADu8LpmC7wTlFptEPgTyokB_mqsMLE4UgI0J0SKw42AOQ"`
-        //         }
-        //     }).then(response => {
-        //         this.dogData = response.data
-        //         console.log(response)
-        //         console.log(dogData)
-        //     })
-        // },
 
-    created: function() {
+       nextPage: function() {
+        this.pageNumber ++
         this.getDogData()
-    }
+       },
+
+       backPage: function() {
+        this.pageNumber --
+        this.getDogData()
+       },
+
+       activityType: function() {
+
+       }
+
+    // created: function() {
+    //     this.getDogData()
+    // }
     }
 })
