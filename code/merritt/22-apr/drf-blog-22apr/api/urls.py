991a8a951e25243e357c17963d28c5b143eef316
Merritt Lawrenson <merrittlawrenson@gmail.com>
2022-07-14 16:30:47 -0700

    drf examples

---
+++ code/merritt/22-apr/drf-blog-22apr/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+router = DefaultRouter()
+router.register('posts', views.PostViewSet, basename='posts')
+router.register('users', views.UserViewSet, basename='users')
+
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path('posts/<int:pk>/', views.PostDetail.as_view()),
+#     path('posts/', views.PostList.as_view()),
+# ]
