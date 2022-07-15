ba2ca65f0ff0af4b904943675779a63726a3636c
Rachel Lloyd <rachellloyd@Rachels-MacBook-Pro.local>
2022-07-15 19:38:03 -0400

    Lab 6 Django - in progress

--- code/rachel/django/django_lab6/apis/views.py
+++ code/rachel/django/django_lab6/apis/views.py
@@ -1,7 +1,11 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from students.models import Student
 from .serializers import StudentSerializer
 
 class ListStudent(generics.ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentViewSet(viewsets.Modelviewset):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
