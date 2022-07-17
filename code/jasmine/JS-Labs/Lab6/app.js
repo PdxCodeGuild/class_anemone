9b906fdd0241964db869226bafe9bbf41b1f50cd
JasmineDee <jasminee.davies1@gmail.com>
2022-07-16 18:31:16 -0800

    movies rendering

--- code/jasmine/JS-Labs/Lab6/app.js
+++ code/jasmine/JS-Labs/Lab6/app.js
@@ -1,57 +1,65 @@
 
 
-// Vue.component('Movie', {
-//     data: function () {
-//         return {
-//             clicks: 0
-//         }
-//     },
-//     props: ['movie', 'addToWatchLater', 'isInWatchLater', 'removeWatchLater'],
-//     template: `
-//     <div class="card">
-//     <img class="card-img-top" :src="movie.Poster" :alt="movie.Title">
-//     <div class="card-body">
-//       <h5 class="card-title">{{movie.Title}}</h5>
-//       <p class="card-text">{{movie.Year}}</p>
-//       <button
-//         v-if="addToWatchLater && !isInWatchLater(movie)"
-//         @click="addToWatchLater(movie)"
-//         type="button"
-//         class="btn btn-success">Watch Later</button>
-//       <button
-//         v-if="removeWatchLater"
-//         @click="removeWatchLater(movie)"
-//         type="button"
-//         class="btn btn-danger">Remove</button>
-//     </div>
-//   </div>
-//     `
-// })
+Vue.component('Movie', {
+    data: function () {
+        return {
+            clicks: 0
+        }
+    },
+    props: ['movie', 'addToWatchLater', 'isInWatchLater', 'removeWatchLater'],
+    template: `
+    <div class="card">
+    <img class="card-img-top" :src="movie.Poster" :alt="movie.Title">
+    <div class="card-body">
+      <h5 class="card-title">{{movie.Title}}</h5>
+      <p class="card-text">{{movie.Year}}</p>
+      <button
+        v-if="addToWatchLater && !isInWatchLater(movie)"
+        @click="addToWatchLater(movie)"
+        type="button"
+        class="btn btn-success">Watch Later</button>
+      <button
+        v-if="removeWatchLater"
+        @click="removeWatchLater(movie)"
+        type="button"
+        class="btn btn-danger">Remove</button>
+    </div>
+  </div>
+    `
+})
 
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=b97ab39d';
+const API_URL = 'http://www.omdbapi.com/?apikey=b97ab39d&?type=movie&s=$';
 const vm= new Vue({
     el: '#app',
     data : {
       searchTerm:'',
       results:[],
       error: '',
+      watchLater:[],
 
     },
     methods:{
-      getResults : function (){
-        this.result = {}
-        this.error = '',
-        axios({
-          method: 'get',
-          url : `http://www.omdbapi.com/?apikey=b97ab39d&?type=movie&s=${this.searchTerm}`,
-       
-        }).then(response => {
-          this.results = response.data
-          console.log(response.data)
-        }).catch(error => {
-          this.error = response.data
-          console.log(error.response.data)
-        })
-    }
-  }
-});
+      async getResults() {
+        const url = `${API_URL}${this.searchTerm}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        if (data.Error) {
+          this.results = [];
+          this.error = data.Error;
+        } else {
+          this.results = data.Search;
+          this.error = '';
+        }
+      },
+      addToWatchLater(movie) {
+        this.watchLater.push(movie);
+      },
+      removeWatchLater(movie) {
+        const index = this.watchLater.indexOf(movie);
+        this.watchLater.splice(index, 1);
+      },
+      isInWatchLater(movie) {
+        return this.watchLater.some(wl => wl.imdbID === movie.imdbID);
+      },
+    },
+  })
