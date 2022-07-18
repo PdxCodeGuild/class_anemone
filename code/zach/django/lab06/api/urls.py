5562b3e972bb4239403ebdf395e2baffa0293ef5
Zach-EE <zachholloway.n@gmail.com>
2022-07-18 14:06:04 -0400

    have api allowing crud operations

--- code/zach/django/lab06/api/urls.py
+++ code/zach/django/lab06/api/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('', views.getData),
-    path('add', views.addStudent)
-]
+router = DefaultRouter()
+router.register('', views.StudentViewSet, basename='students')
+urlpatterns = router.urls
+
+
+# urlpatterns = [
+#     path('add', views.addStudent),
+#     path('all', views.ListStudent.as_view()),
+#     path('<int:pk>/', views.DetailStudent.as_view()),
+# ]
--- code/zach/django/lab06/api/views.py
+++ code/zach/django/lab06/api/views.py
@@ -1,12 +1,13 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from students.models import Student
+from rest_framework import generics, viewsets
+from students import models
 from .serializers import StudentSerializer
 
 
 @api_view(['GET'])
 def getData(request):
-    students = Student.objects.all()
+    students = models.Student.objects.all()
     serializer = StudentSerializer(students, many=True)
     return Response(serializer.data)
 
@@ -16,3 +17,15 @@ def addStudent(request):
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
+
+class ListStudent(generics.ListCreateAPIView):
+    queryset = models.Student.objects.all()
+    serializer_class = StudentSerializer
+    
+class DetailStudent(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Student.objects.all()
+    serializer_class = StudentSerializer
+    
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = models.Student.objects.all()
+    serializer_class = StudentSerializer
