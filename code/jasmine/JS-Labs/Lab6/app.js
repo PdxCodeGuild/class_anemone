7e47f03acfd2d4b8eedf9bfbf1fa686021f4f668
JasmineDee <jasminee.davies1@gmail.com>
2022-07-18 08:34:02 -0800

    functioning app

--- code/jasmine/JS-Labs/Lab6/app.js
+++ code/jasmine/JS-Labs/Lab6/app.js
@@ -1,30 +1,29 @@
 
 
-Vue.component('Movie', {
-    data: function () {
-        return {
-            clicks: 0
-        }
-    },
-    props: ['movie', 'addToWatchLater', 'isInWatchLater', 'removeWatchLater'],
-    template: `
-    <div class="card">
-    <img class="card-img-top" :src="movie.Poster" :alt="movie.Title">
-    <div class="card-body">
-      <h5 class="card-title">{{movie.Title}}</h5>
-      <p class="card-text">{{movie.Year}}</p>
-      <button
-        type="button"
-        class="btn btn-success">Watch Later</button>
-      <button
-        v-if="removeWatchLater"
-        @click="removeWatchLater(movie)"
-        type="button"
-        class="btn btn-danger">Remove</button>
-    </div>
-  </div>
-    `
-})
+// Vue.component('movie-object', {
+ 
+//     props: ['movie', 'addToWatchLater', 'isInWatchLater', 'removeWatchLater'],
+
+//     template: `
+//     <div class="card">
+//     <img class="card-img-top" :src="movie.Poster" :alt="movie.Title">
+//     <div class="card-body">
+//       <h5 class="card-title">{{movie.Title}}</h5>
+//       <p class="card-text">{{movie.Year}}</p>
+//       <button
+//       v-if="addToWatchLater && !isInWatchLater(movie)"
+//       @click="addToWatchLater(movie)"
+//       type="button"
+//       class="btn btn-success">Watch Later</button>
+//       <button
+//         v-if="removeWatchLater"
+//         @click="removeWatchLater(movie)"
+//         type="button"
+//         class="btn btn-danger">Remove</button>
+//     </div>
+//   </div>
+//     `
+// })
 
 const API_URL = 'http://www.omdbapi.com/?apikey=b97ab39d&?type=movie&s=$';
 const vm= new Vue({
