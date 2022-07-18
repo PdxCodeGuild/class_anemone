37923e64969d5a3a8fb9c63e77c88a924c975b71
Frances Kappes <fekmacbookpro2019@Feks-MacBook-Pro-2019.local>
2022-07-18 16:12:42 -0700

    JS Lab05 complete

--- code/fran/javascript/lab05/app.js
+++ code/fran/javascript/lab05/app.js
@@ -1,9 +1,20 @@
+Vue.component('quotes-result', {
+    props: ['componentquote'],  // componentQuote represents a single quote from the api response
+    template: `
+        <li>
+            <p>{{ componentquote.body }}</p>
+            <p><em>- {{ componentquote.author }}</em></p>
+            <br>
+        </li>
+    `
+})
+
 const vm = new Vue({
     el: "#app",
     data: {
         searchString: null,
         selectedType: null,
-        requestedPage: 2,
+        requestedPage: 1,
         quotesResult: {}
     },
     methods: {
@@ -21,14 +32,22 @@ const vm = new Vue({
                     "Authorization": `Token token="855df50978dc9afd6bf86579913c9f8b"`
                 }
             }).then(response => {
-                this.quotesResult = null
-                console.log(response)
-                // console.log(response.data)
+                // this.quotesResult = null
+                // console.log(response)
                 this.quotesResult = response.data
+                console.log(response.data)
             }).catch(error => {
                 console.log(error)
                 console.log(error.response_data)
             })
+        },
+        previousPage: function() {
+            this.requestedPage --
+            this.loadSearchQuotes()
+        },
+        nextPage: function() {
+            this.requestedPage ++
+            this.loadSearchQuotes()
         }
     },
     created: function() {
