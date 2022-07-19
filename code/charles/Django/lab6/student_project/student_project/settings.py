2ecd5c6d1923a9ff7d81951ef0456e9e7695331c
Adeeon <cgt10dt@gmail.com>
2022-07-19 18:05:43 -0500

    lab6wip

--- code/charles/Django/lab6/student_project/student_project/settings.py
+++ code/charles/Django/lab6/student_project/student_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'students.apps.StudentsConfig',
     'api.apps.ApiConfig',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
@@ -129,5 +130,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny'
-    ]
+    ],
+    'DEFAULT_FILTER_BACKENDS':[
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
 }
