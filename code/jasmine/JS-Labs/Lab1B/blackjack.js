98e748468cd5c0dfa07b35c876e9c014554a239b
JasmineDee <jasminee.davies1@gmail.com>
2022-07-19 09:34:24 -0800

    version b updated

--- code/jasmine/JS-Labs/Lab1B/blackjack.js
+++ code/jasmine/JS-Labs/Lab1B/blackjack.js
@@ -1,28 +1,82 @@
-let first_card = document.getElementById('card1')
-let second_card = document.getElementById('card2')
-let third_card = document.getElementById('card3')
-let cards = document.getElementsByClassName('card')
-let total = document.getElementById('points')
-let button = document.getElementById('advice')
+let first_card = document.querySelector("#first_card")
+let second_card = document.querySelector("#second_card")
+let third_card = document.querySelector("#third_card")
 
 
-function addPoints(){
+let card_one = {
+    'a': 11,
+    'A': 11,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10,
+    'j': 10,
+    'J': 10,
+    'q': 10,
+    'Q': 10,
+    'k': 10,
+    'K': 10
+}
+let card_two = {
+    'a': 1,
+    'A': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10,
+    'j': 10,
+    'J': 10,
+    'q': 10,
+    'Q': 10,
+    'k': 10,
+    'K': 10
+}
 
-    
-    
 
-    if (points >= 17 && points < 21){
-        return `{points} - "You should stay here"`
-    } 
-        else if (points === 21){
-            return `{points} - "BlackJack!"`
+
+let btn = document.querySelector("#btn")
+btn.addEventListener("click", function () {
+    let points = document.querySelector("#points")
+
+    total = card_one[first_card.value] + card_one[second_card.value] + card_one[third_card.value]
+
+    if (total < 17) {
+
+        points.innerText = `You should choose another card with ${total}`
+
     }
-        else if (points > 21){
-            return `{points} - "Busted! you lost"`
+    else if (17 <= total && total < 21) {
+        points.innerText = `You should stay here! ${total}`
+    }
+    else if (total === 21) {
+        points.innerText = `${total} Blackjack! You win`
+    }
+    else if (total > 21) {
+        total = card_two[first_card.value] + card_two[second_card.value] + card_two[third_card.value]
+
+        if (total < 17) {
+            points.innerText = `You should choose another card with ${total}`
+        }
+        else if (17 <= total && total < 21) {
+            points.innerText = `You should stay here! ${total}`
+        }
+        else if (total === 21) {
+            points.innerText = `${total} BLACKJACK`
+        }
+        else if (total > 21) {
+            points.innerText = `${total} BUSTED. You lose...`
         }
     }
 
-button.addEventListener('click', function(){
-    let points = parseInt(first_card.value) + parseInt(second_card.value) + parseInt(third_card.value)
-    console.log(points)
+
 })
