9b3f828dfdf0579e601705c28fdc31b33417b1d1
Jonathan meadows <jon_meadows@ymail.com>
2022-07-20 18:32:09 -0500

    should be working but its not posting the searchs

--- code/meadows/js_lab05/favq.js
+++ code/meadows/js_lab05/favq.js
@@ -1,10 +1,10 @@
-const vm = new VTTCue({
+
+const vm = new Vue({
     el:"#app",
     data: {
-        rando: {},
-        searched: {},
-        inputTxt: "",
-        inputTag: "",
+        authors: {},
+        selected: null,
+        inputTxt: null,
         page: 1,
     },
     
@@ -17,10 +17,50 @@ const vm = new VTTCue({
                     "Authorization": `Token token="855df50978dc9afd6bf86579913c9f8b"`
                 }
             }).then(response => {
-                this.rando = response.data
+                this.authors = response.data
             }).catch(error => {
                 console.log(error.response.data)
             })
+        },
+        loadSearch: function() {
+            axios({
+                method: 'get',
+                url: 'https://favqs.com/api/qoutes',
+                params: {
+                    type: this.selected,
+                    filter: this.inputTxt,
+                    page: this.page
+                },
+                headers: {
+                    "Authorization": `Token token="855df50978dc9afd6bf86579913c9f8b"`
+                }
+            }).then(response => {
+                this.authors = response.data
+            }).catch(error => {
+                console.log(error.response.data)
+            })
+        },
+        previousPage: function() {
+            this.page --
+            this.loadSearch()
+        },
+        nextPage: function() {
+            this.page ++
+            this.loadSearch()
         }
+    },
+    created: function() {
+        axios({
+            method:'get',
+            url: 'https://favqs.com/api/quotes/',
+            headers: {
+                "Authorization": `Token token="855df50978dc9afd6bf86579913c9f8b"`
+            }
+            }).then(response => {
+                this.authors = response.data
+                console.log(this.quotes, response.data)
+            }).catch(error => {
+                console.log(error, error.response.data)
+                this.error = error.response.data})
     }
 })
