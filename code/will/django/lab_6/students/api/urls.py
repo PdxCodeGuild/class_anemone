db9d6425a793c0e0bc1283a1c567e1d7f3fa849f
unknown <wfm366@gmail.com>
2022-07-21 07:54:13 -0600

    Working copy Django Lab 6

---
+++ code/will/django/lab_6/students/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.StudentAPIView.as_view()),
+    path('students/<int:pk>', views.StudentDetails.as_view()),
+    path('students/', views.StudentList.as_view())
+
+]
---
+++ code/will/django/lab_6/students/students_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Student(models.Model):
+    firstname = models.CharField(max_length=120)
+    lastname = models.CharField(max_length=120)
+    cohort = models.CharField(max_length=120)
+    favorite_topic = models.CharField(max_length=120)
+    favorite_teacher = models.CharField(max_length=120)
+    capstone = models.URLField
+
+    def __str__(self):
+        return self.lastname
