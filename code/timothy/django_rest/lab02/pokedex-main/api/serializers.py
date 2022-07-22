de07ad0ce2eec49714cdb4ab351e84de73791858
Timothy <tim.ttaj@gmail.com>
2022-07-22 11:20:25 -0700

    Pokedex Complete

--- code/timothy/django_rest/lab02/pokedex-main/api/serializers.py
+++ code/timothy/django_rest/lab02/pokedex-main/api/serializers.py
@@ -8,6 +8,7 @@ class NestedPokemonSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Pokemon
         fields = (
+            'id',
             'name',
         )
 
@@ -15,11 +16,12 @@ class NestedTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Type
         fields = (
+            'id',
             'type',
         )
 
 class PokemonSerializer(serializers.ModelSerializer):
-    type = NestedTypeSerializer(many=True, source='types')
+    type_info = NestedTypeSerializer(many=True, source='types')
     class Meta:
         model = models.Pokemon
         fields = (
@@ -31,24 +33,26 @@ class PokemonSerializer(serializers.ModelSerializer):
             'image_front',
             'image_back',
             'caught_by',
-            'type',
+            'type_info',
         )
 
 class TypeSerializer(serializers.ModelSerializer):
-    name = NestedPokemonSerializer(many=True, source='pokemon')
+    name_info = NestedPokemonSerializer(many=True, source='pokemon')
     class Meta:
         model = models.Type
         fields = (
             'type',
-            'name',
+            'name_info',
             'pokemon',
         )
 
 class UserSerializer(serializers.ModelSerializer):
+    caught_info = NestedPokemonSerializer(many=True, read_only=True, source="caught")
     class Meta:
         model = get_user_model()
         fields = (
             'id',
             'username',
             'caught',
+            "caught_info",
         )
