32888f5b42f2fcea1349bc3ec65f30a534876aa6
mac <mac@macs-Air.hsd1.wa.comcast.net>
2022-07-21 18:24:21 -0700

    exmaple

---
+++ code/jasmine/Django/blog_example/books/models.py
@@ -0,0 +1,15 @@
+from audioop import maxpp
+from django.db import models
+
+# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    subtitile = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=13)
+
+    def __str__(self):
+        return self.title
+
+        
---
+++ code/jasmine/Django/blog_example/books/views.py
@@ -0,0 +1,8 @@
+
+from django.views.generic import ListView
+from .models import Book
+
+# Create your views here.
+class BookListView(ListView):
+    model = Book
+    template_name = 'books/index.html'
