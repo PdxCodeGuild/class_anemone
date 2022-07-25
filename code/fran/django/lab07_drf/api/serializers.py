f554bbd5cc3440f9957ee293149acf7f89fe9204
Frances Kappes <fekmacbookpro2019@Feks-MacBook-Pro-2019.local>
2022-07-25 12:34:07 -0700

    Django Lab07 complete

---
+++ code/fran/django/lab07_drf/api/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from pokemon.models import Pokemon, Type
+
+class NestedPokemonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pokemon
+        fields = ('name',)
+
+class NestedTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Type
+        fields = ('type',)
+
+
+class PokemonSerializer(serializers.ModelSerializer):
+    type_info = NestedTypeSerializer(many=True, source='types', read_only=True)
+    class Meta:
+        model = Pokemon
+        fields = ('id', 'number', 'name', 'height', 'weight', 'image_front', 'image_back', 'type_info')
+        # fields = ('number', 'name', 'height', 'weight', 'image_front', 'image_back', 'type_info', 'caught_by')
+
+
+class TypeSerializer(serializers.ModelSerializer):
+    pokemon_info = NestedPokemonSerializer(many=True, source='pokemon', read_only=True)
+    class Meta:
+        model = Type
+        fields = ('id', 'type', 'pokemon_info')
