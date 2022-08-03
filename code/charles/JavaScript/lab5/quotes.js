e2ebcb1092c7aed99a6215b5b3410380862c5b81
Adeeon <cgt10dt@gmail.com>
2022-08-03 12:45:29 -0500

    I had changed it bout i dont think that i had finished so i never commited it

--- code/charles/JavaScript/lab5/quotes.js
+++ code/charles/JavaScript/lab5/quotes.js
@@ -36,6 +36,8 @@ const vm = new Vue({
         error: {},
         typesearch: '',
         input:'',
+        input1: '',
+        input2: '',
         page:1
     },
     
@@ -50,8 +52,50 @@ const vm = new Vue({
                 this.error = error.response.data
             })
         },
-        loadquots: function()  {
+        loadquota: function()  {
             if (this.typesearch === 'author') {
+                axios({
+                    method: 'get', 
+                    url: 'https://favqs.com/api/quotes',
+                    params: {
+                        type: this.typesearch,
+                        filter: (this.input1+'+'+this.input2)
+                    },
+                    headers: {
+                        "Authorization": `Token token="855df50978dc9afd6bf86579913c9f8b"`
+                    }
+                }).then(response => {
+                    this.quotes = response.data
+                    console.log(this.quotes, response.data)
+                }).catch(error => {
+                    console.log(error, error.response.data)
+                    this.error = error.response.data
+                })
+            } 
+        },
+        loadquott: function()  {
+            if (this.typesearch === 'tags') {
+                axios({
+                    method: 'get', 
+                    url: 'https://favqs.com/api/quotes',
+                    params: {
+                        type: this.typesearch,
+                        filter: this.input,
+                    },
+                    headers: {
+                        "Authorization": `Token token="855df50978dc9afd6bf86579913c9f8b"`
+                    }
+                }).then(response => {
+                    this.quotes = response.data
+                    console.log(this.quotes, response.data)
+                }).catch(error => {
+                    console.log(error, error.response.data)
+                    this.error = error.response.data
+                })
+            } 
+        },
+        loadquotu: function()  {
+            if (this.typesearch === 'user') {
                 axios({
                     method: 'get', 
                     url: 'https://favqs.com/api/quotes',
@@ -73,51 +117,133 @@ const vm = new Vue({
         },
         nextq: function() {
             this.page++
-            axios({
-                method:'get',
-                url: 'https://favqs.com/api/quotes',
-                
-                params: {
-                    filter: (this.first+'+'+this.last),
-                    type: 'author',
-                    page: this.page
+            if (this.typesearch === 'author') {
+                axios({
+                    method: 'get', 
+                    url: 'https://favqs.com/api/quotes',
+                    params: {
+                        type: this.typesearch,
+                        filter: (this.input1+'+'+this.input2),
+                        page:this.page
+                    },
+                    headers: {
+                        "Authorization": `Token token="855df50978dc9afd6bf86579913c9f8b"`
+                    }
+                }).then(response => {
+                    this.quotes = response.data
+                    console.log(this.quotes, response.data)
+                }).catch(error => {
+                    console.log(error, error.response.data)
+                    this.error = error.response.data
+                })
+            } 
+            if (this.typesearch === 'tags') {
+                axios({
+                    method: 'get', 
+                    url: 'https://favqs.com/api/quotes',
+                    params: {
+                        type: this.typesearch,
+                        filter: this.input,
+                        page:this.page
+                    },
+                    headers: {
+                        "Authorization": `Token token="855df50978dc9afd6bf86579913c9f8b"`
+                    }
+                }).then(response => {
+                    this.quotes = response.data
+                    console.log(this.quotes, response.data)
+                }).catch(error => {
+                    console.log(error, error.response.data)
+                    this.error = error.response.data
+                })
+            }
+            if (this.typesearch === 'user') {
+                axios({
+                    method:'get',
+                    url: 'https://favqs.com/api/quotes',
+                    params: {
+                        type: this.typesearch,
+                        filter: this.input,
+                        page:this.page
+                    },
+                    headers: {
+                        "Authorization": `Token token="855df50978dc9afd6bf86579913c9f8b"`
+                    }
+                }).then(response => {
+                    this.quotes = response.data
                     
-                },
-                headers: {
-                    "Authorization": `Token token="855df50978dc9afd6bf86579913c9f8b"`
-                }
-            }).then(response => {
-                this.quotes = response.data
-                
-                console.log(this.quotes, response.data)
-            }).catch(error => {
-                console.log(error, error.response.data)
-                this.error = error.response.data
-            })
+                    console.log(this.quotes, response.data)
+                }).catch(error => {
+                    console.log(error, error.response.data)
+                    this.error = error.response.data
+                })
+            }
         },
         previousq: function() {
             if (this.page > 1) {
                this.page-- 
             }  console.log(this.page)          
-            axios({
-                method:'get',
-                url: 'https://favqs.com/api/quotes',
-                params: {
-                    filter: (this.first+'+'+this.last),
-                    type: 'author',
-                    page: this.page
-                },
-                headers: {
-                    "Authorization": `Token token="855df50978dc9afd6bf86579913c9f8b"`
-                }
-            }).then(response => {
-                this.quotes = response.data
-                
-                console.log(this.quotes, response.data)
-            }).catch(error => {
-                console.log(error, error.response.data)
-                this.error = error.response.data
-            })
+            if (this.typesearch === 'author') {
+                axios({
+                    method: 'get', 
+                    url: 'https://favqs.com/api/quotes',
+                    params: {
+                        type: this.typesearch,
+                        filter: (this.input1+'+'+this.input2),
+                        page:this.page
+                    },
+                    headers: {
+                        "Authorization": `Token token="855df50978dc9afd6bf86579913c9f8b"`
+                    }
+                }).then(response => {
+                    this.quotes = response.data
+                    console.log(this.quotes, response.data)
+                }).catch(error => {
+                    console.log(error, error.response.data)
+                    this.error = error.response.data
+                })
+            } 
+            if (this.typesearch === 'tags') {
+                axios({
+                    method: 'get', 
+                    url: 'https://favqs.com/api/quotes',
+                    params: {
+                        type: this.typesearch,
+                        filter: this.input,
+                        page:this.page
+                    },
+                    headers: {
+                        "Authorization": `Token token="855df50978dc9afd6bf86579913c9f8b"`
+                    }
+                }).then(response => {
+                    this.quotes = response.data
+                    console.log(this.quotes, response.data)
+                }).catch(error => {
+                    console.log(error, error.response.data)
+                    this.error = error.response.data
+                })
+            }
+            if (this.typesearch === 'user') {
+                axios({
+                    method:'get',
+                    url: 'https://favqs.com/api/quotes',
+                    params: {
+                        type: this.typesearch,
+                        filter: this.input,
+                        page:this.page
+                    },
+                    headers: {
+                        "Authorization": `Token token="855df50978dc9afd6bf86579913c9f8b"`
+                    }
+                }).then(response => {
+                    this.quotes = response.data
+                    
+                    console.log(this.quotes, response.data)
+                }).catch(error => {
+                    console.log(error, error.response.data)
+                    this.error = error.response.data
+                })
+            }
         }
     },
     created: function() {
