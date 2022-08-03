4c1c2d3634f7345a950b8428c045ce34c97b82cf
Adeeon <cgt10dt@gmail.com>
2022-08-03 12:48:23 -0500

    I had changed it bout i dont think that i had finished so i never commited it

---
+++ code/jasmine/JS-Labs/Lab1B/blackjack.js
@@ -0,0 +1,19 @@
+var cards = {
+    A: 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10,
+    J: 10,
+    Q: 10,
+    K: 10
+}
+   
+
+var cards = document.getElementsByClassName('card')
+var totalBtn = document.getElementById('total')
