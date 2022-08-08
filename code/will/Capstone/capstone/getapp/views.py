8f7186ac63828c3bace7df6844c2e52b4010f305
unknown <wfm366@gmail.com>
2022-08-08 10:21:36 -0600

    Capstone final changes Will

--- code/will/Capstone/capstone/getapp/views.py
+++ code/will/Capstone/capstone/getapp/views.py
@@ -17,25 +17,42 @@ def zillow(request):
     print(request.POST)
     stored_data = []
      
-    url = "https://zillow56.p.rapidapi.com/search"
+    url = "https://zillow-com1.p.rapidapi.com/propertyExtendedSearch"
 
     querystring = {"location":f'{address}. {city},{state}. {zip}'}
 
     headers = {
-	"X-RapidAPI-Key": "42123ed4f1msh64988eb134ebadcp1a8ce1jsn4fa401f0b51a",
-	"X-RapidAPI-Host": "zillow56.p.rapidapi.com"
-
-  
+	"X-RapidAPI-Key": "a734b12ec8msh6f0fe8ed4e1ba42p170be5jsn34eb295dbddb",
+	"X-RapidAPI-Host": "zillow-com1.p.rapidapi.com"
 }
-    
-  
 
     response = requests.request("GET", url, headers=headers, params=querystring)
-    stored_data.append(response)
-    parse_json = json.loads(response.text)
-    display_data = parse_json.keys()
-    # print(display_data)
+
+    zpid = response.json()
+    print(type(dict(zpid)))
+
+
+    url = "https://zillow-com1.p.rapidapi.com/property"
+
+    querystring2 = {"zpid":f'{zpid["zpid"]}'}
+
+    headers2 = {
+	"X-RapidAPI-Key": "a734b12ec8msh6f0fe8ed4e1ba42p170be5jsn34eb295dbddb",
+	"X-RapidAPI-Host": "zillow-com1.p.rapidapi.com"
+}
+
+    response2 = requests.request("GET", url, headers=headers2, params=querystring2)
+
+    parse_json = json.loads(response2.text)
     print(parse_json)
+  
+
+#     response = requests.request("GET", url, headers=headers, params=querystring)
+#     stored_data.append(response)
+#     parse_json = json.loads(response.text)
+#     display_data = parse_json.keys()
+#     # print(display_data)
+#     print(parse_json)
 
   
 
@@ -63,30 +80,30 @@ def zillow(request):
     cloudcover = weather_json['locations'][f'{address}. {city},{state}. {zip}']['values'][0]['cloudcover']
     mint = weather_json['locations'][f'{address}. {city},{state}. {zip}']['values'][0]['mint']
 
-
+# image':image, 'zipcode':zipcode, 'description':description,'home_type':home_type, 'living_area':living_area, 'hoa':hoa,'address': address, 'bathrooms': bathrooms, 'bedrooms':bedrooms, 'lot_size':lot_size, 'rent_zestimate':rent_zestimate
 
     address = parse_json['address']['streetAddress']
-    zipcode = parse_json['address']['zipcode']
+    zipcode = parse_json['zipcode']
     description = parse_json['description']
     home_type = parse_json['homeType']
     living_area = parse_json['livingArea']
-    hoa = parse_json['monthlyHoaFee']
-    image = parse_json['hiResImageLink']
+    # hoa = parse_json['monthlyHoaFee']
+    image = parse_json['imgSrc']
 
     bathrooms = parse_json['bathrooms']
     bedrooms = parse_json['bedrooms']
-    last_price = parse_json['lastSoldPrice']
-    lot_size = parse_json['lotSize']
-    zestimate = parse_json['zestimate']
+    last_price = parse_json['price']
+    lot_size = parse_json['resoFacts']['lotSize']
+    # zestimate = parse_json['zestimate']
     rent_zestimate = parse_json['rentZestimate']
 
-    context = {'image':image, 'zipcode':zipcode, 'description':description,'home_type':home_type, 'living_area':living_area, 'hoa':hoa, 'wdir':wdir, 'maxt':maxt, 'visiblity':visibility, 'wspd':wspd, 'solarenergy':solarenergy, 'cloudcover':cloudcover, 'mint':mint, 'temp':temp, 'weather_response':weather_json, 'address': address, 'bathrooms': bathrooms, 'bedrooms':bedrooms, 'last_price': last_price, 'lot_size':lot_size, 'zestimate':zestimate, 'rent_zestimate':rent_zestimate }
+    context = { 'last_price':last_price,'image':image, 'zipcode':zipcode, 'description':description,'home_type':home_type, 'living_area':living_area,'address': address, 'bathrooms': bathrooms, 'bedrooms':bedrooms, 'lot_size':lot_size, 'rent_zestimate':rent_zestimate, 'wdir':wdir, 'maxt':maxt, 'visiblity':visibility, 'wspd':wspd, 'solarenergy':solarenergy, 'cloudcover':cloudcover, 'mint':mint, 'temp':temp, 'weather_response':weather_json }
 
     return render (request,'users.html', context)
 
    
 
-# 'dew': dew, 'temp': temp, 'temp_max': temp_max, 'temp_min' : temp_min, 'precip': precip, 'dew2': dew2, 'humidity' : humidity, 'solarenergy' : solarenergy, 'cloudcover' : cloudcover
+
 
 def new(request):
     form = InputForm()
