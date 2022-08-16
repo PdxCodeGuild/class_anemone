00e0de029ae5b9fdca3c1f31bf4f2a5e79b1af65
Ollie's Macbook Pro <ojc@Ollies-MacBook-Pro.local>
2022-08-16 14:57:38 -0700

    done

--- code/ollie/django/Lab-01/groceries/urls.py
+++ code/ollie/django/Lab-01/groceries/urls.py
@@ -7,5 +7,7 @@ from . import views
 app_name = 'groceries'
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('create/', views.create, name='create'),
+    path('create/', views.create, name='create'),
+    path('update/<int:pk>/', views.update, name='update'),
+    path('delete/<int:pk>/', views.delete, name='delete')
 ]
--- code/ollie/django/Lab-01/groceries/views.py
+++ code/ollie/django/Lab-01/groceries/views.py
@@ -15,6 +15,24 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
-# def create(request):
-#     item = request.POST['']
-#     GroceryItem.objects.create()
+def create(request):
+    item = request.POST['item_text']
+    GroceryItem.objects.create(item_text=item, created_date=GroceryItem.completed_date)
+    return HttpResponseRedirect(reverse('groceries:index'))
+
+def update(request, pk):
+    item = get_object_or_404(GroceryItem, pk=pk)
+    if item.complete:
+        item.complete = False
+        item.completed_date = None
+        item.save()
+    else:
+        item.complete = True
+        item.completed_date = GroceryItem.completed_date
+        item.save()
+    return HttpResponseRedirect(reverse('groceries:index'))
+
+def delete(request, pk):
+    item = get_object_or_404(GroceryItem, pk=pk)
+    item.delete()
+    return HttpResponseRedirect(reverse('groceries:index'))
